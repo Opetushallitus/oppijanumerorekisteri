b14de33d21c16a90e12b0cf73b070509e9549e92
Petri Ryhänen <petri.ryhanen@csc.fi>
2016-09-19 12:32:10 +0300

    Add multi-module setup.

---
+++ oppijanumerorekisteri-service/src/main/java/fi/vm/sade/oppijanumerorekisteri/OppijanumerorekisteriServiceApplication.java
@@ -0,0 +1,12 @@
+package fi.vm.sade.oppijanumerorekisteri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OppijanumerorekisteriServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OppijanumerorekisteriServiceApplication.class, args);
+	}
+}
---
+++ oppijanumerorekisteri-service/src/test/java/fi/vm/sade/oppijanumerorekisteri/OppijanumerorekisteriServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package fi.vm.sade.oppijanumerorekisteri;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OppijanumerorekisteriServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
