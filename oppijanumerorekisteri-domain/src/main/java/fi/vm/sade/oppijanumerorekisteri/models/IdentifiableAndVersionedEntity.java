d0afea86b225c763a3261e97229bde4d43affad5
Petri Ryhänen <petri.ryhanen@csc.fi>
2016-09-20 15:13:15 +0300

    Generate schema.ddl. Refactor version properties.

--- oppijanumerorekisteri-domain/src/main/java/fi/vm/sade/oppijanumerorekisteri/models/IdentifiableAndVersionedEntity.java
+++ oppijanumerorekisteri-domain/src/main/java/fi/vm/sade/oppijanumerorekisteri/models/IdentifiableAndVersionedEntity.java
@@ -2,20 +2,12 @@ package fi.vm.sade.oppijanumerorekisteri.models;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.annotations.TypeDef;
-import org.hibernate.annotations.TypeDefs;
 
 import javax.persistence.*;
 
 @Getter
 @Setter
 @MappedSuperclass
-@TypeDefs({
-        @TypeDef(name = "localTime", typeClass = org.jadira.usertype.dateandtime.joda.PersistentLocalTime.class),
-        @TypeDef(name = "localDate", typeClass = org.jadira.usertype.dateandtime.joda.PersistentLocalDate.class),
-        @TypeDef(name = "dateTime", typeClass = org.jadira.usertype.dateandtime.joda.PersistentDateTime.class,
-                parameters = {@org.hibernate.annotations.Parameter(name = "databaseZone", value = "jvm")})
-})
 public class IdentifiableAndVersionedEntity {
     @Id
     @Column(name = "id", unique = true, nullable = false)
