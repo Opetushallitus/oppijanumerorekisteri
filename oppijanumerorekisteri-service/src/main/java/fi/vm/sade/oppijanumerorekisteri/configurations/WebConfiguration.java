cf9e914062f7192f976dd79b5e45a71a7c006ddb
Petri Ryhänen <petri.ryhanen@csc.fi>
2016-09-20 13:14:45 +0300

    Add swagger documentation using springfox.

--- oppijanumerorekisteri-service/src/main/java/fi/vm/sade/oppijanumerorekisteri/configurations/WebConfiguration.java
+++ oppijanumerorekisteri-service/src/main/java/fi/vm/sade/oppijanumerorekisteri/configurations/WebConfiguration.java
@@ -2,8 +2,10 @@ package fi.vm.sade.oppijanumerorekisteri.configurations;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
+@EnableSwagger2
 public class WebConfiguration extends WebMvcConfigurerAdapter{
 
 }
