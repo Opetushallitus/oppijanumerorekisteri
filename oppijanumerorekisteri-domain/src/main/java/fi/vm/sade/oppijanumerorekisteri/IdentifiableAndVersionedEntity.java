0c89f3e7dae14cff7d36647843b1eee3713ab38d
Petri Ryhänen <petri.ryhanen@csc.fi>
2016-09-20 10:08:21 +0300

    Add sample domain class. Generate querydsl metadata on compile.

---
+++ oppijanumerorekisteri-domain/src/main/java/fi/vm/sade/oppijanumerorekisteri/IdentifiableAndVersionedEntity.java
@@ -0,0 +1,28 @@
+package fi.vm.sade.oppijanumerorekisteri;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.TypeDef;
+import org.hibernate.annotations.TypeDefs;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@MappedSuperclass
+@TypeDefs({
+        @TypeDef(name = "localTime", typeClass = org.jadira.usertype.dateandtime.joda.PersistentLocalTime.class),
+        @TypeDef(name = "localDate", typeClass = org.jadira.usertype.dateandtime.joda.PersistentLocalDate.class),
+        @TypeDef(name = "dateTime", typeClass = org.jadira.usertype.dateandtime.joda.PersistentDateTime.class,
+                parameters = {@org.hibernate.annotations.Parameter(name = "databaseZone", value = "jvm")})
+})
+public class IdentifiableAndVersionedEntity {
+    @Id
+    @Column(name = "id", unique = true, nullable = false)
+    @GeneratedValue
+    private Long id;
+    
+    @Version
+    @Column(name = "version", nullable = false)
+    private Long version;
+}
