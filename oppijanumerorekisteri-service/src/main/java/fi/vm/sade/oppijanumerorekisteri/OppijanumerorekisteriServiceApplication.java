01725d275e077caa3142fc9e9150fd5fa90a14bc
Petri Ryhänen <petri.ryhanen@csc.fi>
2016-09-27 16:26:27 +0300

    Go back to manually setting the version numbers.

--- oppijanumerorekisteri-service/src/main/java/fi/vm/sade/oppijanumerorekisteri/OppijanumerorekisteriServiceApplication.java
+++ oppijanumerorekisteri-service/src/main/java/fi/vm/sade/oppijanumerorekisteri/OppijanumerorekisteriServiceApplication.java
@@ -1,19 +1,10 @@
 package fi.vm.sade.oppijanumerorekisteri;
 
-import fi.vm.sade.oppijanumerorekisteri.configurations.PropertiesConfiguration;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 public class OppijanumerorekisteriServiceApplication {
-
-	@Autowired
-	void setPropertiesConfiguration(PropertiesConfiguration cp) {
-
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(OppijanumerorekisteriServiceApplication.class, args);
