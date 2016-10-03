911e25df67d9b02ef29853eebf6a5c1cfe71b8e7
Petri Ryhänen <petri.ryhanen@csc.fi>
2016-10-03 15:05:41 +0300

    Add hikari properties. Add hikariCP datasource. Update properties.

--- oppijanumerorekisteri-service/src/main/java/fi/vm/sade/oppijanumerorekisteri/configurations/db/JpaConfiguration.java
+++ oppijanumerorekisteri-service/src/main/java/fi/vm/sade/oppijanumerorekisteri/configurations/db/JpaConfiguration.java
@@ -1,15 +1,29 @@
 package fi.vm.sade.oppijanumerorekisteri.configurations.db;
 
+import com.zaxxer.hikari.HikariDataSource;
+import fi.vm.sade.oppijanumerorekisteri.configurations.properties.HikariProperties;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
 @Configuration
 @EnableJpaRepositories(basePackages = {"fi.vm.sade.oppijanumerorekisteri.repositories"})
-//@ComponentScan(basePackages = "fi.vm.sade.oppijanumerorekisteri.models")
 @EntityScan({"fi.vm.sade.oppijanumerorekisteri.models"})
 @EnableTransactionManagement
 public class JpaConfiguration {
+    private HikariProperties hikariProperties;
+
+    public JpaConfiguration(HikariProperties hikariProperties) {
+        this.hikariProperties = hikariProperties;
+    }
+
+    @Bean
+    public DataSource dataSource() throws SQLException {
+        return new HikariDataSource(hikariProperties);
+    }
 }
