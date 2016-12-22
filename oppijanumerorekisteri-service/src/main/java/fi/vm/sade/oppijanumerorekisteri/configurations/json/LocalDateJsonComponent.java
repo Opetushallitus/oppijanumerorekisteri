2048d462796e39c86e8d2878e35af02f3594e55a
Jarkko Pätäri <jarkko.patari@gofore.com>
2016-12-22 08:21:38 +0200

    KJHH-907: Improve LocalDate deserializer

--- oppijanumerorekisteri-service/src/main/java/fi/vm/sade/oppijanumerorekisteri/configurations/json/LocalDateJsonComponent.java
+++ oppijanumerorekisteri-service/src/main/java/fi/vm/sade/oppijanumerorekisteri/configurations/json/LocalDateJsonComponent.java
@@ -3,12 +3,15 @@ package fi.vm.sade.oppijanumerorekisteri.configurations.json;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.JsonToken;
 import com.fasterxml.jackson.databind.DeserializationContext;
-import com.fasterxml.jackson.databind.JsonDeserializer;
-import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
 import java.io.IOException;
 import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
 import org.springframework.boot.jackson.JsonComponent;
 
 /**
@@ -17,20 +20,41 @@ import org.springframework.boot.jackson.JsonComponent;
 @JsonComponent
 public class LocalDateJsonComponent {
 
-    public static class Serializer extends JsonSerializer<LocalDate> {
+    public static class Serializer extends StdSerializer<LocalDate> {
+
+        public Serializer() {
+            super(LocalDate.class);
+        }
 
         @Override
-        public void serialize(LocalDate value, JsonGenerator gen, SerializerProvider serializers) throws IOException, JsonProcessingException {
+        public void serialize(LocalDate value, JsonGenerator gen, SerializerProvider provider) throws IOException {
             gen.writeString(value.toString());
         }
 
     }
 
-    public static class Deserializer extends JsonDeserializer<LocalDate> {
+    public static class Deserializer extends StdDeserializer<LocalDate> {
+
+        public Deserializer() {
+            super(LocalDate.class);
+        }
 
         @Override
         public LocalDate deserialize(JsonParser p, DeserializationContext ctxt) throws IOException, JsonProcessingException {
-            return LocalDate.parse(p.getText());
+            if (!p.hasToken(JsonToken.VALUE_STRING)) {
+                throw ctxt.wrongTokenException(p, JsonToken.VALUE_STRING, null);
+            }
+            String string = p.getText().trim();
+            if (string.isEmpty()) {
+                return null;
+            }
+            try {
+                return LocalDate.parse(string);
+            } catch (DateTimeParseException e1) {
+                JsonMappingException e2 = ctxt.weirdStringException(string, handledType(), e1.getMessage());
+                e2.initCause(e1);
+                throw e2;
+            }
         }
 
     }
