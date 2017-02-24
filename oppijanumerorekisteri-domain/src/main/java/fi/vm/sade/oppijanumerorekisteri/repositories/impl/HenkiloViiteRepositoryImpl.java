2538a804039f78de6545f9c4955dd6d30023fdc2
Petri Ryhänen <petri.ryhanen@csc.fi>
2017-02-24 10:42:38 +0200

    Add check if results exist in henkilo table.

--- oppijanumerorekisteri-domain/src/main/java/fi/vm/sade/oppijanumerorekisteri/repositories/impl/HenkiloViiteRepositoryImpl.java
+++ oppijanumerorekisteri-domain/src/main/java/fi/vm/sade/oppijanumerorekisteri/repositories/impl/HenkiloViiteRepositoryImpl.java
@@ -1,19 +1,24 @@
 package fi.vm.sade.oppijanumerorekisteri.repositories.impl;
 
+import com.google.common.collect.Sets;
 import com.querydsl.core.types.Projections;
 import fi.vm.sade.oppijanumerorekisteri.dto.HenkiloViiteDto;
 import fi.vm.sade.oppijanumerorekisteri.repositories.criteria.HenkiloCriteria;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import fi.vm.sade.oppijanumerorekisteri.repositories.HenkiloViiteRepositoryCustom;
 import org.springframework.util.CollectionUtils;
 
 import static fi.vm.sade.oppijanumerorekisteri.models.QHenkiloViite.henkiloViite;
+import static fi.vm.sade.oppijanumerorekisteri.models.QHenkilo.henkilo;
 
 public class HenkiloViiteRepositoryImpl extends AbstractRepository implements HenkiloViiteRepositoryCustom {
 
     @Override
     public List<HenkiloViiteDto> findBy(HenkiloCriteria criteria) {
+        List<HenkiloViiteDto> result;
         if(!CollectionUtils.isEmpty(criteria.getHenkiloOids())) {
             // Find master oids
             List<String> masters = jpa().select(henkiloViite.masterOid)
@@ -22,7 +27,7 @@ public class HenkiloViiteRepositoryImpl extends AbstractRepository implements He
                             .or(henkiloViite.slaveOid.in(criteria.getHenkiloOids())))
                     .fetch();
             // Find all slaves for the master oids
-            return jpa().select(Projections.bean(HenkiloViiteDto.class,
+            result = jpa().select(Projections.bean(HenkiloViiteDto.class,
                     henkiloViite.masterOid.as("masterOid"),
                     henkiloViite.slaveOid.as("henkiloOid")))
                     .from(henkiloViite)
@@ -31,11 +36,22 @@ public class HenkiloViiteRepositoryImpl extends AbstractRepository implements He
         }
         else {
             // Find all
-            return jpa().select(Projections.bean(HenkiloViiteDto.class,
+            result = jpa().select(Projections.bean(HenkiloViiteDto.class,
                     henkiloViite.masterOid.as("masterOid"),
                     henkiloViite.slaveOid.as("henkiloOid")))
                     .from(henkiloViite)
                     .fetch();
         }
+
+        // Make sure all henkilos exist
+        List<String> existingHenkilos = jpa().select(henkilo.oidHenkilo)
+                .from(henkilo)
+                .where(henkilo.oidHenkilo.in(result.stream().flatMap(henkiloViiteDto ->
+                        Stream.of(henkiloViiteDto.getHenkiloOid(), henkiloViiteDto.getMasterOid())).collect(Collectors.toSet())))
+                .fetch();
+
+        return result.stream().filter(henkiloViiteDto ->
+                existingHenkilos.containsAll(Sets.newHashSet(henkiloViiteDto.getHenkiloOid(), henkiloViiteDto.getMasterOid())))
+                .collect(Collectors.toList());
     }
 }
