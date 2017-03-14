5521c8fced12e5a8f390d2a95945b67672388c7a
Jarkko Pätäri <jarkko.patari@gofore.com>
2017-03-14 09:47:06 +0200

    KJHH-944: Remove koodisto http cache

--- oppijanumerorekisteri-service/src/main/java/fi/vm/sade/oppijanumerorekisteri/clients/impl/KoodistoClientImpl.java
+++ oppijanumerorekisteri-service/src/main/java/fi/vm/sade/oppijanumerorekisteri/clients/impl/KoodistoClientImpl.java
@@ -3,14 +3,11 @@ package fi.vm.sade.oppijanumerorekisteri.clients.impl;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import fi.vm.sade.javautils.httpclient.ApacheOphHttpClient;
 import fi.vm.sade.javautils.httpclient.OphHttpClient;
-import fi.vm.sade.koodisto.service.types.common.KoodiCollectionType;
 import fi.vm.sade.koodisto.service.types.common.KoodiType;
-import fi.vm.sade.koodisto.service.types.common.KoodistoType;
 import fi.vm.sade.oppijanumerorekisteri.clients.KoodistoClient;
 import fi.vm.sade.oppijanumerorekisteri.configurations.properties.AuthenticationProperties;
 import fi.vm.sade.oppijanumerorekisteri.configurations.properties.UrlConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
@@ -22,23 +19,18 @@ import static fi.vm.sade.javautils.httpclient.OphHttpClient.JSON;
 @Component
 public class KoodistoClientImpl implements KoodistoClient {
     private final ObjectMapper objectMapper;
-    // Static so caching is used properly.
-    private final OphHttpClient cachingRestClient;
+    private final OphHttpClient httpClient;
 
     @Autowired
     public KoodistoClientImpl(ObjectMapper objectMapper, UrlConfiguration urlConfiguration,
                                   AuthenticationProperties authenticationProperties) {
         this.objectMapper = objectMapper;
-        ApacheOphHttpClient.ApacheHttpClientBuilder builder = ApacheOphHttpClient.createCustomBuilder()
-                .createCachingClient(50 * 1000, 10 * 1024 * 1024)
-                .setDefaultConfiguration(10000, 60);
-        this.cachingRestClient = new OphHttpClient(builder.build(), "oppijanumerorekisteri.oppijanumerorekisteri-service",
-                urlConfiguration);
+        this.httpClient = ApacheOphHttpClient.createDefaultOphHttpClient("oppijanumerorekisteri.oppijanumerorekisteri-service", urlConfiguration, 10000, 60);
     }
 
     @Override
     public List<KoodiType> getKoodisForKoodisto(String koodistoUri, int koodistoVersio, boolean onlyValidKoodis) {
-        KoodiType[] koodiCollectionType = this.cachingRestClient
+        KoodiType[] koodiCollectionType = this.httpClient
                 .get("koodisto-service.koodi", koodistoUri, Boolean.toString(onlyValidKoodis), Integer.toString(koodistoVersio))
                 .expectStatus(200).accept(JSON)
                 .execute(r -> objectMapper.readValue(r.asInputStream(), KoodiType[].class));
