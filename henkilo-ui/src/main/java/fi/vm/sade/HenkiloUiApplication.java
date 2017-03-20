19f8c0451f52ad3c5f70f187b161388674b9844b
Petri Ryhänen <petri.ryhanen@csc.fi>
2017-03-20 11:31:39 +0200

    Init spring-boot commit.

---
+++ henkilo-ui/src/main/java/fi/vm/sade/HenkiloUiApplication.java
@@ -0,0 +1,12 @@
+package fi.vm.sade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HenkiloUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HenkiloUiApplication.class, args);
+	}
+}
