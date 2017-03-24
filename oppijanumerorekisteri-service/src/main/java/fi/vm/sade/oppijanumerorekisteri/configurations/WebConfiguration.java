d426e97775dee233f0a69130e9e9263c2db65139
Petri Ryhänen <petri.ryhanen@csc.fi>
2017-03-24 14:12:18 +0200

    Add cors headers for localhost origin and with ports 3000 and 8280

--- oppijanumerorekisteri-service/src/main/java/fi/vm/sade/oppijanumerorekisteri/configurations/WebConfiguration.java
+++ oppijanumerorekisteri-service/src/main/java/fi/vm/sade/oppijanumerorekisteri/configurations/WebConfiguration.java
@@ -2,14 +2,34 @@ package fi.vm.sade.oppijanumerorekisteri.configurations;
 
 import org.joda.time.DateTime;
 import org.joda.time.LocalDate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.core.env.Environment;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+import java.util.Arrays;
+
 @Configuration
 public class WebConfiguration extends WebMvcConfigurerAdapter{
+    private final Environment environment;
+
+    @Autowired
+    public WebConfiguration(Environment environment) {
+        this.environment = environment;
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        if(Arrays.stream(this.environment.getActiveProfiles()).anyMatch(s -> s.equals("dev") || s.equals("luokka"))) {
+            registry.addMapping("/**")
+                    .allowedOrigins("http://localhost:3000", "http://localhost:8280");
+        }
+    }
+
     @Override
     public void configurePathMatch(PathMatchConfigurer configurer) {
         // Disable extensions causing trouble when OID is provided at the end of rest API.
