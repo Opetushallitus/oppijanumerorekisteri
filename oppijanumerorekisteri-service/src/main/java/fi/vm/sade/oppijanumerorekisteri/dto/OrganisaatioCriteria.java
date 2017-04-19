a4f768d82e4e654495b1fd2a54f78c16fac6d7b1
Jarkko Pätäri <jarkko.patari@gofore.com>
2017-04-19 15:20:49 +0300

    KJHH-977: Refactor

--- oppijanumerorekisteri-service/src/main/java/fi/vm/sade/oppijanumerorekisteri/dto/OrganisaatioCriteria.java
+++ oppijanumerorekisteri-service/src/main/java/fi/vm/sade/oppijanumerorekisteri/dto/OrganisaatioCriteria.java
@@ -1,6 +1,10 @@
 package fi.vm.sade.oppijanumerorekisteri.dto;
 
+import java.util.Collection;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Set;
+import static java.util.stream.Collectors.joining;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -17,5 +21,23 @@ public class OrganisaatioCriteria {
     private Boolean passivoitu;
     private Set<String> organisaatioOids;
     private Set<String> kayttoOikeusRyhmaNimet;
+
+    public Map<String, Object> getAsMap() {
+        Map<String, Object> map = new LinkedHashMap<>();
+        if (getPassivoitu() != null) {
+            map.put("passivoitu", getPassivoitu());
+        }
+        if (getOrganisaatioOids() != null) {
+            map.put("organisaatioOids", getAsCommaSeparated(getOrganisaatioOids()));
+        }
+        if (getKayttoOikeusRyhmaNimet() != null) {
+            map.put("kayttoOikeusRyhmaNimet", getAsCommaSeparated(getKayttoOikeusRyhmaNimet()));
+        }
+        return map;
+    }
+
+    private String getAsCommaSeparated(Collection<String> collection) {
+        return collection.stream().collect(joining(","));
+    }
 
 }
