1644354a32dae0edf3b0e48fc785dea0817e269a
Petri Ryhänen <petri.ryhanen@csc.fi>
2017-05-11 16:39:22 +0300

    Fix incorrect reference on passivoibutton. Fix some ExistingKayttooikeus table widths.

--- henkilo-ui/src/main/static/src/components/common/henkilo/buttons/PassivoiButton.js
+++ henkilo-ui/src/main/static/src/components/common/henkilo/buttons/PassivoiButton.js
@@ -4,7 +4,7 @@ import Button from "../../button/Button";
 import StaticUtils from "../../StaticUtils";
 
 const PassivoiButton = ({henkilo, passivoiAction, L}) => henkilo.henkilo.passivoitu
-    ? <Button key="passivoi" big disabled action={() => {}}>{this.L['PASSIVOI_PASSIVOITU']}</Button>
+    ? <Button key="passivoi" big disabled action={() => {}}>{L['PASSIVOI_PASSIVOITU']}</Button>
     : <ConfirmButton key="passivoi" big action={() => passivoiAction(henkilo.henkilo.oidHenkilo)}
                      normalLabel={L['PASSIVOI_LINKKI']} confirmLabel={L['PASSIVOI_LINKKI_CONFIRM']}
                      errorMessage={StaticUtils.createPopupErrorMessage('passivoi', henkilo, L)} />;
