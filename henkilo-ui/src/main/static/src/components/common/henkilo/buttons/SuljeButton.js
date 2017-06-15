67675a62217162f36796fd795d90dccad562f81f
Petri Ryhänen <petri.ryhanen@csc.fi>
2017-06-15 16:30:49 +0300

    Make SuljeButton gray.

--- henkilo-ui/src/main/static/src/components/common/henkilo/buttons/SuljeButton.js
+++ henkilo-ui/src/main/static/src/components/common/henkilo/buttons/SuljeButton.js
@@ -3,6 +3,7 @@ import ConfirmButton from "../../button/ConfirmButton";
 
 const SuljeButton = ({L, suljeAction}) =>
     <ConfirmButton action={suljeAction}
+                   cancel
                    normalLabel={L['HENKILO_KAYTTOOIKEUSANOMUS_SULJE']}
                    confirmLabel={L['HENKILO_KAYTTOOIKEUSANOMUS_SULJE_CONFIRM']}
                    key="sulje"
