02b19721407fc8d7d305b5ad872873fc5c76a3da
Petri Ryhänen <petri.ryhanen@csc.fi>
2017-07-04 10:06:25 +0300

    Add integration working integration test.

--- oppijanumerorekisteri-service/src/test/java/fi/vm/sade/oppijanumerorekisteri/services/impl/IdentificationServiceIntegrationTest.java
+++ oppijanumerorekisteri-service/src/test/java/fi/vm/sade/oppijanumerorekisteri/services/impl/IdentificationServiceIntegrationTest.java
@@ -1,93 +1,87 @@
 package fi.vm.sade.oppijanumerorekisteri.services.impl;
 
-import fi.vm.sade.oppijanumerorekisteri.TestApplication;
-import fi.vm.sade.oppijanumerorekisteri.configurations.H2Configuration;
-import fi.vm.sade.oppijanumerorekisteri.configurations.HibernateConfiguration;
-import fi.vm.sade.oppijanumerorekisteri.exceptions.NotFoundException;
-import fi.vm.sade.oppijanumerorekisteri.mappers.OrikaConfiguration;
+import fi.vm.sade.oppijanumerorekisteri.IntegrationTest;
+import fi.vm.sade.oppijanumerorekisteri.dto.HenkiloTyyppi;
 import fi.vm.sade.oppijanumerorekisteri.models.Henkilo;
-import fi.vm.sade.oppijanumerorekisteri.repositories.HenkiloJpaRepository;
-import fi.vm.sade.oppijanumerorekisteri.repositories.HenkiloRepository;
 import fi.vm.sade.oppijanumerorekisteri.services.IdentificationService;
-import fi.vm.sade.oppijanumerorekisteri.services.UserDetailsHelper;
 import fi.vm.sade.oppijanumerorekisteri.services.YksilointiService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.when;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Matchers.eq;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE, classes = {TestApplication.class, H2Configuration.class})
-//@Import(IdentificationServiceImpl.class)
+@IntegrationTest
 @Transactional
 public class IdentificationServiceIntegrationTest {
-    @MockBean
-    private HenkiloJpaRepository henkiloJpaRepository;
-    @MockBean
-    private HenkiloRepository henkiloRepository;
-    @MockBean
-    private UserDetailsHelper userDetailsHelper;
-    @MockBean
-    private OrikaConfiguration mapper;
     @MockBean
     private YksilointiService yksilointiService;
 
     @Autowired
-    private IdentificationServiceImpl identificationService;
+    private IdentificationService identificationService;
 
     @Autowired
     private EntityManager entityManager;
-
-//    @Before
-//    public void setup() {
-//        identificationService = new IdentificationServiceImpl(henkiloRepository, henkiloJpaRepository,
-//                userDetailsHelper, mapper, yksilointiService);
-//    }
 
     @Test
     public void identifyHenkilos() {
         Henkilo henkiloWithFakeSSN = Henkilo.builder()
                 .hetu("111111-985K")
                 .oidHenkilo("FakeSSN")
+                .created(new Date())
+                .modified(new Date())
+                .henkiloTyyppi(HenkiloTyyppi.VIRKAILIJA)
                 .build();
-//        this.entityManager.persist(henkiloWithFakeSSN);
+        this.entityManager.persist(henkiloWithFakeSSN);
 
         Henkilo henkiloInBlacklist = Henkilo.builder()
                 .hetu("111111-1234")
                 .oidHenkilo("Blacklisted")
+                .created(new Date())
+                .modified(new Date())
+                .henkiloTyyppi(HenkiloTyyppi.VIRKAILIJA)
                 .eiYksiloida(true)
                 .build();
-//        this.entityManager.persist(henkiloInBlacklist);
+        this.entityManager.persist(henkiloInBlacklist);
 
         Henkilo henkiloNotFoundInVTJ = Henkilo.builder()
-                .hetu("111111-1234")
+                .hetu("111111-1235")
                 .oidHenkilo("NotInVTJ")
+                .created(new Date())
+                .modified(new Date())
+                .henkiloTyyppi(HenkiloTyyppi.VIRKAILIJA)
                 .build();
-//        this.entityManager.persist(henkiloNotFoundInVTJ);
+        this.entityManager.persist(henkiloNotFoundInVTJ);
 
         Henkilo henkiloEverythingOK = Henkilo.builder()
-                .hetu("111111-1234")
+                .hetu("111111-1236")
                 .oidHenkilo("EverythingOK")
+                .created(new Date())
+                .modified(new Date())
+                .henkiloTyyppi(HenkiloTyyppi.VIRKAILIJA)
                 .yksiloityVTJ(true)
                 .build();
-//        this.entityManager.persist(henkiloEverythingOK);
+        this.entityManager.persist(henkiloEverythingOK);
 
         List<Henkilo> unidentifiedHenkilos = Arrays.asList(henkiloWithFakeSSN, henkiloInBlacklist, henkiloNotFoundInVTJ, henkiloEverythingOK);
 
 //        when(henkiloRepository.save(any(Henkilo.class))).then(AdditionalAnswers.returnsFirstArg());
-        when(yksilointiService.yksiloiManuaalisesti(henkiloNotFoundInVTJ.getOidHenkilo())).thenThrow(new NotFoundException());
-        when(yksilointiService.yksiloiManuaalisesti(henkiloEverythingOK.getOidHenkilo())).thenReturn(henkiloEverythingOK);
+        given(yksilointiService.yksiloiAutomaattisesti(henkiloNotFoundInVTJ.getOidHenkilo()))
+                .willReturn(Optional.empty());
+        given(this.yksilointiService.yksiloiAutomaattisesti(eq(henkiloEverythingOK.getOidHenkilo())))
+                .willReturn(Optional.of(henkiloEverythingOK));
 
         identificationService.identifyHenkilos(unidentifiedHenkilos, 0L);
 
