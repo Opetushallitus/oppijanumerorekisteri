6a7fda5e97dfa688b790d502c1a89d83f8b8ae0b
Petri Ryhänen <petri.ryhanen@csc.fi>
2017-07-27 10:38:04 +0300

    Add Perustiedot to /rekisteroidy page.

--- henkilo-ui/src/main/static/src/components/rekisterointi/RekisteroidyContainer.js
+++ henkilo-ui/src/main/static/src/components/rekisterointi/RekisteroidyContainer.js
@@ -3,19 +3,32 @@ import {connect} from 'react-redux'
 import RekisteroidyPage from "./RekisteroidyPage";
 import {updateNavigation} from "../../actions/navigation.actions";
 import {emptyNavi} from "../../configuration/navigationconfigurations";
+import {fetchKieliKoodisto} from "../../actions/koodisto.actions";
+import Loader from "../common/icons/Loader";
 
 class RekisteroidyContainer extends React.Component {
     componentWillMount() {
         this.props.updateNavigation(emptyNavi);
+
+        this.props.fetchKieliKoodisto();
     }
 
     render() {
-        return <RekisteroidyPage />
+        return <div>
+            {
+                this.props.koodistoLoading
+                    ? <Loader />
+                    : <RekisteroidyPage {...this.props} />
+            }
+        </div>
     }
 }
 
-const mapStateToProps = () => ({
-
+const mapStateToProps = (state, ownProps) => ({
+    locale: state.locale,
+    L: state.l10n.localisations[state.locale],
+    koodistoLoading: state.koodisto.kieliKoodistoLoading,
+    koodisto: state.koodisto,
 });
 
-export default connect(mapStateToProps, {updateNavigation})(RekisteroidyContainer);
+export default connect(mapStateToProps, {updateNavigation, fetchKieliKoodisto})(RekisteroidyContainer);
--- henkilo-ui/src/main/static/src/components/rekisterointi/RekisteroidyPage.js
+++ henkilo-ui/src/main/static/src/components/rekisterointi/RekisteroidyPage.js
@@ -1,10 +1,48 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import Columns from 'react-columns'
+import RekisteroidyPerustiedot from './content/RekisteroidyPerustiedot'
+import Asiointikieli from "../common/henkilo/labelvalues/Asiointikieli";
 
 class RekisteroidyPage extends React.Component {
+    static propTypes = {
+        koodisto: PropTypes.shape({
+            kieli: PropTypes.array.isRequired,
+        }).isRequired,
+    };
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            henkilo: {
+                henkilo: {
+                    etunimet: '',
+                    sukunimi: '',
+                    kutsumanimi: '',
+                },
+                kayttajatieto: {
+                    username: '',
+                    password: '',
+                    passwordAgain: '',
+                },
+            },
+        };
+    }
+
     render() {
         return <div className="borderless-wrapper">
             <span className="oph-h2 oph-bold">Rekisteröidy</span>
-        </div>
+            <Columns columns={2} gap="25px">
+                <div>
+                    <RekisteroidyPerustiedot henkilo={this.state.henkilo} />
+                </div>
+                <div>
+
+                </div>
+            </Columns>
+            <Asiointikieli henkilo={this.state.henkilo} koodisto={this.props.koodisto} henkiloUpdate={{}} />
+        </div>;
     }
 }
 
