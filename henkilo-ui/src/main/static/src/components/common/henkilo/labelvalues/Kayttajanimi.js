4a0f9ff354e2457556ae3c30cfd32a15952d44bf
Tapio Jaakkola <tapio.jaakkola@github.mail.kapsi.fi>
2017-09-26 13:03:05 +0300

    Fix error thrown if user doesn't have kayttajanimi

--- henkilo-ui/src/main/static/src/components/common/henkilo/labelvalues/Kayttajanimi.js
+++ henkilo-ui/src/main/static/src/components/common/henkilo/labelvalues/Kayttajanimi.js
@@ -1,19 +1,20 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import LabelValue from "./LabelValue";
+import R from 'ramda';
 
-const Kayttajanimi = (props) => <LabelValue {...props} values={{
+const Kayttajanimi = (props) => R.path(['henkilo', 'kayttajatieto', 'username'], props) ? <LabelValue {...props} values={{
     label: 'HENKILO_KAYTTAJANIMI',
     value: props.henkilo.kayttajatieto.username,
     inputValue: 'kayttajanimi',
     disabled: props.disabled,
     isError: props.isError,
-}} />;
+}} /> : null;
 
 Kayttajanimi.propTypes = {
     henkilo: PropTypes.shape({
         kayttajatieto: PropTypes.shape({
-            username: PropTypes.string.isRequired,
+            username: PropTypes.string,
         }).isRequired,
     }).isRequired,
     disabled: PropTypes.bool.isRequired,
