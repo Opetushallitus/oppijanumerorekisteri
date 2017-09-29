667913bf6af6419bce3fbac65703b6eb3cb6a5dd
Jarkko Pätäri <jarkko.patari@gofore.com>
2017-09-29 08:41:04 +0300

    Move swagger configuration to its own file

--- henkilo-ui/src/main/java/fi/vm/sade/henkiloui/configurations/WebConfiguration.java
+++ henkilo-ui/src/main/java/fi/vm/sade/henkiloui/configurations/WebConfiguration.java
@@ -3,13 +3,10 @@ package fi.vm.sade.henkiloui.configurations;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableSwagger2
 public class WebConfiguration extends WebMvcConfigurerAdapter {
 
     @Override
