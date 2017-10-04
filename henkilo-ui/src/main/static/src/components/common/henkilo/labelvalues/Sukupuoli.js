031716d972c33dc602eeeba6cbd2846507ed77e1
Jarkko Pätäri <jarkko.patari@gofore.com>
2017-10-04 16:24:38 +0300

    KJHH-1094: Syntymäaika ja sukupuoli disabloidaan jos hetu annettu

--- henkilo-ui/src/main/static/src/components/common/henkilo/labelvalues/Sukupuoli.js
+++ henkilo-ui/src/main/static/src/components/common/henkilo/labelvalues/Sukupuoli.js
@@ -1,18 +1,18 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import LabelValue from "./LabelValue"
-import StaticUtils from "../../StaticUtils";
 
 const Sukupuoli = (props) => <LabelValue {...props} values={{
     label: 'HENKILO_SUKUPUOLI',
     value: props.henkilo.henkilo.sukupuoli,
     inputValue: 'sukupuoli',
-    disabled: StaticUtils.hasHetuAndIsYksiloity(props.henkilo)
+    disabled: !!props.henkilo.henkilo.hetu,
 }} />;
 
 Sukupuoli.propTypes = {
     henkilo: PropTypes.shape({henkilo: PropTypes.shape({
         sukupuoli: PropTypes.string,
+        hetu: PropTypes.string,
     })}),
 };
 
