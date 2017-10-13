eb8c21dce6cb3308ec2763f4c340a3e62ac4ea1f
Tapio Jaakkola <tapio.jaakkola@github.mail.kapsi.fi>
2017-10-13 14:52:14 +0300

    Add set locale type in locale reducer

--- henkilo-ui/src/main/static/src/reducers/locale.reducer.js
+++ henkilo-ui/src/main/static/src/reducers/locale.reducer.js
@@ -1,8 +1,9 @@
 import {FETCH_OMATTIEDOT_SUCCESS} from "../actions/actiontypes";
+import type {Locale} from "../types/locale.type";
 
 type Action = { lang: string };
 
-export const locale = (state = 'fi', action: Action): string => {
+export const locale = (state : Locale = 'fi', action: Action): Locale => {
     switch (action.type) {
         case FETCH_OMATTIEDOT_SUCCESS:
             return action.lang || 'fi';
