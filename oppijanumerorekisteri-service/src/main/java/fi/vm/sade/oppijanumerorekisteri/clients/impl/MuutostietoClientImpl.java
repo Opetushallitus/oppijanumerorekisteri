6761a1fd0d57d348ecd682844f132f4b5571b5d2
Wille Keltikangas <wille.keltikangas@gofore.com>
2018-01-01 23:23:17 +0200

    Muutostietopalvelu: Hetu sync

---
+++ oppijanumerorekisteri-service/src/main/java/fi/vm/sade/oppijanumerorekisteri/clients/impl/MuutostietoClientImpl.java
@@ -0,0 +1,82 @@
+package fi.vm.sade.oppijanumerorekisteri.clients.impl;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import fi.vm.sade.javautils.http.OphHttpClient;
+import fi.vm.sade.javautils.http.OphHttpEntity;
+import fi.vm.sade.javautils.http.OphHttpRequest;
+import fi.vm.sade.javautils.http.OphHttpResponse;
+import fi.vm.sade.javautils.http.auth.CasAuthenticator;
+import fi.vm.sade.oppijanumerorekisteri.clients.MuutostietoClient;
+import fi.vm.sade.oppijanumerorekisteri.configurations.properties.AuthenticationProperties;
+import fi.vm.sade.oppijanumerorekisteri.configurations.properties.UrlConfiguration;
+import fi.vm.sade.oppijanumerorekisteri.dto.MuutostietoHetus;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Collections;
+import java.util.List;
+
+@Component
+@Slf4j
+public class MuutostietoClientImpl implements MuutostietoClient {
+
+    private UrlConfiguration urlConfiguration;
+    private AuthenticationProperties authenticationProperties;
+    private OphHttpClient httpClient;
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Autowired
+    public MuutostietoClientImpl(UrlConfiguration urlConfiguration, AuthenticationProperties authenticationProperties) {
+        this.urlConfiguration = urlConfiguration;
+        this.authenticationProperties = authenticationProperties;
+        this.httpClient = createClient();
+    }
+
+    @Override
+    public List<String> sendHetus(MuutostietoHetus hetus) {
+        OphHttpEntity entity = new OphHttpEntity.Builder()
+                .content(serializeHetus(hetus))
+                .build();
+
+        OphHttpResponse response = httpClient.execute(OphHttpRequest.Builder
+                .post(urlConfiguration.url("henkilotietomuutos-service.vtj.hetut"))
+                .setEntity(entity).build());
+
+        if (response.getStatusCode() == 200) {
+            return deserializeHetus(response);
+        }
+        return Collections.emptyList();
+    }
+
+    private String serializeHetus(MuutostietoHetus hetus) {
+        try {
+            return objectMapper.writeValueAsString(hetus);
+        } catch (JsonProcessingException e) {
+            throw new IllegalArgumentException("Could not serialize muutostietohetus: ", e);
+        }
+    }
+
+    private List<String> deserializeHetus(OphHttpResponse response) {
+        try (InputStream is = response.asInputStream()) {
+            return objectMapper.readValue(is, List.class);
+        } catch (IOException e) {
+            log.error("Failed to read response: {}", e);
+            return Collections.emptyList();
+        }
+    }
+
+    private OphHttpClient createClient() {
+        CasAuthenticator authenticator = new CasAuthenticator.Builder()
+                .username(authenticationProperties.getHenkilotietomuutos().getUsername())
+                .password(authenticationProperties.getHenkilotietomuutos().getPassword())
+                .webCasUrl(urlConfiguration.url("cas.url"))
+                .casServiceUrl(urlConfiguration.url("henkilotietomuutos-service.security-check"))
+                .build();
+
+        return new OphHttpClient.Builder().authenticator(authenticator).build();
+    }
+}
--- oppijanumerorekisteri-service/src/main/java/fi/vm/sade/oppijanumerorekisteri/configurations/scheduling/ScheduledTasks.java
+++ oppijanumerorekisteri-service/src/main/java/fi/vm/sade/oppijanumerorekisteri/configurations/scheduling/ScheduledTasks.java
@@ -1,17 +1,19 @@
 package fi.vm.sade.oppijanumerorekisteri.configurations.scheduling;
 
+import fi.vm.sade.oppijanumerorekisteri.clients.MuutostietoClient;
 import fi.vm.sade.oppijanumerorekisteri.configurations.properties.OppijanumerorekisteriProperties;
+import fi.vm.sade.oppijanumerorekisteri.dto.MuutostietoHetus;
 import fi.vm.sade.oppijanumerorekisteri.models.Henkilo;
 import fi.vm.sade.oppijanumerorekisteri.repositories.HenkiloJpaRepository;
+import fi.vm.sade.oppijanumerorekisteri.repositories.HenkiloRepository;
 import fi.vm.sade.oppijanumerorekisteri.services.IdentificationService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Collection;
+import java.util.*;
 
 /**
  * Ajastusten konfigurointi.
@@ -28,6 +30,10 @@ public class ScheduledTasks {
     private final IdentificationService identificationService;
 
     private final HenkiloJpaRepository henkiloJpaRepository;
+
+    private final HenkiloRepository henkiloRepository;
+
+    private final MuutostietoClient muutostietoClient;
 
     @Autowired
     public void setProperties(OppijanumerorekisteriProperties properties) {
@@ -54,4 +60,27 @@ public class ScheduledTasks {
 
     }
 
+    @Scheduled(fixedDelayString = "${oppijanumerorekisteri.scheduling.vtjsync.fixed-delay-in-millis}")
+    public void startHetuSync() {
+        if (properties.getEnabled()) {
+            log.info("Started syncing hetus to VTJ...");
+            long start = System.currentTimeMillis();
+
+            List<String> addedHetus = henkiloJpaRepository.findHetusMissingFromVTJRegister();
+            List<String> removedHetus = new ArrayList<>();
+
+            MuutostietoHetus hetus = MuutostietoHetus.builder()
+                    .addedHetus(addedHetus)
+                    .removedHetus(removedHetus)
+                    .build();
+            List<String> syncedHetus = muutostietoClient.sendHetus(hetus);
+
+            for (String hetu : syncedHetus) {
+                henkiloJpaRepository.addHetuToVTJRegister(hetu);
+            }
+
+            long duration = System.currentTimeMillis() - start;
+            log.info("Hetu sync completed, duration: " + duration + "ms");
+        }
+    }
 }
