b882c958a88d324443fd80b1f9b4ea2454526771
Jarkko Pätäri <jarkko.patari@gofore.com>
2018-01-16 12:18:03 +0200

    KJHH-1087: Muuttaa /vahvatunnistusinfo/lisatiedot julkiseksi

--- henkilo-ui/src/main/java/fi/vm/sade/henkiloui/configurations/security/SecurityConfiguration.java
+++ henkilo-ui/src/main/java/fi/vm/sade/henkiloui/configurations/security/SecurityConfiguration.java
@@ -133,6 +133,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                     .antMatchers("/l10n").permitAll()
                     .antMatchers("/vahvatunnistusinfo/*/*").permitAll()
                     .antMatchers("/vahvatunnistusinfo/virhe/*/*").permitAll()
+                    .antMatchers("/vahvatunnistusinfo/lisatiedot/**").permitAll()
                     .antMatchers("/rekisteroidy").permitAll()
                     .antMatchers("/salasananresetointi/*/*").permitAll()
                     // Admin domain
