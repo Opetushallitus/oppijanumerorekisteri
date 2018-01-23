8616d01e4fe8b56af0a21534b4c12d7050e7660b
Tapio Jaakkola <tapio.jaakkola@github.mail.kapsi.fi>
2018-01-23 11:11:16 +0200

    KJHH-1213: Add test

--- oppijanumerorekisteri-service/src/test/java/fi/vm/sade/oppijanumerorekisteri/services/HenkiloServiceTest.java
+++ oppijanumerorekisteri-service/src/test/java/fi/vm/sade/oppijanumerorekisteri/services/HenkiloServiceTest.java
@@ -1,6 +1,7 @@
 package fi.vm.sade.oppijanumerorekisteri.services;
 
 
+import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
 import com.querydsl.core.types.Predicate;
 import fi.vm.sade.kayttooikeus.dto.KayttooikeudetDto;
@@ -45,6 +46,7 @@ import static java.util.Collections.emptyList;
 import static java.util.Collections.emptySet;
 
 import static java.util.Collections.singletonList;
+import static java.util.stream.Collectors.toList;
 import static java.util.stream.Collectors.toSet;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.tuple;
@@ -633,5 +635,45 @@ public class HenkiloServiceTest {
         Henkilo saved = argumentCaptor.getValue();
         assertThat(saved.getPassinumerot()).containsExactly("passi123");
     }
+
+    @Test
+    public void getHenkiloDuplicateDtoListShouldReturnApplicationsFromAtaruAndHakuApp() {
+        Henkilo henkilo1 = EntityUtils.createHenkilo("arpa noppa", "arpa", "kuutio", "hetu", "1.2.3.4.5", false, HenkiloTyyppi.OPPIJA, "fi", "FI", "2.3.34.5", new Date(), new Date(), "2.3.34.5", "arvo");
+        Henkilo henkilo2 = EntityUtils.createHenkilo("arpa2 noppa2", "arpa2", "kuutio2", "hetu2", "1.2.3.4.6", false, HenkiloTyyppi.OPPIJA, "fi", "FI", "2.3.34.5", new Date(), new Date(), "2.3.34.5", "arvo");
+        Henkilo henkilo3 = EntityUtils.createHenkilo("arpa3 noppa3", "arpa3", "kuutio3", "hetu3", "1.3.3.4.7", false, HenkiloTyyppi.OPPIJA, "fi", "FI", "2.3.34.56", new Date(), new Date(), "2.3.34.5", "arvo");
+        List<Henkilo> henkilos = Arrays.asList(henkilo1, henkilo2, henkilo3);
+        given(this.userDetailsHelperMock.getCurrentUserOid()).willReturn("1.2.3.4.5");
+        HashMap<String, Object> hakemus1 = new HashMap<>();
+        hakemus1.put("service", "ataru");
+        HashMap<String, Object> hakemus2 = new HashMap<>();
+        hakemus2.put("service", "haku-app");
+
+        HakemusDto hakemusDto1 = new HakemusDto(hakemus1);
+        HakemusDto hakemusDto2 = new HakemusDto(hakemus2);
+
+        HashMap<String, List<HakemusDto>> ataruApplications = new HashMap<String, List<HakemusDto>>() { { put("1.2.3.4.6", Arrays.asList(hakemusDto1)); }; };
+        HashMap<String, List<HakemusDto>> hakuAppApplications = new HashMap<String, List<HakemusDto>>() { { put("1.3.3.4.7", Arrays.asList(hakemusDto2)); put("1.2.3.4.6", Arrays.asList(hakemusDto2)); }; };
+
+        hakuAppApplications.put("1.3.3.4.7", Arrays.asList(hakemusDto2));
+
+        given(this.ataruClient.fetchApplicationsByOid(any())).willReturn(ataruApplications);
+        given(this.hakuappClient.fetchApplicationsByOid(any())).willReturn(hakuAppApplications);
+
+        List<HenkiloDuplicateDto> duplicates = ReflectionTestUtils.invokeMethod(service, "getHenkiloDuplicateDtoList", henkilos);
+
+        assertThat(duplicates.size()).isEqualTo(2); // filter current user
+        HenkiloDuplicateDto henkiloDuplicateDto2 = duplicates.get(0);
+        assertThat(henkiloDuplicateDto2.getHakemukset().size()).isEqualTo(2);
+        assertThat(henkiloDuplicateDto2.getHakemukset()).contains(hakemusDto1);
+        assertThat(henkiloDuplicateDto2.getHakemukset()).contains(hakemusDto2);
+
+        HenkiloDuplicateDto henkiloDuplicateDto3 = duplicates.get(1);
+        assertThat(henkiloDuplicateDto3.getHakemukset().size()).isEqualTo(1);
+        assertThat(henkiloDuplicateDto3.getHakemukset()).contains(hakemusDto2);
+
+
+    }
+
+
 
 }
