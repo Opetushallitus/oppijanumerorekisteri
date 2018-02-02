6c2e363980a10f9a6442cdaefe221fbd4a7b450c
Petri Ryhänen <petri.ryhanen@csc.fi>
2018-02-02 11:01:50 +0200

    KJHH-1285: Refactor.

--- oppijanumerorekisteri-service/src/main/java/fi/vm/sade/oppijanumerorekisteri/validators/KutsumanimiValidator.java
+++ oppijanumerorekisteri-service/src/main/java/fi/vm/sade/oppijanumerorekisteri/validators/KutsumanimiValidator.java
@@ -8,49 +8,49 @@ import static java.util.stream.Collectors.toList;
 
 public final class KutsumanimiValidator {
 
-    private final String etunimet;
+    private final List<String> basicDataSeparatedByWhitespace;
+    private final List<String> basicDataSeparatedByWhitespaceAndDashes;
 
     public KutsumanimiValidator(String etunimet) {
-        this.etunimet = etunimet.toLowerCase();
+        String etunimetLowercase = etunimet.toLowerCase();
+        // Basic set
+        this.basicDataSeparatedByWhitespace = Arrays.stream(etunimetLowercase.split(" "))
+                .map(String::trim)
+                .collect(toList());
+        // Basic set separated by dashes and spaces
+        this.basicDataSeparatedByWhitespaceAndDashes = Arrays.stream(etunimetLowercase.split("[ \\-]"))
+                .map(String::trim)
+                .collect(toList());
+
     }
 
     public boolean isValid(String kutsumanimi) {
-        String kutsumanimiLowerCase = kutsumanimi.toLowerCase();
-
-        // Basic set
-        List<String> separatedByWhitespace = Arrays.stream(this.etunimet.split(" "))
-                .map(String::trim)
-                .collect(toList());
-
         // e.g. "arpa-tupla noppa kuutio" => "arpa-tupla" "noppa" "kuutio" "arpa-tupla noppa" "noppa kuutio" "arpa-tupla noppa kuutio"
-        Set<String> subsequentPairs = separatedByWhitespace.stream()
-                .flatMap(etunimi -> this.findSequentialTuples(separatedByWhitespace, etunimi))
+        Set<String> subsequentTuples = this.basicDataSeparatedByWhitespace.stream()
+                .flatMap(etunimi -> this.findSequentialTuples(this.basicDataSeparatedByWhitespace, etunimi))
                 .collect(Collectors.toSet());
 
-        // Basic set separated by dashes and spaces
-        List<String> withoutDashes = Arrays.stream(this.etunimet.split("[ \\-]"))
-                .map(String::trim)
-                .collect(toList());
-        // e.g. "arpa-tupla noppa kuutio" "arpa tupla noppa" "noppa kuutio" "arpa tupla noppa kuutio"
-        Set<String> subsequentWithoutDash = withoutDashes.stream()
-                .flatMap(etunimi -> this.findSequentialTuples(withoutDashes, etunimi))
+        // e.g. "arpa-tupla noppa kuutio" => "arpa" "tupla" "noppa" "kuutio" "arpa tupla noppa" "noppa kuutio" "arpa tupla noppa kuutio"
+        Set<String> subsequentTuplesWithoutDash = this.basicDataSeparatedByWhitespaceAndDashes.stream()
+                .flatMap(etunimi -> this.findSequentialTuples(this.basicDataSeparatedByWhitespaceAndDashes, etunimi))
                 .collect(Collectors.toSet());
+
         // Compare all valid cases
-        return Stream.of(subsequentPairs, subsequentWithoutDash)
+        return Stream.of(subsequentTuples, subsequentTuplesWithoutDash)
                 .flatMap(Collection::stream)
                 .map(String::toLowerCase)
                 .distinct()
-                .anyMatch(etunimi -> etunimi.equals(kutsumanimiLowerCase));
+                .anyMatch(etunimi -> etunimi.equals(kutsumanimi.toLowerCase()));
     }
 
-    private Stream<? extends String> findSequentialTuples(List<String> separatedByWhitespace, String etunimi) {
-        int currentIndex = separatedByWhitespace.indexOf(etunimi);
-        int finalIndex = separatedByWhitespace.size();
+    private Stream<? extends String> findSequentialTuples(List<String> basicDataSet, String etunimi) {
+        int currentIndex = basicDataSet.indexOf(etunimi);
+        int finalIndex = basicDataSet.size();
         Set<String> set = new HashSet<>();
         // Go through sequential tuples with all valid tuple sizes
         for (int tupleSize = 1; tupleSize <= finalIndex; tupleSize++) {
             for (int i = currentIndex; i <= finalIndex-tupleSize; i++) {
-                String flatSet = separatedByWhitespace.subList(i, i + tupleSize).stream()
+                String flatSet = basicDataSet.subList(i, i + tupleSize).stream()
                         .reduce((currentEtunimi, nextEtunimi) -> currentEtunimi + " " + nextEtunimi)
                         .orElseThrow(RuntimeException::new);
                 set.add(flatSet);
