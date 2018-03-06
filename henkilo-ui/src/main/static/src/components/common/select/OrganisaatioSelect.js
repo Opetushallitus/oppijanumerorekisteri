982763d817aee871069b148c7c088122c6d7e856
Tapio Jaakkola <tapio.jaakkola@github.mail.kapsi.fi>
2018-03-06 14:55:07 +0200

    KJHH-1246: Refactoring

--- henkilo-ui/src/main/static/src/components/common/select/OrganisaatioSelect.js
+++ henkilo-ui/src/main/static/src/components/common/select/OrganisaatioSelect.js
@@ -55,39 +55,16 @@ export class OrganisaatioSelect extends React.Component<Props, State> {
 
     _renderRow = (renderParams: any) => {
         const organisaatio: OrganisaatioSelectObject = this.state.organisaatiot[renderParams.index];
-        return <div className="organisaatio" onClick={this.makeSelection.bind(this, organisaatio)} style={renderParams.style} key={organisaatio.oid}>
-                {this._renderParents(organisaatio)}
-                {this._renderOrganisaatioNimi(organisaatio)}
+        return <div className="organisaatio" onClick={this.makeSelection.bind(this, organisaatio)}
+                    style={renderParams.style} key={organisaatio.oid}>
+            {this._renderParents(organisaatio)}
+            {this._renderOrganisaatioNimi(organisaatio)}
         </div>;
     };
 
     makeSelection = (organisaatio: OrganisaatioSelectObject): void => {
         this.props.onSelect(organisaatio);
     };
-
-    onFilter(event: SyntheticEvent<HTMLInputElement>) {
-        const currentSearchWord: string = event.currentTarget.value;
-        const previousSearchWord = this.state.searchWord;
-
-        if (previousSearchWord.length > currentSearchWord.length) {
-
-            if (this.state.allOrganisaatiot.length !== this.state.organisaatiot.length) {
-                const filteredOrganisaatiot = this._filterOrganisaatioList(this.state.allOrganisaatiot, currentSearchWord);
-                const organisaatiot = this._sortBySearchWord(filteredOrganisaatiot, currentSearchWord);
-                this.setState({searchWord: currentSearchWord, organisaatiot: organisaatiot});
-            } else {
-                this.setState({searchWord: currentSearchWord});
-            }
-        } else {
-            // optimize filtering if the new search searchword starts with the previous
-            const filterCurrentList: boolean = currentSearchWord.startsWith(previousSearchWord);
-            const filteredOrganisaatiot = filterCurrentList ?
-                this._filterOrganisaatioList(this.state.organisaatiot, currentSearchWord) :
-                this._filterOrganisaatioList(this.state.allOrganisaatiot, currentSearchWord);
-            const organisaatiot = this._sortBySearchWord(filteredOrganisaatiot, currentSearchWord);
-            this.setState({searchWord: currentSearchWord, organisaatiot});
-        }
-    }
 
     _renderOrganisaatioNimi = (organisaatio: OrganisaatioSelectObject) => {
         return <div
@@ -103,25 +80,53 @@ export class OrganisaatioSelect extends React.Component<Props, State> {
             <span key={index} className="parent">{parent} > </span>);
     };
 
-    _filterOrganisaatioList(organisaatiot: Array<OrganisaatioSelectObject>, searchWord: string): Array<OrganisaatioSelectObject> {
-        return organisaatiot.filter((organisaatio: OrganisaatioSelectObject) => organisaatio.name.toLowerCase().includes(searchWord.toLowerCase()));
+    onFilter(event: SyntheticEvent<HTMLInputElement>) {
+        const currentSearchWord: string = event.currentTarget.value;
+        const previousSearchWord = this.state.searchWord;
+
+        if (previousSearchWord.length > currentSearchWord.length) {
+            if (this.state.allOrganisaatiot.length !== this.state.organisaatiot.length) {
+                const organisaatiot = this._filterAndSortOrganisaatios(this.state.allOrganisaatiot, currentSearchWord);
+                this.setState({searchWord: currentSearchWord, organisaatiot});
+            } else {
+                this.setState({searchWord: currentSearchWord});
+            }
+        } else {
+            // optimize filtering if the new search searchword starts with the previous
+            const organisaatiot = this._filterAndSortOrganisaatios(this.state.organisaatiot, currentSearchWord);
+            this.setState({searchWord: currentSearchWord, organisaatiot});
+        }
     }
+
+    _filterAndSortOrganisaatios(organisaatiot: Array<OrganisaatioSelectObject>, searchWord: string): Array<OrganisaatioSelectObject> {
+        const containsSearchword = (organisaatio: OrganisaatioSelectObject) => organisaatio.name.toLowerCase().includes(searchWord.toLowerCase());
+        const startsWithSearchWord = (organisaatio: OrganisaatioSelectObject) => organisaatio.name.toLowerCase().startsWith(searchWord.toLowerCase());
+        const notStartingWithSearchWord = (organisaatio: OrganisaatioSelectObject) => !organisaatio.name.toLowerCase().startsWith(searchWord.toLowerCase());
+
+        const organisaatioFilteredBySearchword = this._organisaatiotFilteredBy(organisaatiot, containsSearchword);
+        const organisaatiotStartingWithSearchword = this._organisaatiotFilteredBy(organisaatioFilteredBySearchword, startsWithSearchWord);
+        const organisaatiotStartingWithSearchwordSortedByParentName = this._sortOrganisaatiotByParentName(organisaatiotStartingWithSearchword);
+        const organisaatiotNotStartingWithSearchword = this._organisaatiotFilteredBy(organisaatioFilteredBySearchword, notStartingWithSearchWord);
+
+        return [...organisaatiotStartingWithSearchwordSortedByParentName, ...organisaatiotNotStartingWithSearchword];
+    };
 
     _sortAlphabetically(organisaatiot: Array<OrganisaatioSelectObject>): Array<OrganisaatioSelectObject> {
         return R.sortBy(R.compose(R.toLower, R.prop('name')))(organisaatiot);
     }
 
-    _sortBySearchWord(organisaatiot: Array<OrganisaatioSelectObject>, searchWord: string): Array<OrganisaatioSelectObject> {
-        const organisaatiotStartingWithSearchword = organisaatiot.filter((organisaatio: OrganisaatioSelectObject) => organisaatio.name.toLowerCase().startsWith(searchWord.toLowerCase()));
-        const organisaatiotStartingWithSearchwordSortedByParentName = this._sortOrganisaatiotByParentName(organisaatiotStartingWithSearchword);
-        const others = organisaatiot.filter((organisaatio: OrganisaatioSelectObject) => !organisaatio.name.toLowerCase().startsWith(searchWord.toLowerCase()));
-        return [...organisaatiotStartingWithSearchwordSortedByParentName, ...others];
+    _organisaatiotFilteredBy(organisaatiot: Array<OrganisaatioSelectObject>, func: (organisaatio: OrganisaatioSelectObject) => boolean): Array<OrganisaatioSelectObject> {
+        return organisaatiot.filter(func);
     }
 
     _sortOrganisaatiotByParentName(organisaatiot: Array<any>): Array<OrganisaatioSelectObject> {
-        const hasParent = organisaatiot.filter( (organisaatio: OrganisaatioSelectObject) => organisaatio.parentNames && organisaatio.parentNames.length > 0);
-        const noParent = organisaatiot.filter( (organisaatio: OrganisaatioSelectObject) => !organisaatio.parentNames || organisaatio.parentNames.length === 0);
-        return [...noParent, ...R.sortBy(R.compose(R.toLower, R.head, R.prop('parentNames')))(hasParent)];
+        const hasParent = (organisaatio: OrganisaatioSelectObject) => organisaatio.parentNames && organisaatio.parentNames.length > 0;
+        const noParent = (organisaatio: OrganisaatioSelectObject) => !organisaatio.parentNames || organisaatio.parentNames.length === 0;
+
+        const organisaatiotHavingParents: any = this._organisaatiotFilteredBy(organisaatiot, hasParent);
+        const organisaatiotNotHavingParents = this._organisaatiotFilteredBy(organisaatiot, noParent);
+
+        return [...organisaatiotNotHavingParents, ...R.sortBy(R.compose(R.toLower, R.head, R.prop('parentNames')))(organisaatiotHavingParents)];
     }
 
 
