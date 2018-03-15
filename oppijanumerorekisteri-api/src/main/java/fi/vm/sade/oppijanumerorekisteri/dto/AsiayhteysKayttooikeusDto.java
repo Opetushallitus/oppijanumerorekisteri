ec038cc8cc2ed87ebe5d7d40cd7d0f37f92bfdf9
Jarkko Pätäri <jarkko.patari@gofore.com>
2018-03-15 08:30:12 +0200

    KJHH-1145: Lisää puuttuvan rakentajan

--- oppijanumerorekisteri-api/src/main/java/fi/vm/sade/oppijanumerorekisteri/dto/AsiayhteysKayttooikeusDto.java
+++ oppijanumerorekisteri-api/src/main/java/fi/vm/sade/oppijanumerorekisteri/dto/AsiayhteysKayttooikeusDto.java
@@ -3,10 +3,12 @@ package fi.vm.sade.oppijanumerorekisteri.dto;
 import java.time.LocalDate;
 import javax.validation.constraints.NotNull;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class AsiayhteysKayttooikeusDto {
 
     @NotNull
