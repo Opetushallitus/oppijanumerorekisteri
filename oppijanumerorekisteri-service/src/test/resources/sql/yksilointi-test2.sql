02fb7483242aa784b810e4d1edc7a0046483fe8c
Jarkko Pätäri <jarkko.patari@gofore.com>
2018-03-29 14:12:56 +0300

    Korjaa testikannan alustuksen

--- oppijanumerorekisteri-service/src/test/resources/sql/yksilointi-test2.sql
+++ oppijanumerorekisteri-service/src/test/resources/sql/yksilointi-test2.sql
@@ -1,23 +1,23 @@
-INSERT INTO henkilo (version, hetu, oidhenkilo, created, modified, henkilotyyppi, duplicate, eisuomalaistahetua, ei_yksiloida, passivoitu, yksilointi_yritetty, yksiloity, yksiloityvtj, etunimet, kutsumanimi, sukunimi) VALUES
-(0, '111111-1235', 'SomeOtherVirkailija', NOW(), NOW(), FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, 'Teppo Taneli', 'Teppo', 'Testaaja'),
-(0, '010101-123N', 'EverythingOK', NOW(), NOW(), FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, TRUE, 'Teppo Taneli', 'Teppo', 'Testaaja'),
-(0, '010101-123M', 'Tyoosoite', NOW(), NOW(), FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, 'Teppo Taneli', 'Teppo', 'Testaaja'),
-(0, '010101-123L', 'TyoosoiteVainLuku', NOW(), NOW(), FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, 'Teppo Taneli', 'Teppo', 'Testaaja'),
-(0, '010101-123K', 'Kotiosoite', NOW(), NOW(), FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, 'Teppo Taneli', 'Teppo', 'Testaaja'),
-(0, '010101-234R', 'EverythingOkOppija', NOW(), NOW(), FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, TRUE, 'Teppo Taneli', 'Teppo', 'Testaaja'),
-(0, NULL, 'NoHetu', NOW(), NOW(), FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, TRUE, 'Teppo Taneli', 'Teppo', 'Testaaja');
+INSERT INTO henkilo (id, version, hetu, oidhenkilo, created, modified, duplicate, eisuomalaistahetua, ei_yksiloida, passivoitu, yksilointi_yritetty, yksiloity, yksiloityvtj, etunimet, kutsumanimi, sukunimi) VALUES
+(-1, 0, '111111-1235', 'SomeOtherVirkailija', NOW(), NOW(), FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, 'Teppo Taneli', 'Teppo', 'Testaaja'),
+(-2, 0, '010101-123N', 'EverythingOK', NOW(), NOW(), FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, TRUE, 'Teppo Taneli', 'Teppo', 'Testaaja'),
+(-3, 0, '010101-123M', 'Tyoosoite', NOW(), NOW(), FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, 'Teppo Taneli', 'Teppo', 'Testaaja'),
+(-4, 0, '010101-123L', 'TyoosoiteVainLuku', NOW(), NOW(), FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, 'Teppo Taneli', 'Teppo', 'Testaaja'),
+(-5, 0, '010101-123K', 'Kotiosoite', NOW(), NOW(), FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, 'Teppo Taneli', 'Teppo', 'Testaaja'),
+(-6, 0, '010101-234R', 'EverythingOkOppija', NOW(), NOW(), FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, TRUE, 'Teppo Taneli', 'Teppo', 'Testaaja'),
+(-7, 0, NULL, 'NoHetu', NOW(), NOW(), FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, TRUE, 'Teppo Taneli', 'Teppo', 'Testaaja');
 
-INSERT INTO yhteystiedotryhma (version, henkilo_id, ryhmakuvaus, read_only) VALUES
-(0, (SELECT id FROM henkilo where oidhenkilo = 'Tyoosoite'), 'yhteystietotyyppi2', FALSE);
-INSERT INTO yhteystiedot (version, yhteystiedotryhma_id, yhteystieto_tyyppi, yhteystieto_arvo) VALUES
-(0, (SELECT MAX(id) FROM yhteystiedotryhma), 'YHTEYSTIETO_SAHKOPOSTI', 'tyoosoite@example.com');
+INSERT INTO yhteystiedotryhma (id, version, henkilo_id, ryhmakuvaus, read_only) VALUES
+(-1, 0, (SELECT id FROM henkilo where oidhenkilo = 'Tyoosoite'), 'yhteystietotyyppi2', FALSE);
+INSERT INTO yhteystiedot (id, version, yhteystiedotryhma_id, yhteystieto_tyyppi, yhteystieto_arvo) VALUES
+(-1, 0, (SELECT MIN(id) FROM yhteystiedotryhma), 'YHTEYSTIETO_SAHKOPOSTI', 'tyoosoite@example.com');
 
-INSERT INTO yhteystiedotryhma (version, henkilo_id, ryhmakuvaus, read_only) VALUES
-(0, (SELECT id FROM henkilo where oidhenkilo = 'TyoosoiteVainLuku'), 'yhteystietotyyppi2', TRUE);
-INSERT INTO yhteystiedot (version, yhteystiedotryhma_id, yhteystieto_tyyppi, yhteystieto_arvo) VALUES
-(0, (SELECT MAX(id) FROM yhteystiedotryhma), 'YHTEYSTIETO_SAHKOPOSTI', 'tyoosoite@example.com');
+INSERT INTO yhteystiedotryhma (id, version, henkilo_id, ryhmakuvaus, read_only) VALUES
+(-2, 0, (SELECT id FROM henkilo where oidhenkilo = 'TyoosoiteVainLuku'), 'yhteystietotyyppi2', TRUE);
+INSERT INTO yhteystiedot (id, version, yhteystiedotryhma_id, yhteystieto_tyyppi, yhteystieto_arvo) VALUES
+(-2, 0, (SELECT MIN(id) FROM yhteystiedotryhma), 'YHTEYSTIETO_SAHKOPOSTI', 'tyoosoite@example.com');
 
-INSERT INTO yhteystiedotryhma (version, henkilo_id, ryhmakuvaus, read_only) VALUES
-(0, (SELECT id FROM henkilo where oidhenkilo = 'Kotiosoite'), 'yhteystietotyyppi1', FALSE);
-INSERT INTO yhteystiedot (version, yhteystiedotryhma_id, yhteystieto_tyyppi, yhteystieto_arvo) VALUES
-(0, (SELECT MAX(id) FROM yhteystiedotryhma), 'YHTEYSTIETO_SAHKOPOSTI', 'kotiosoite@example.com');
+INSERT INTO yhteystiedotryhma (id, version, henkilo_id, ryhmakuvaus, read_only) VALUES
+(-3, 0, (SELECT id FROM henkilo where oidhenkilo = 'Kotiosoite'), 'yhteystietotyyppi1', FALSE);
+INSERT INTO yhteystiedot (id, version, yhteystiedotryhma_id, yhteystieto_tyyppi, yhteystieto_arvo) VALUES
+(-3, 0, (SELECT MIN(id) FROM yhteystiedotryhma), 'YHTEYSTIETO_SAHKOPOSTI', 'kotiosoite@example.com');
