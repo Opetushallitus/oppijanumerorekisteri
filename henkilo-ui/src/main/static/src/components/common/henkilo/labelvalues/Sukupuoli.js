5a0f65b1c62a25edf111fa0433dc058605d39abf
Jarkko Pätäri <jarkko.patari@gofore.com>
2018-04-26 11:55:16 +0300

    KJHH-1096: Lisää pudotusvalikon sukupuolelle

--- henkilo-ui/src/main/static/src/components/common/henkilo/labelvalues/Sukupuoli.js
+++ henkilo-ui/src/main/static/src/components/common/henkilo/labelvalues/Sukupuoli.js
@@ -3,9 +3,17 @@ import React from 'react';
 import {connect} from 'react-redux';
 import LabelValue from './LabelValue';
 import type {HenkiloState} from "../../../../reducers/henkilo.reducer";
+import type {ReactSelectOption} from "../../../../types/react-select.types";
+import type {Locale} from "../../../../types/locale.type";
+import type {Henkilo} from "../../../../types/domain/oppijanumerorekisteri/henkilo.types";
 
 type Props = {
     henkilo: HenkiloState,
+    koodisto: {
+        sukupuoli: Array<ReactSelectOption>,
+    },
+    locale: Locale,
+    henkiloUpdate: Henkilo,
     readOnly: boolean,
     updateModelFieldAction: () => void,
 }
@@ -15,14 +23,19 @@ const Sukupuoli = (props: Props) => <LabelValue
     updateModelFieldAction={props.updateModelFieldAction}
     values={{
         label: 'HENKILO_SUKUPUOLI',
-        value: props.henkilo.henkilo.sukupuoli,
-        inputValue: 'sukupuoli',
+        data: props.koodisto.sukupuoli.map(koodi => ({value: koodi.value, label: koodi[props.locale],
+            optionsName: 'sukupuoli',})),
+        value: props.henkilo.henkilo.sukupuoli && props.koodisto.sukupuoli.filter(kieli =>
+            kieli.value === props.henkilo.henkilo.sukupuoli)[0][props.locale],
+        selectValue: props.henkiloUpdate.sukupuoli,
         disabled: !!props.henkilo.henkilo.hetu,
     }}
 />;
 
 const mapStateToProps = state => ({
     henkilo: state.henkilo,
+    koodisto: state.koodisto,
+    locale: state.locale,
 });
 
 export default connect(mapStateToProps, {})(Sukupuoli);
