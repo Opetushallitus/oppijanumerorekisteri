ad132b8e5d53cd3ef5704c36f7250ca22d3e9d33
Tapio Jaakkola <tapio.jaakkola@github.mail.kapsi.fi>
2018-05-08 13:07:49 +0300

    KJHH-1253: Korjataan oppijantietojen hakeminen

--- henkilo-ui/src/main/static/src/components/henkilo/OppijaViewContainer.js
+++ henkilo-ui/src/main/static/src/components/henkilo/OppijaViewContainer.js
@@ -15,18 +15,22 @@ class OppijaViewContainer extends React.Component {
         if (this.props.externalPermissionService) {
             PropertySingleton.setState({externalPermissionService: this.props.externalPermissionService});
         }
-        this.props.updateHenkiloNavigation(oppijaNavi(this.props.oidHenkilo));
-        await this.props.fetchHenkilo(this.props.oidHenkilo);
-        this.props.fetchHenkiloSlaves(this.props.oidHenkilo);
-        this.props.fetchYhteystietotyypitKoodisto();
-        this.props.fetchKieliKoodisto();
-        this.props.fetchKansalaisuusKoodisto();
+        await this.fetchOppijaViewData(this.props.oid);
     }
 
     async componentWillReceiveProps(nextProps) {
         if (nextProps.oidHenkilo !== this.props.oidHenkilo) {
-            this.fetch(nextProps.oidHenkilo)
+            await this.fetchOppijaViewData(nextProps.oidHenkilo);
         }
+    }
+
+    async fetchOppijaViewData(oid) {
+        this.props.updateHenkiloNavigation(oppijaNavi(oid));
+        await this.props.fetchHenkilo(oid);
+        this.props.fetchHenkiloSlaves(oid);
+        this.props.fetchYhteystietotyypitKoodisto();
+        this.props.fetchKieliKoodisto();
+        this.props.fetchKansalaisuusKoodisto();
     }
 
     render() {
