3e837e72d31df3300b7aa2f2a00020bbe8b734d0
Tapio Jaakkola <tapio.jaakkola@github.mail.kapsi.fi>
2018-05-14 09:17:19 +0300

    KJHH-824: Migraatioskripti, siirretään slave-henkilöiden sähköpostitunnisteet masterille

---
+++ oppijanumerorekisteri-service/src/main/resources/db/migration/V20180511092120234__sahkopostitunnusten_siirto.sql
@@ -0,0 +1,6 @@
+update identification i
+set henkilo_id = h1.id
+from henkiloviite hv
+  join henkilo h1 on h1.oidhenkilo = hv.master_oid
+  join henkilo h2 on h2.oidhenkilo = hv.slave_oid
+where i.henkilo_id = h2.id and i.idpentityid = 'email';
