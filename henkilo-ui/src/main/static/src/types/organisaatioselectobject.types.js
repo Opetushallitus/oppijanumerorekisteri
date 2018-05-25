6447482ce1e9a2e858e583b1590f6d738b850fe4
Tapio Jaakkola <tapio.jaakkola@github.mail.kapsi.fi>
2018-05-25 16:06:16 +0300

    KJHH-1391: Lisää suunniteltu-tiedon organisaation valintaan suunniteltu-tilaisille organisaatioille, jotka on haettu organisaatiopalvelusta

--- henkilo-ui/src/main/static/src/types/organisaatioselectobject.types.js
+++ henkilo-ui/src/main/static/src/types/organisaatioselectobject.types.js
@@ -4,5 +4,6 @@ export type OrganisaatioSelectObject = {
     oid: string,
     name: string,
     parentNames: Array<string>,
-    organisaatiotyypit: Array<string>
+    organisaatiotyypit: Array<string>,
+    status: string
 };
