b65b728f4a6a04b8efb1946f5fef5d0e39f9030c
Petri Ryhänen <petri.ryhanen@csc.fi>
2018-09-05 11:38:28 +0300

    KJHH-1444: trim kutsu email.

--- henkilo-ui/src/main/static/src/components/kutsuminen/KutsuConfirmation.js
+++ henkilo-ui/src/main/static/src/components/kutsuminen/KutsuConfirmation.js
@@ -101,10 +101,11 @@ export default class KutsuConfirmation extends React.Component<Props, State> {
 
         e.preventDefault();
 
+        const sahkoposti = this.props.basicInfo.email && this.props.basicInfo.email.trim();
         const payload = {
             etunimi: this.props.basicInfo.etunimi,
             sukunimi: this.props.basicInfo.sukunimi,
-            sahkoposti: this.props.basicInfo.email,
+            sahkoposti,
             asiointikieli: this.props.basicInfo.languageCode,
             organisaatiot: R.map(addedOrg => ({
                 organisaatioOid: addedOrg.oid,
