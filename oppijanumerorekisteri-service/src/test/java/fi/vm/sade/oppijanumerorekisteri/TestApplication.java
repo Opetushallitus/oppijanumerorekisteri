39d3c50487f43abf6360a6f6f4a6f2f4498598ed
Jarkko Pätäri <jarkko.patari@gofore.com>
2018-09-20 14:23:50 +0300

    KJHH-1443: Korjaa uloskirjautumisen jos on useampi instanssi ajossa

--- oppijanumerorekisteri-service/src/test/java/fi/vm/sade/oppijanumerorekisteri/TestApplication.java
+++ oppijanumerorekisteri-service/src/test/java/fi/vm/sade/oppijanumerorekisteri/TestApplication.java
@@ -2,10 +2,14 @@ package fi.vm.sade.oppijanumerorekisteri;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.session.jdbc.JdbcOperationsSessionRepository;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import static org.mockito.Mockito.mock;
 
 @SpringBootApplication
 public class TestApplication {
@@ -16,6 +20,11 @@ public class TestApplication {
     @EnableTransactionManagement
     public class H2JpaConfig {
 
+    }
+
+    @Bean
+    public JdbcOperationsSessionRepository jdbcOperationsSessionRepository() {
+        return mock(JdbcOperationsSessionRepository.class);
     }
 
     public static void main(String[] args) {
