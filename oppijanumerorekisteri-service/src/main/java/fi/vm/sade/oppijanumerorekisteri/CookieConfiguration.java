32c2d5cbf955476fa6f9fe9f83484f42b4acae70
Petri Ryhänen <petri.ryhanen@csc.fi>
2019-01-21 14:01:06 +0200

    KJHH-1546: Fix cross site development.

---
+++ oppijanumerorekisteri-service/src/main/java/fi/vm/sade/oppijanumerorekisteri/CookieConfiguration.java
@@ -0,0 +1,23 @@
+package fi.vm.sade.oppijanumerorekisteri;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.session.web.http.CookieSerializer;
+import org.springframework.session.web.http.DefaultCookieSerializer;
+
+@Configuration
+public class CookieConfiguration {
+    /**
+     * Meant to disable SameSite cookie attribute for cross site development on test environments. Otherwise cookies
+     * are not set causing infinite cas login loop.
+     * Not enabled by default.
+     */
+    @ConditionalOnProperty("oppijanumerorekisteri.dev.disable-same-site-cookie")
+    @Bean
+    public CookieSerializer cookieSerializer() {
+        DefaultCookieSerializer defaultCookieSerializer = new DefaultCookieSerializer();
+        defaultCookieSerializer.setSameSite(null);
+        return defaultCookieSerializer;
+    }
+}
