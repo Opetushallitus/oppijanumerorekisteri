a9762394ea72d7120ea5c13ded56e6abd456b871
Petri Ryhänen <petri.ryhanen@csc.fi>
2019-01-22 10:29:07 +0200

    KJHH-1546: Add missing cookie configurations.

--- oppijanumerorekisteri-service/src/main/java/fi/vm/sade/oppijanumerorekisteri/configurations/CookieConfiguration.java
+++ oppijanumerorekisteri-service/src/main/java/fi/vm/sade/oppijanumerorekisteri/configurations/CookieConfiguration.java
@@ -6,18 +6,21 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.session.web.http.CookieSerializer;
 import org.springframework.session.web.http.DefaultCookieSerializer;
 
+/**
+ * Meant to disable SameSite cookie attribute for cross site development on test environments. Otherwise cookies
+ * are not set causing infinite cas login loop.
+ * Not enabled by default.
+ */
 @Configuration
 public class CookieConfiguration {
-    /**
-     * Meant to disable SameSite cookie attribute for cross site development on test environments. Otherwise cookies
-     * are not set causing infinite cas login loop.
-     * Not enabled by default.
-     */
+
     @ConditionalOnProperty("oppijanumerorekisteri.disable-same-site-cookie")
     @Bean
     public CookieSerializer cookieSerializer() {
         DefaultCookieSerializer defaultCookieSerializer = new DefaultCookieSerializer();
         defaultCookieSerializer.setSameSite(null);
+        defaultCookieSerializer.setCookieName("JSESSIONID");
+        defaultCookieSerializer.setUseSecureCookie(true);
         return defaultCookieSerializer;
     }
 }
