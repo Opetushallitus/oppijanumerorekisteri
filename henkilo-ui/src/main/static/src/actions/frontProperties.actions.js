a3e4e61dcf5b938f7c21a078729972d56f47abe4
Jani Kaarela <jani.kaarela@gmail.com>
2020-07-09 15:50:02 +0300

    Caller-Id front propertiesin hakuun.

--- henkilo-ui/src/main/static/src/actions/frontProperties.actions.js
+++ henkilo-ui/src/main/static/src/actions/frontProperties.actions.js
@@ -6,6 +6,7 @@ import {fetchL10n} from './l10n.actions';
 import {fetchPrequels} from "./prequel.actions";
 import {fetchLocale, fetchOmattiedot} from "./omattiedot.actions";
 import {Dispatch} from "../types/dispatch.type";
+import PropertySingleton from "../globals/PropertySingleton";
 
 const requestFrontProperties = () => ({type: FETCH_FRONTPROPERTIES_REQUEST});
 const receivedFrontProperties = () => ({
@@ -15,7 +16,8 @@ const receivedFrontProperties = () => ({
 export const fetchFrontProperties = () => async (dispatch: Dispatch) => {
     dispatch(requestFrontProperties());
     urls.addProperties(frontUrls);
-    await urls.load({overrides: '/henkilo-ui/config/frontProperties',});
+    urls.addCallerId(PropertySingleton.getState().opintopolkuCallerId);
+    await urls.load({overrides: '/henkilo-ui/config/frontProperties'});
     dispatch(receivedFrontProperties());
     // Fetch localisations
     dispatch(fetchL10n());
