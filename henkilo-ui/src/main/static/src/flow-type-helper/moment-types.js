31e544e3cb4b20d03293e0b81ad1f4e68fc21127
Lauri Arimo <lauri.arimo@wunderdog.fi>
2020-12-14 16:47:13 +0200

    Prepare for flow-to-typescript migration

--- henkilo-ui/src/main/static/src/flow-type-helper/moment-types.js
+++
@@ -1,7 +0,0 @@
-declare module 'moment/locale/fi' {
-    declare module.exports: any
-}
-
-declare module 'moment/locale/sv' {
-    declare module.exports: any
-}
--- henkilo-ui/src/main/static/src/flow-type-helper/prop-styles.js
+++
@@ -1,5 +0,0 @@
-// https://github.com/facebook/flow/issues/2092
-
-declare module 'prop-types' {
-    declare module.exports: any
-}
