88f6b98a4c3ffc73d136ed181055c514e27e6091
Lauri Arimo <lauri.arimo@wunderdog.fi>
2021-04-26 13:27:56 +0300

    KJHH-1989: Password rules by Valtori

--- henkilo-ui/src/main/static/src/validation/PasswordValidator.ts
+++ henkilo-ui/src/main/static/src/validation/PasswordValidator.ts
@@ -1,11 +1,32 @@
-export const minimunPasswordLength = 20;
+const minimunPasswordLength = 20;
+const forbidden = /[^a-zA-Z0-9!#$%*_+=?]/;
+const specialCharacterRegex = /[!#$%*_+=?]/;
+const numberRegex = /\d/;
+const lowercaseLetters = /[a-z]/;
+const uppercaseLetters = /[A-Z]/;
 
 export const isValidPasswordLength = (password: string) => password.length >= minimunPasswordLength;
+export const hasForbiddenCharacters = (password: string) => forbidden.exec(password) !== null;
+export const containsNumber = (password: string) => numberRegex.exec(password) !== null;
+export const containsLowercaseLetters = (password: string) => lowercaseLetters.exec(password) !== null;
+export const containsUppercaseLetters = (password: string) => uppercaseLetters.exec(password) !== null;
+export const containsSpecialCharacter = (password: string) => specialCharacterRegex.exec(password) !== null;
+const atLeast = (password: string, n: number, fn: ((string) => boolean)[]): boolean =>
+    fn.map((f) => f(password)).filter(Boolean).length >= n;
 
 export const isValidPassword = (password: string): boolean => {
     if (typeof password !== 'string') {
         return false;
     }
 
-    return isValidPasswordLength(password);
+    return (
+        isValidPasswordLength(password) &&
+        !hasForbiddenCharacters(password) &&
+        atLeast(password, 3, [
+            containsNumber,
+            containsLowercaseLetters,
+            containsUppercaseLetters,
+            containsSpecialCharacter,
+        ])
+    );
 };
