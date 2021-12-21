c1e8e1574e30784bb43419f4f8c806663fc58107
Lauri Arimo <lauri.arimo@wunderdog.fi>
2021-12-21 15:09:51 +0200

    KJHH-2075: Add death cleanup task for removing access rights

--- oppijanumerorekisteri-domain/src/main/java/fi/vm/sade/oppijanumerorekisteri/enums/CleanupStep.java
+++ oppijanumerorekisteri-domain/src/main/java/fi/vm/sade/oppijanumerorekisteri/enums/CleanupStep.java
@@ -12,5 +12,6 @@ package fi.vm.sade.oppijanumerorekisteri.enums;
 public enum CleanupStep {
     INITIATED, // NOP operation acting as a sentry
     CLEAR_MUNICIPALITY,
-    REMOVE_CONTACT_INFORMATION
+    REMOVE_CONTACT_INFORMATION,
+    REMOVE_ACCESS_RIGHTS
 }
--- oppijanumerorekisteri-service/src/test/java/fi/vm/sade/oppijanumerorekisteri/services/death/steps/RemoveAccessRightsStepTest.java
+++ oppijanumerorekisteri-service/src/test/java/fi/vm/sade/oppijanumerorekisteri/services/death/steps/RemoveAccessRightsStepTest.java
@@ -1,29 +1,30 @@
 package fi.vm.sade.oppijanumerorekisteri.services.death.steps;
 
+import fi.vm.sade.oppijanumerorekisteri.clients.KayttooikeusClient;
 import fi.vm.sade.oppijanumerorekisteri.configurations.properties.OppijanumerorekisteriProperties;
 import fi.vm.sade.oppijanumerorekisteri.enums.CleanupStep;
 import fi.vm.sade.oppijanumerorekisteri.models.Henkilo;
-import fi.vm.sade.oppijanumerorekisteri.models.YhteystiedotRyhma;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
-import java.util.Set;
-
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
-class RemoveContactInformationStepTest {
+class RemoveAccessRightsStepTest {
 
-    final RemoveContactInformationStep step = new RemoveContactInformationStep();
+    private static final String SUBJECT_OID = "subject";
+    private static final String ROOT_OID = "root";
+
+    final RemoveAccessRightsStep step = new RemoveAccessRightsStep();
 
     @Mock
     Henkilo subject;
 
     @Mock
-    Set<YhteystiedotRyhma> contactInfo;
+    KayttooikeusClient kayttooikeusClient;
 
     @Mock
     OppijanumerorekisteriProperties properties;
@@ -31,16 +32,18 @@ class RemoveContactInformationStepTest {
     @BeforeEach
     void setUp() {
         MockitoAnnotations.initMocks(this);
+        step.kayttooikeusClient = kayttooikeusClient;
         step.properties = properties;
     }
 
     @Test
     void clearsContactInformation() {
-        given(subject.getYhteystiedotRyhma()).willReturn(contactInfo);
+        given(subject.getOidHenkilo()).willReturn(SUBJECT_OID);
+        given(step.properties.getRootUserOid()).willReturn(ROOT_OID);
 
         step.applyTo(subject);
 
-        verify(subject, times(1)).setCleanupStep(CleanupStep.REMOVE_CONTACT_INFORMATION);
-        verify(contactInfo, times(1)).clear();
+        verify(subject, times(1)).setCleanupStep(CleanupStep.REMOVE_ACCESS_RIGHTS);
+        verify(step.kayttooikeusClient, times(1)).passivoiHenkilo(SUBJECT_OID, ROOT_OID);
     }
 }
