f22c683265e01f5a5c6e20fb598777c7ced32215
Lauri Arimo <lauri.arimo@wunderdog.fi>
2022-02-18 16:12:24 +0200

    KJHH-2030: Add couple of tests for existence check validation

--- oppijanumerorekisteri-api/src/test/java/fi/vm/sade/oppijanumerorekisteri/dto/HenkiloExistenceCheckDtoTest.java
+++ oppijanumerorekisteri-api/src/test/java/fi/vm/sade/oppijanumerorekisteri/dto/HenkiloExistenceCheckDtoTest.java
@@ -35,7 +35,9 @@ class HenkiloExistenceCheckDtoTest {
                 Arguments.of("Validates lastName", new HenkiloExistenceCheckDto("230668-003A", "a b c", "b", ""), false),
                 Arguments.of("Validates lastName for leading spaces", new HenkiloExistenceCheckDto("230668-003A", "a b c", "b", " d"), false),
                 Arguments.of("Validates lastName for trailing spaces", new HenkiloExistenceCheckDto("230668-003A", "a b c", "b", "d "), false),
-                Arguments.of("Should be valid (special chars)", new HenkiloExistenceCheckDto("230668-003A", "X Æ A-12", "Æ", "Musk"), true)
+                Arguments.of("Should be valid (special chars)", new HenkiloExistenceCheckDto("230668-003A", "X Æ A-12", "Æ", "Musk"), true),
+                Arguments.of("Should be valid (case insensitive nick)", new HenkiloExistenceCheckDto("230668-003A", "a b c", "B", "d"), true),
+                Arguments.of("No partial match for nickname", new HenkiloExistenceCheckDto("230668-003A", "X Æ A-12", "-", "Musk"), false)
         );
     }
 
