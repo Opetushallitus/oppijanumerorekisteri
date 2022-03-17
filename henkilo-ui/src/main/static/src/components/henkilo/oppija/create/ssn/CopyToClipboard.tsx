127fac076692266c5f3f80387f12fa0526091747
Lauri Arimo <lauri.arimo@wunderdog.fi>
2022-03-17 12:25:09 +0200

    KJHH-2030: Add visual cue to copy-to-clipboard button
    * externalise component css

--- henkilo-ui/src/main/static/src/components/henkilo/oppija/create/ssn/CopyToClipboard.tsx
+++ henkilo-ui/src/main/static/src/components/henkilo/oppija/create/ssn/CopyToClipboard.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import Button from '../../../../common/button/Button';
+import './DetailsForm.css';
 
 type Props = {
     text: string;
     translate: (key: string) => string;
 };
 
-const CopyToClipboard: React.FC<Props> = ({ text, translate }) => (
-    <div className="oph-field" style={{ display: 'flex' }}>
-        <input type="text" className="oph-input" value={text || ''} readOnly style={{ width: 'fit-content' }} />
-        <Button action={() => navigator.clipboard.writeText(text)}>{translate('KOPIOI')}</Button>
-    </div>
-);
+const CopyToClipboard: React.FC<Props> = ({ text, translate }) => {
+    return (
+        <div className="oph-field copy-to-clipboard">
+            <input type="text" className="oph-input" value={text || ''} readOnly />
+            <Button action={() => navigator.clipboard.writeText(text)}>{translate('KOPIOI')}</Button>
+        </div>
+    );
+};
 
 export default CopyToClipboard;
