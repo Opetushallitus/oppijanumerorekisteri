e220c87220fa6761d83b46b86642696892344e77
Lauri Arimo <lauri.arimo@wunderdog.fi>
2022-10-31 10:11:26 +0000

    KJHH-2204: Refactor: improve HetuUtils readability by renaming things

--- oppijanumerorekisteri-api/src/main/java/fi/vm/sade/oppijanumerorekisteri/validation/HetuUtils.java
+++ oppijanumerorekisteri-api/src/main/java/fi/vm/sade/oppijanumerorekisteri/validation/HetuUtils.java
@@ -9,10 +9,10 @@ import java.util.stream.Collectors;
 
 public class HetuUtils {
     private static final char[] tarkistusmerkit = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y'};
-    private static final Map<String, Integer> erotinmerkit = Map.of(
-                    1800, List.of("+"),
-                    1900, List.of("-", "Y", "X", "W", "V", "U"),
-                    2000, List.of("A", "B", "C", "D", "E", "F")
+    private static final Map<String, String> vuosisataByErotinmerkki = Map.of(
+                    "18", List.of("+"),
+                    "19", List.of("-", "Y", "X", "W", "V", "U"),
+                    "20", List.of("A", "B", "C", "D", "E", "F")
             ).entrySet()
             .stream()
             .flatMap(erotinByVuosisata -> erotinByVuosisata.getValue().stream().map(erotin -> Map.entry(erotin, erotinByVuosisata.getKey())))
@@ -23,8 +23,7 @@ public class HetuUtils {
         if (hetu != null && hetu.length() == 11) {
             try {
                 dateFromHetu(hetu);
-                int luku = Integer.parseInt(hetu.substring(0, 6) + hetu.substring(7, 10), 10);
-                return hetu.charAt(10) == tarkistusmerkit[luku % 31];
+                return extractTarkistusmerkki(hetu) == tarkistusmerkit[tarkistussumma(hetu) % tarkistusmerkit.length];
             } catch (Exception e) {
                 // do not throw exceptions
             }
@@ -33,12 +32,30 @@ public class HetuUtils {
     }
 
     public static LocalDate dateFromHetu(String hetu) {
-        int vuosi = Optional.of(erotinmerkit.get(hetu.substring(6, 7))).get();
-        vuosi += Integer.parseInt(hetu.substring(4, 6), 10);
-        return LocalDate.parse(hetu.substring(0, 4) + vuosi, dateFormatter);
+        return LocalDate.parse(String.format("%s%s%s", extractPvm(hetu), extractVuosisata(hetu), extractVuosi(hetu)), dateFormatter);
     }
 
     public static String sukupuoliFromHetu(String hetu) {
         return Character.digit(hetu.charAt(9), 10) % 2 == 0 ? "2" : "1";
+    }
+
+    private static char extractTarkistusmerkki(String hetu) {
+        return hetu.charAt(10);
+    }
+
+    private static int tarkistussumma(String hetu) {
+        return Integer.parseInt(hetu.substring(0, 6) + hetu.substring(7, 10), 10);
+    }
+
+    private static String extractVuosisata(String hetu) {
+        return Optional.of(vuosisataByErotinmerkki.get(hetu.substring(6, 7))).get();
+    }
+
+    private static String extractVuosi(String hetu) {
+        return hetu.substring(4, 6);
+    }
+
+    private static String extractPvm(String hetu) {
+        return hetu.substring(0, 4);
     }
 }
