d9f71e7785e1b96d73c368e0af9adb9e4f3b729d
Tomi Karlstedt <tomi.karlstedt@reaktor.com>
2023-02-14 15:04:05 +0200

    KJHH-2280 upgrade node 14->18 and cra 4->5

--- henkilo-ui/src/main/static/src/actions/tuontikooste.actions.test.ts
+++ henkilo-ui/src/main/static/src/actions/tuontikooste.actions.test.ts
@@ -1,10 +1,18 @@
 import { fetchTuontiKooste } from './tuontikooste.actions';
 import { FETCH_TUONTIKOOSTE_REQUEST, FETCH_TUONTIKOOSTE_SUCCESS, FETCH_TUONTIKOOSTE_FAILURE } from './actiontypes';
 import { http } from '../http';
+import { TuontiKoosteCriteria } from '../types/tuontikooste.types';
 
 jest.mock('oph-urls-js');
 jest.mock('../http');
 jest.mock('../utilities/localisation.util');
+
+const criteria = {
+    page: 1,
+    pageSize: 1,
+    field: 'timestamp',
+    sort: 'ASC',
+} as TuontiKoosteCriteria;
 
 afterAll(() => {
     jest.clearAllMocks();
@@ -16,7 +24,7 @@ beforeEach(() => {
 
 describe('Tuontikooste action creator', () => {
     const dispatch = jest.fn();
-    const actionCreator = fetchTuontiKooste();
+    const actionCreator = fetchTuontiKooste(criteria);
 
     test('Fetch data', async () => {
         http.get = jest.fn().mockResolvedValue([]);
