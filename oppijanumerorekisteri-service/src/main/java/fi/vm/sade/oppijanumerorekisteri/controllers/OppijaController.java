6cad494a7b089fb00c5af718a05a4ef51cccaa28
Lauri Arimo <lauri.arimo@wunderdog.fi>
2023-03-14 14:26:21 +0200

    KJHH-2275: Tuontidata: Sanitize hetu:s from output

--- oppijanumerorekisteri-service/src/main/java/fi/vm/sade/oppijanumerorekisteri/controllers/OppijaController.java
+++ oppijanumerorekisteri-service/src/main/java/fi/vm/sade/oppijanumerorekisteri/controllers/OppijaController.java
@@ -16,6 +16,7 @@ import javax.validation.Valid;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Oppijoiden tuontiin liittyvät toiminnot. Oppijoita tuodaan
@@ -121,7 +122,17 @@ public class OppijaController {
             + "'APP_OPPIJANUMEROREKISTERI_TUONTIDATA_READ')")
     @ApiOperation(value = "Tuontiin liittyvä tuontidata")
     List<OppijaTuontiRiviCreateDto> tuontiData(@PathVariable final long  tuontiId) {
-        return oppijaService.tuontiData(tuontiId);
+        return oppijaService.tuontiData(tuontiId).stream()
+                .map(this::sanitizeHetu)
+                .collect(Collectors.toList());
+
+    }
+
+    private OppijaTuontiRiviCreateDto sanitizeHetu(OppijaTuontiRiviCreateDto dto) {
+        if ( dto.getHenkilo().getHetu() != null ) {
+            dto.getHenkilo().setHetu(dto.getHenkilo().getHetu().replaceFirst(".{4}$", "****"));
+        }
+        return dto;
     }
 
     @GetMapping("/muuttuneet")
