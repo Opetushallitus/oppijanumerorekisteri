7fbf326f9e809083a351509eccba3ab0bc5f510f
Tomi Karlstedt <tomi.karlstedt@reaktor.com>
2023-06-16 12:00:23 +0300

    KJHH-2348 handle transactions programmatically and stop on failure when fetching muutostietos

--- oppijanumerorekisteri-service/src/main/java/fi/vm/sade/oppijanumerorekisteri/services/impl/VtjMuutostietoServiceImpl.java
+++ oppijanumerorekisteri-service/src/main/java/fi/vm/sade/oppijanumerorekisteri/services/impl/VtjMuutostietoServiceImpl.java
@@ -3,12 +3,10 @@ package fi.vm.sade.oppijanumerorekisteri.services.impl;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.concurrent.CompletionException;
-import java.util.concurrent.ExecutionException;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import fi.vm.sade.oppijanumerorekisteri.clients.VtjMuutostietoClient;
 import fi.vm.sade.oppijanumerorekisteri.clients.model.VtjMuutostietoResponse;
@@ -29,6 +27,9 @@ public class VtjMuutostietoServiceImpl implements VtjMuutostietoService {
     private final VtjMuutostietoClient muutostietoClient;
     private final HenkiloRepository henkiloRepository;
 
+    @Autowired
+    private TransactionTemplate transaction;
+
     private VtjMuutostietoKirjausavain fetchNewKirjausavain(long id) {
         try {
             long avain = muutostietoClient.fetchMuutostietoKirjausavain();
@@ -44,45 +45,44 @@ public class VtjMuutostietoServiceImpl implements VtjMuutostietoService {
         }
     }
 
-    @Transactional
-    public boolean fetchMuutostietoBatchForBucket(long bucketId, List<String> hetus)
-            throws JsonProcessingException, InterruptedException, ExecutionException {
-        VtjMuutostietoKirjausavain kirjausavain = kirjausavainRepository.findById(bucketId)
-                .orElseGet(() -> fetchNewKirjausavain(bucketId));
-        log.info("using kirjausavain " + kirjausavain.getAvain() + " for bucket " + bucketId);
-        VtjMuutostietoResponse response = muutostietoClient.fetchHenkiloMuutostieto(kirjausavain.getAvain(),
-                hetus);
-        muutostietoRepository.saveAll(response.getMuutokset());
-        kirjausavain.setAvain(response.getViimeisinKirjausavain());
-        kirjausavain.setUpdatedAt(LocalDateTime.now());
-        kirjausavainRepository.save(kirjausavain);
-        return response.getAjanTasalla();
+    public boolean fetchMuutostietoBatchForBucket(long bucketId, List<String> hetus) {
+        try {
+            VtjMuutostietoKirjausavain kirjausavain = kirjausavainRepository.findById(bucketId)
+                    .orElseGet(() -> fetchNewKirjausavain(bucketId));
+            log.info("using kirjausavain " + kirjausavain.getAvain() + " for bucket " + bucketId);
+            VtjMuutostietoResponse response = muutostietoClient.fetchHenkiloMuutostieto(kirjausavain.getAvain(),
+                    hetus);
+            muutostietoRepository.saveAll(response.getMuutokset());
+            kirjausavain.setAvain(response.getViimeisinKirjausavain());
+            kirjausavain.setUpdatedAt(LocalDateTime.now());
+            kirjausavainRepository.save(kirjausavain);
+            return response.getAjanTasalla();
+        } catch (InterruptedException ie) {
+            log.error("interrupted while fetching muutostieto for bucket " + bucketId, ie);
+            Thread.currentThread().interrupt();
+            return true;
+        } catch (Exception e) {
+            log.error("exception while fetching muutostieto for bucket " + bucketId, e);
+            return true;
+        }
     }
 
     @Override
     public void fetchHenkiloMuutostieto() {
         for (long i = 0; i < 100; i++) {
             final long bucketId = i;
-            try {
-                log.info("fetching muutostieto for bucket " + bucketId);
-                long start = System.currentTimeMillis();
+            log.info("fetching muutostieto for bucket " + bucketId);
+            long start = System.currentTimeMillis();
 
-                boolean ajanTasalla;
-                List<String> hetus = henkiloRepository.findHetusInBucket(bucketId);
-                log.info("bucket " + bucketId + " size is " + hetus.size());
-                do {
-                    ajanTasalla = fetchMuutostietoBatchForBucket(bucketId, hetus);
-                } while (!ajanTasalla);
+            boolean ajanTasalla;
+            List<String> hetus = henkiloRepository.findHetusInBucket(bucketId);
+            log.info("bucket " + bucketId + " size is " + hetus.size());
+            do {
+                ajanTasalla = transaction.execute(status -> fetchMuutostietoBatchForBucket(bucketId, hetus));
+            } while (!ajanTasalla);
 
-                long duration = System.currentTimeMillis() - start;
-                log.info("fetching muutostieto for bucket took " + duration + "ms");
-            } catch (InterruptedException ie) {
-                log.error("interrupted while fetching bucket " + bucketId, ie);
-                Thread.currentThread().interrupt();
-            } catch (Exception e) {
-                log.error("failed to fetch muutostieto for bucket " + bucketId, e);
-            }
+            long duration = System.currentTimeMillis() - start;
+            log.info("fetching muutostieto for bucket took " + duration + "ms");
         }
     }
-
 }
