bc765a0cb09f5069eb51b79463ef7f22aa4acc41
Tomi Karlstedt <tomi.karlstedt@reaktor.com>
2023-08-08 15:54:40 +0300

    KJHH-2356 test turvakielto and foreign addresses for VTJ perustieto

--- oppijanumerorekisteri-domain/src/main/java/fi/vm/sade/oppijanumerorekisteri/models/VtjPerustieto.java
+++ oppijanumerorekisteri-domain/src/main/java/fi/vm/sade/oppijanumerorekisteri/models/VtjPerustieto.java
@@ -2,6 +2,11 @@ package fi.vm.sade.oppijanumerorekisteri.models;
 
 import com.fasterxml.jackson.databind.JsonNode;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
 public class VtjPerustieto {
     public String henkilotunnus;
     public JsonNode tietoryhmat;
--- oppijanumerorekisteri-service/src/main/java/fi/vm/sade/oppijanumerorekisteri/services/impl/VtjMuutostietoServiceImpl.java
+++ oppijanumerorekisteri-service/src/main/java/fi/vm/sade/oppijanumerorekisteri/services/impl/VtjMuutostietoServiceImpl.java
@@ -7,7 +7,6 @@ import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 import java.util.concurrent.CompletionException;
-import java.util.function.Predicate;
 import java.util.stream.Stream;
 
 import org.springframework.beans.factory.annotation.Autowired;
