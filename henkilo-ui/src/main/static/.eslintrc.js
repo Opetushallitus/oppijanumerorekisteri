6895384c9e08e834753c52c3453ee38c61d0a740
Tomi Karlstedt <tomi.karlstedt@reaktor.com>
2023-09-14 14:35:11 +0300

    enable jsx-a11y eslint rules and set currently violated rules to warning level

--- henkilo-ui/src/main/static/.eslintrc.js
+++ henkilo-ui/src/main/static/.eslintrc.js
@@ -7,7 +7,7 @@ module.exports = {
     extends: [
         'eslint:recommended',
         'plugin:react/recommended',
-        //'plugin:jsx-a11y/recommended', TODO fix stuff
+        'plugin:jsx-a11y/recommended',
         'plugin:@typescript-eslint/recommended',
         'eslint-config-prettier',
     ],
@@ -20,5 +20,10 @@ module.exports = {
         // TODO fix stuff
         '@typescript-eslint/no-explicit-any': 1,
         'react/no-deprecated': 1,
+        'jsx-a11y/click-events-have-key-events': 1,
+        'jsx-a11y/no-static-element-interactions': 1,
+        'jsx-a11y/no-autofocus': 1,
+        'jsx-a11y/label-has-associated-control': 1,
+        'jsx-a11y/no-noninteractive-element-interactions': 1,
     },
 };
