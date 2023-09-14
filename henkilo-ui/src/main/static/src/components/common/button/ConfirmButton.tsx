0a6256ec34f1679537ff47dccff26b3fe82ffbbc
Tomi Karlstedt <tomi.karlstedt@reaktor.com>
2023-09-14 10:10:35 +0300

    refactor confirmbutton into a function component and remove react-timeout dependency from it

--- henkilo-ui/src/main/static/src/components/common/button/ConfirmButton.tsx
+++ henkilo-ui/src/main/static/src/components/common/button/ConfirmButton.tsx
@@ -1,7 +1,6 @@
 import './Button.css';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
-import ReactTimeout from 'react-timeout';
 
 import type { RootState } from '../../../store';
 import type { Notification } from '../../../reducers/notifications.reducer';
@@ -9,7 +8,6 @@ import NotificationButton from './NotificationButton';
 
 type OwnProps = {
     action: () => void;
-    setTimeout?: (fn: () => void, millis: number) => void; // Set by react-timeout HOC
     normalLabel: string;
     confirmLabel: string;
     id: string;
@@ -23,59 +21,46 @@ type StateProps = {
 
 type Props = OwnProps & StateProps;
 
-type State = {
-    confirmState: boolean;
-    disabled: boolean;
-};
-
-class ConfirmButton extends React.Component<Props, State> {
-    constructor(props: Props) {
-        super(props);
-        this.state = {
-            confirmState: false,
-            disabled: false,
-        };
-    }
-
-    componentWillReceiveProps(nextProps: Props) {
-        if (nextProps.notifications.filter((notification) => notification.id === this.props.id)[0]) {
-            this.setState({ confirmState: false, disabled: true });
-            this.props.setTimeout(() => {
-                this.setState({ disabled: false });
-            }, 2000);
-        }
-    }
-
-    render() {
-        const confirmProps = { ...this.props, cancel: false, action: this.action };
-        return !this.state.confirmState ? (
-            <NotificationButton
-                className={this.props.className}
-                {...this.props}
-                action={() => {
-                    this.setState({ confirmState: true });
-                }}
-                disabled={this.state.disabled || this.props.disabled}
-            >
-                {this.props.normalLabel}
-            </NotificationButton>
-        ) : (
-            <NotificationButton className={this.props.className} confirm {...confirmProps} id={this.props.id}>
-                {this.props.confirmLabel}
-            </NotificationButton>
-        );
-    }
-
-    action = () => {
-        this.setState({
-            confirmState: false,
-        });
-        this.props.action();
+const ConfirmButton = (props: Props) => {
+    const { action, className, confirmLabel, disabled, id, normalLabel, notifications } = props;
+    const [confirmState, setConfirmState] = useState(false);
+    const [disabledState, setDisabledState] = useState(false);
+    const actionFunction = () => {
+        setConfirmState(false);
+        action();
     };
-}
+    const confirmProps = { ...props, cancel: false, action: actionFunction };
+
+    useEffect(() => {
+        if (notifications.find((n) => n.id === id)) {
+            setConfirmState(false);
+            setDisabledState(true);
+        }
+        const timer = setTimeout(() => {
+            setDisabledState(false);
+        }, 2000);
+        return () => clearTimeout(timer);
+    }, [notifications, id]);
+
+    return !confirmState ? (
+        <NotificationButton
+            className={className}
+            {...props}
+            action={() => setConfirmState(true)}
+            disabled={disabledState || disabled}
+            id={id}
+        >
+            {normalLabel}
+        </NotificationButton>
+    ) : (
+        <NotificationButton className={className} confirm {...confirmProps} id={id}>
+            {confirmLabel}
+        </NotificationButton>
+    );
+};
 
 const mapStateToProps = (state: RootState): StateProps => ({
     notifications: state.notifications.buttonNotifications,
 });
 
-export default connect<StateProps, object, OwnProps, RootState>(mapStateToProps)(ReactTimeout(ConfirmButton));
+export default connect<StateProps, object, OwnProps, RootState>(mapStateToProps)(ConfirmButton);
