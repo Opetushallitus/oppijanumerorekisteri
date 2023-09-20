13bc778af96b89fb2d5535cb9f79e0f133189c4a
Tomi Karlstedt <tomi.karlstedt@reaktor.com>
2023-09-20 14:18:39 +0300

    HenkiloVarmentajaSuhde into a function component

--- henkilo-ui/src/main/static/src/components/common/henkilo/labelvalues/HenkiloVarmentajaSuhde.tsx
+++ henkilo-ui/src/main/static/src/components/common/henkilo/labelvalues/HenkiloVarmentajaSuhde.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import type { RootState } from '../../../../store';
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { useAppDispatch, type RootState } from '../../../../store';
 import { Link } from 'react-router';
 import LabelValueGroup from './LabelValueGroup';
 import { fetchHenkiloLinkitykset } from '../../../../actions/henkiloLinkitys.actions';
@@ -11,73 +11,43 @@ type OwnProps = {
     type: 'henkiloVarmentajas' | 'henkiloVarmennettavas';
 };
 
-type StateProps = {
-    linkitetyt: HenkiloLinkitysState;
-};
-
-type DispatchProps = {
-    fetchHenkiloLinkitykset: (oidHenkilo: string) => void;
-};
-
-type HenkiloVarmentajaSuhdeProps = OwnProps & StateProps & DispatchProps;
-
 /**
  * Hakee ja näyttää henkilön varmentajasuhteen LabelValueGroup:ina. Näitä suhteita on vain virkailijoilla joten linkki
  * olettaa /virkailija polun.
  */
-class HenkiloVarmentajaSuhde extends React.Component<HenkiloVarmentajaSuhdeProps> {
-    typeToL10nKeyMap: {
-        [key: string]: string;
+const HenkiloVarmentajaSuhde = (props: OwnProps) => {
+    const { oidHenkilo, type } = props;
+    const dispatch = useAppDispatch();
+    const linkitetyt = useSelector<RootState, HenkiloLinkitysState>((state) => state.linkitykset);
+    const typeToL10nKeyMap = {
+        henkiloVarmentajas: 'HENKILO_VARMENTAJA',
+        henkiloVarmennettavas: 'HENKILO_VARMENNETTAVA',
     };
 
-    componentWillMount() {
-        this.props.fetchHenkiloLinkitykset(this.props.oidHenkilo);
-    }
+    useEffect(() => {
+        dispatch<any>(fetchHenkiloLinkitykset(oidHenkilo));
+    }, [oidHenkilo]);
 
-    constructor(props: HenkiloVarmentajaSuhdeProps) {
-        super(props);
-
-        this.typeToL10nKeyMap = {
-            henkiloVarmentajas: 'HENKILO_VARMENTAJA',
-            henkiloVarmennettavas: 'HENKILO_VARMENNETTAVA',
-        };
-    }
-
-    render() {
-        const linkitetytByOid = this.props.linkitetyt[this.props.oidHenkilo];
+    const linkitetytGroup = (varmentajas: Array<string>) => {
         return (
-            <div>
-                {linkitetytByOid && linkitetytByOid[this.props.type] && !!linkitetytByOid[this.props.type].length && (
-                    <LabelValueGroup
-                        valueGroup={this.linkitetytGroup(linkitetytByOid[this.props.type])}
-                        label={this.typeToL10nKeyMap[this.props.type]}
-                    />
-                )}
-            </div>
-        );
-    }
-
-    linkitetytGroup(varmentajas: Array<string>) {
-        return (
-            <React.Fragment>
+            <>
                 {varmentajas.map((varmentajaOid, index) => (
                     <div key={index} className="nowrap">
-                        <Link to={HenkiloVarmentajaSuhde.getVirkailijaLink(varmentajaOid)}>{varmentajaOid}</Link>
+                        <Link to={`/virkailija/${varmentajaOid}`}>{varmentajaOid}</Link>
                     </div>
                 ))}
-            </React.Fragment>
+            </>
         );
-    }
+    };
 
-    static getVirkailijaLink(oid: string) {
-        return `/virkailija/${oid}`;
-    }
-}
+    const linkitetytByOid = linkitetyt[oidHenkilo];
+    return (
+        <div>
+            {linkitetytByOid && linkitetytByOid[type] && !!linkitetytByOid[type].length && (
+                <LabelValueGroup valueGroup={linkitetytGroup(linkitetytByOid[type])} label={typeToL10nKeyMap[type]} />
+            )}
+        </div>
+    );
+};
 
-const mapStateToProps = (state: RootState): StateProps => ({
-    linkitetyt: state.linkitykset,
-});
-
-export default connect<StateProps, DispatchProps, OwnProps, RootState>(mapStateToProps, { fetchHenkiloLinkitykset })(
-    HenkiloVarmentajaSuhde
-);
+export default HenkiloVarmentajaSuhde;
