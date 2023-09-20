9cbca5ea0b1bbacef6678e259cf1e4e6e81182bf
Tomi Karlstedt <tomi.karlstedt@reaktor.com>
2023-09-20 14:10:19 +0300

    KayttooikeusryhmanOrganisaatiorajoite into a function component

--- henkilo-ui/src/main/static/src/components/kayttooikeusryhmat/kayttooikeusryhma/KayttooikeusryhmanOrganisaatiorajoite.tsx
+++ henkilo-ui/src/main/static/src/components/kayttooikeusryhmat/kayttooikeusryhma/KayttooikeusryhmanOrganisaatiorajoite.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import type { Options } from 'react-select';
+
 import ItemList from './ItemList';
 import './KayttooikeusryhmanOrganisaatiorajoite.css';
-import { Locale } from '../../../types/locale.type';
-import type { Options } from 'react-select';
-import { Localisations } from '../../../types/localisation.type';
 import OrganisaatioSelectModal from '../../common/select/OrganisaatioSelectModal';
 import { OrganisaatioSelectObject } from '../../../types/organisaatioselectobject.types';
 import OphCheckboxFieldset from '../../common/forms/OphCheckboxFieldset';
 import { toLocalizedText } from '../../../localizabletext';
+import { KoodistoState } from '../../../reducers/koodisto.reducer';
+import { useLocalisations } from '../../../selectors';
+import { RootState } from '../../../store';
 
 type Props = {
-    L: Localisations;
-    koodisto: any;
-    locale: Locale;
     ryhmaRestriction: boolean;
     toggleRyhmaRestriction: () => void;
     organisaatioSelections: Array<OrganisaatioSelectObject>;
@@ -24,85 +24,72 @@ type Props = {
     organisaatiotyypitSelectAction: (selection: React.ChangeEvent<HTMLInputElement>) => void;
 };
 
-type State = {
-    oppilaitostyypitOptions: Array<any>;
-    organisaatiotyypitOptions: Array<any>;
-};
+const KayttooikeusryhmanOrganisaatiorajoite = (props: Props) => {
+    const { L, locale } = useLocalisations();
+    const koodisto = useSelector<RootState, KoodistoState>((state) => state.koodisto);
+    const oppilaitostyypitOptions: Options<string> = koodisto.oppilaitostyypit.map((oppilaitostyyppi) => ({
+        label: oppilaitostyyppi[locale],
+        value: oppilaitostyyppi.value,
+    }));
+    const organisaatiotyypitOptions: Options<string> = koodisto.organisaatiotyyppiKoodisto.map(
+        (organisaatiotyyppi) => ({
+            label: toLocalizedText(locale, organisaatiotyyppi.metadata) || organisaatiotyyppi.koodiUri,
+            value: organisaatiotyyppi.koodiUri,
+        })
+    );
 
-export default class KayttooikeusryhmanOrganisaatiorajoite extends React.Component<Props, State> {
-    state = {
-        oppilaitostyypitOptions: [],
-        organisaatiotyypitOptions: [],
-    };
+    return (
+        <div className="kayttooikeusryhman-myonto-kohde">
+            <h4>{L['KAYTTOOIKEUSRYHMAT_LISAA_ORGANISAATIORAJOITE_OTSIKKO']}</h4>
+            <label className="oph-checkable" htmlFor="ryhmarestriction">
+                <input
+                    id="ryhmarestriction"
+                    className="oph-checkable-input"
+                    type="checkbox"
+                    onChange={props.toggleRyhmaRestriction}
+                    checked={props.ryhmaRestriction}
+                />
+                <span className="oph-checkable-text">{L['KAYTTOOIKEUSRYHMAT_LISAA_RYHMA']}</span>
+            </label>
 
-    componentWillMount() {
-        const oppilaitostyypitOptions: Options<string> = this.props.koodisto.oppilaitostyypit.map(
-            (oppilaitostyyppi) => ({
-                label: oppilaitostyyppi[this.props.locale],
-                value: oppilaitostyyppi.value,
-            })
-        );
-        const organisaatiotyypitOptions: Options<string> = this.props.koodisto.organisaatiotyyppiKoodisto.map(
-            (organisaatiotyyppi) => ({
-                label: toLocalizedText(this.props.locale, organisaatiotyyppi.metadata) || organisaatiotyyppi.koodiUri,
-                value: organisaatiotyyppi.koodiUri,
-            })
-        );
-        this.setState({ oppilaitostyypitOptions, organisaatiotyypitOptions });
-    }
-
-    render() {
-        return (
-            <div className="kayttooikeusryhman-myonto-kohde">
-                <h4>{this.props.L['KAYTTOOIKEUSRYHMAT_LISAA_ORGANISAATIORAJOITE_OTSIKKO']}</h4>
-                <label className="oph-checkable" htmlFor="ryhmarestriction">
-                    <input
-                        id="ryhmarestriction"
-                        className="oph-checkable-input"
-                        type="checkbox"
-                        onChange={this.props.toggleRyhmaRestriction}
-                        checked={this.props.ryhmaRestriction}
+            <div className="flex-horizontal">
+                <div className="flex-item-1 ">
+                    <OrganisaatioSelectModal onSelect={props.organisaatioSelectAction} />
+                    <ItemList
+                        items={props.organisaatioSelections}
+                        labelPath={['name']}
+                        removeAction={props.removeOrganisaatioSelectAction}
                     />
-                    <span className="oph-checkable-text">{this.props.L['KAYTTOOIKEUSRYHMAT_LISAA_RYHMA']}</span>
-                </label>
-
-                <div className="flex-horizontal">
-                    <div className="flex-item-1 ">
-                        <OrganisaatioSelectModal onSelect={this.props.organisaatioSelectAction} />
-                        <ItemList
-                            items={this.props.organisaatioSelections}
-                            labelPath={['name']}
-                            removeAction={this.props.removeOrganisaatioSelectAction}
-                        />
-                    </div>
-                    <div className="flex-item-1 oppilaitostyyppi-wrapper">
-                        <OphCheckboxFieldset
-                            legendText={this.props.L['KAYTTOOIKEUSRYHMAT_LISAA_VALITSE_OPPILAITOSTYYPPI']}
-                            options={this.state.oppilaitostyypitOptions.map((option) => ({
-                                label: option.label,
-                                value: option.value,
-                                checked:
-                                    this.props.oppilaitostyypitSelections &&
-                                    this.props.oppilaitostyypitSelections.indexOf(option.value) !== -1,
-                            }))}
-                            selectAction={this.props.oppilaitostyypitSelectAction}
-                        />
-                    </div>
-                    <div className="flex-item-1 organisaatiotyyppi-wrapper">
-                        <OphCheckboxFieldset
-                            legendText={this.props.L['KAYTTOOIKEUSRYHMAT_LISAA_VALITSE_ORGANISAATIOTYYPPI']}
-                            options={this.state.organisaatiotyypitOptions.map((option) => ({
-                                label: option.label,
-                                value: option.value,
-                                checked:
-                                    this.props.organisaatiotyypitSelections &&
-                                    this.props.organisaatiotyypitSelections.indexOf(option.value) !== -1,
-                            }))}
-                            selectAction={this.props.organisaatiotyypitSelectAction}
-                        />
-                    </div>
+                </div>
+                <div className="flex-item-1 oppilaitostyyppi-wrapper">
+                    <OphCheckboxFieldset
+                        legendText={L['KAYTTOOIKEUSRYHMAT_LISAA_VALITSE_OPPILAITOSTYYPPI']}
+                        options={oppilaitostyypitOptions.map((option) => ({
+                            label: option.label,
+                            value: option.value,
+                            checked:
+                                props.oppilaitostyypitSelections &&
+                                props.oppilaitostyypitSelections.indexOf(option.value) !== -1,
+                        }))}
+                        selectAction={props.oppilaitostyypitSelectAction}
+                    />
+                </div>
+                <div className="flex-item-1 organisaatiotyyppi-wrapper">
+                    <OphCheckboxFieldset
+                        legendText={L['KAYTTOOIKEUSRYHMAT_LISAA_VALITSE_ORGANISAATIOTYYPPI']}
+                        options={organisaatiotyypitOptions.map((option) => ({
+                            label: option.label,
+                            value: option.value,
+                            checked:
+                                props.organisaatiotyypitSelections &&
+                                props.organisaatiotyypitSelections.indexOf(option.value) !== -1,
+                        }))}
+                        selectAction={props.organisaatiotyypitSelectAction}
+                    />
                 </div>
             </div>
-        );
-    }
-}
+        </div>
+    );
+};
+
+export default KayttooikeusryhmanOrganisaatiorajoite;
