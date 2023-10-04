ee7aa37c9e915de8ca2147bf750fa41652ad83da
Tomi Karlstedt <tomi.karlstedt@reaktor.com>
2023-10-04 10:10:32 +0300

    KJHH-2392 remove tuontikooste indices as it's a view, not a table...

--- oppijanumerorekisteri-service/src/main/resources/db/migration/V20231003000000000__add_tuontikooste_indices.sql
+++
@@ -1,4 +0,0 @@
-CREATE INDEX IF NOT EXISTS tuontikooste_id_idx ON tuontikooste USING btree (id);
-CREATE INDEX IF NOT EXISTS tuontikooste_timestamp_idx ON tuontikooste USING btree (timestamp);
-CREATE INDEX IF NOT EXISTS tuontikooste_org_idx ON tuontikooste USING btree (org);
-CREATE INDEX IF NOT EXISTS tuontikooste_author_idx ON tuontikooste USING btree (author);
