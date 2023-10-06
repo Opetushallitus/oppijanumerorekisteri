09399eb73fbbc3a4e136671a317d4a2ea270a2a9
Tomi Karlstedt <tomi.karlstedt@reaktor.com>
2023-10-06 11:23:28 +0300

    use upgraded react-table in kutsudetails

--- henkilo-ui/src/main/static/src/components/OphTable.tsx
+++ henkilo-ui/src/main/static/src/components/OphTable.tsx
@@ -63,63 +63,65 @@ const OphTable = <T,>({ table, isLoading }: Props<T>) => {
                 </tbody>
             </table>
             {isLoading && <Loader />}
-            <div className="pagination">
-                <div className="previous">
-                    <button
-                        className="button"
-                        onClick={() => {
-                            pageRef.current.value = String(table.getState().pagination.pageIndex);
-                            table.previousPage();
-                        }}
-                        disabled={isLoading || !table.getCanPreviousPage()}
-                    >
-                        {L['TAULUKKO_EDELLINEN']}
-                    </button>
-                </div>
-                <div className="center">
-                    <span className="page-info">
-                        {L['TAULUKKO_SIVU']}
-                        <div className="page-jump">
-                            <input
-                                ref={pageRef}
-                                type="number"
-                                defaultValue={table.getState().pagination.pageIndex || 0 + 1}
-                                onChange={(e) => {
-                                    const page = e.target.value ? Number(e.target.value) - 1 : 1;
-                                    table.setPageIndex(page);
-                                }}
-                            />
-                        </div>
-                        /{table.getPageCount()}
-                    </span>
-                    <span className="page-select">
-                        <select
-                            value={table.getState().pagination.pageSize}
-                            onChange={(e) => {
-                                table.setPageSize(Number(e.target.value));
+            {table.getPageCount() > 1 && (
+                <div className="pagination">
+                    <div className="previous">
+                        <button
+                            className="button"
+                            onClick={() => {
+                                pageRef.current.value = String(table.getState().pagination.pageIndex);
+                                table.previousPage();
                             }}
+                            disabled={isLoading || !table.getCanPreviousPage()}
                         >
-                            {[10, 20, 50, 100].map((pageSize) => (
-                                <option key={pageSize} value={pageSize}>
-                                    {pageSize} {L['TAULUKKO_RIVIA']}
-                                </option>
-                            ))}
-                        </select>
-                    </span>
+                            {L['TAULUKKO_EDELLINEN']}
+                        </button>
+                    </div>
+                    <div className="center">
+                        <span className="page-info">
+                            {L['TAULUKKO_SIVU']}
+                            <div className="page-jump">
+                                <input
+                                    ref={pageRef}
+                                    type="number"
+                                    defaultValue={table.getState().pagination.pageIndex || 0 + 1}
+                                    onChange={(e) => {
+                                        const page = e.target.value ? Number(e.target.value) - 1 : 1;
+                                        table.setPageIndex(page);
+                                    }}
+                                />
+                            </div>
+                            /{table.getPageCount()}
+                        </span>
+                        <span className="page-select">
+                            <select
+                                value={table.getState().pagination.pageSize}
+                                onChange={(e) => {
+                                    table.setPageSize(Number(e.target.value));
+                                }}
+                            >
+                                {[10, 20, 50, 100].map((pageSize) => (
+                                    <option key={pageSize} value={pageSize}>
+                                        {pageSize} {L['TAULUKKO_RIVIA']}
+                                    </option>
+                                ))}
+                            </select>
+                        </span>
+                    </div>
+                    <div className="next">
+                        <button
+                            className="border rounded p-1"
+                            onClick={() => {
+                                pageRef.current.value = String(table.getState().pagination.pageIndex + 2);
+                                table.nextPage();
+                            }}
+                            disabled={isLoading || !table.getCanNextPage()}
+                        >
+                            {L['TAULUKKO_SEURAAVA']}
+                        </button>
+                    </div>
                 </div>
-                <div className="next">
-                    <button
-                        className="border rounded p-1"
-                        onClick={() => {
-                            pageRef.current.value = String(table.getState().pagination.pageIndex + 2);
-                            table.nextPage();
-                        }}
-                        disabled={isLoading || !table.getCanNextPage()}
-                    >
-                        {L['TAULUKKO_SEURAAVA']}
-                    </button>
-                </div>
-            </div>
+            )}
         </div>
     );
 };
--- henkilo-ui/src/main/static/src/components/kutsutut/KutsuDetails.tsx
+++ henkilo-ui/src/main/static/src/components/kutsutut/KutsuDetails.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
-import ReactTable from 'react-table';
+import React, { useMemo } from 'react';
+import { useReactTable, getCoreRowModel, ColumnDef } from '@tanstack/react-table';
+
 import { toLocalizedText } from '../../localizabletext';
 import { Localisations } from '../../types/localisation.type';
 import { Locale } from '../../types/locale.type';
 import { KutsuRead as Kutsu } from '../../types/domain/kayttooikeus/Kutsu.types';
+import OphTable from '../OphTable';
 
 type Props = {
     kutsu?: Kutsu;
@@ -24,31 +26,38 @@ export const resolveInvitationRights = (
           )
         : [];
 
-const getLocalizedColumns = (L: Localisations) => [
-    {
-        Header: L['KUTSU_KAYTTOOIKEUSRYHMAT_MAIN_HEADER'],
-        columns: [
+const KutsuDetails = ({ kutsu, L, locale }: Props) => {
+    const columns = useMemo<ColumnDef<{ organisaatio: string; ryhma: string }>[]>(
+        () => [
             {
-                Header: L['HENKILO_KAYTTOOIKEUSANOMUS_KAYTTOOIKEUSRYHMAT_ORGANISAATIO_HEADER'],
-                accessor: 'organisaatio',
-            },
-            {
-                Header: L['HENKILO_KAYTTOOIKEUSANOMUS_KAYTTOOIKEUSRYHMAT_KAYTTOOIKEUS_HEADER'],
-                accessor: 'ryhma',
+                header: () => L['KUTSU_KAYTTOOIKEUSRYHMAT_MAIN_HEADER'],
+                id: 'group',
+                columns: [
+                    {
+                        header: () => L['HENKILO_KAYTTOOIKEUSANOMUS_KAYTTOOIKEUSRYHMAT_ORGANISAATIO_HEADER'],
+                        accessorFn: (row) => row.organisaatio,
+                        id: 'organisaatio',
+                    },
+                    {
+                        header: () => L['HENKILO_KAYTTOOIKEUSANOMUS_KAYTTOOIKEUSRYHMAT_KAYTTOOIKEUS_HEADER'],
+                        accessorFn: (row) => row.ryhma,
+                        id: 'ryhma',
+                    },
+                ],
             },
         ],
-    },
-];
-
-const KutsuDetails = ({ kutsu, L, locale }: Props) => (
-    <div className="anoja-kayttooikeusryhmat">
-        <ReactTable
-            data={resolveInvitationRights(kutsu, locale)}
-            columns={getLocalizedColumns(L)}
-            showPagination={false}
-            minRows={0}
-        />
-    </div>
-);
+        []
+    );
+    const table = useReactTable({
+        data: resolveInvitationRights(kutsu, locale),
+        getCoreRowModel: getCoreRowModel(),
+        columns,
+    });
+    return (
+        <div className="anoja-kayttooikeusryhmat">
+            <OphTable table={table} isLoading={false} />
+        </div>
+    );
+};
 
 export default KutsuDetails;
