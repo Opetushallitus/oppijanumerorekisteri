b35e8132d0ce121ddbed3fbf687551f0e6ccd016
Tomi Karlstedt <tomi.karlstedt@reaktor.com>
2023-10-30 17:31:18 +0200

    remove cra build script

--- henkilo-ui/src/main/static/webpack.config.js
+++ henkilo-ui/src/main/static/webpack.config.js
@@ -2,53 +2,38 @@ const path = require('path');
 const webpack = require('webpack');
 const resolve = require('resolve');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const InlineChunkHtmlPlugin = require('react-dev-utils/InlineChunkHtmlPlugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
-const InterpolateHtmlPlugin = require('react-dev-utils/InterpolateHtmlPlugin');
-const paths = require('./paths');
-const modules = require('./modules');
-const getClientEnvironment = require('./env');
-const ModuleNotFoundPlugin = require('react-dev-utils/ModuleNotFoundPlugin');
-const ForkTsCheckerWebpackPlugin =
-    process.env.TSC_COMPILE_ON_ERROR === 'true'
-        ? require('react-dev-utils/ForkTsCheckerWarningWebpackPlugin')
-        : require('react-dev-utils/ForkTsCheckerWebpackPlugin');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 const ReactRefreshTypeScript = require('react-refresh-typescript');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const { createHash } = require('crypto');
 
-const createEnvironmentHash = require('./webpack/persistentCache/createEnvironmentHash');
-
-// Source maps are resource heavy and can cause out of memory issue for large source files.
 const shouldUseSourceMap = process.env.GENERATE_SOURCEMAP !== 'false';
-
-// Some apps do not need the benefits of saving a web request, so not inlining the chunk
-// makes for a smoother build process.
-const shouldInlineRuntimeChunk = process.env.INLINE_RUNTIME_CHUNK !== 'false';
-
 const imageInlineSizeLimit = parseInt(process.env.IMAGE_INLINE_SIZE_LIMIT || '10000');
+const host = process.env.HOST || '0.0.0.0';
 
-// style files regexes
 const cssRegex = /\.css$/;
 const cssModuleRegex = /\.module\.css$/;
 
-const host = process.env.HOST || '0.0.0.0';
+const createEnvironmentHash = (env) => {
+    const hash = createHash('md5');
+    hash.update(JSON.stringify(env));
 
-// This is the production and development configuration.
-// It is focused on developer experience, fast rebuilds, and a minimal bundle.
-module.exports = function (webpackEnv) {
+    return hash.digest('hex');
+};
+
+module.exports = function () {
     const isEnvDevelopment = process.env.NODE_ENV === 'development';
-    const isEnvProduction = webpackEnv === 'production';
-    // Variable used for enabling profiling in Production
-    // passed into alias object. Uses a flag if passed into the build command
+    const isEnvProduction = process.env.NODE_ENV === 'production';
     const isEnvProductionProfile = isEnvProduction && process.argv.includes('--profile');
-
-    // We will provide `paths.publicUrlOrPath` to our app
-    // as %PUBLIC_URL% in `index.html` and `process.env.PUBLIC_URL` in JavaScript.
-    // Omit trailing slash as %PUBLIC_URL%/xyz looks better than %PUBLIC_URL%xyz.
-    // Get environment variables to inject into our app.
-    const env = getClientEnvironment(paths.publicUrlOrPath.slice(0, -1));
+    const env = {
+        NODE_ENV: process.env.NODE_ENV,
+        PUBLIC_URL: '/henkilo-ui',
+        FAST_REFRESH: true,
+    };
     return {
         target: ['browserslist'],
         stats: 'errors-warnings',
@@ -68,8 +53,8 @@ module.exports = function (webpackEnv) {
             },
             compress: true,
             static: {
-                directory: paths.appPublic,
-                publicPath: [paths.publicUrlOrPath],
+                directory: path.resolve(__dirname, 'public'),
+                publicPath: ['/henkilo-ui/'],
                 watch: {
                     ignored: {},
                 },
@@ -81,12 +66,12 @@ module.exports = function (webpackEnv) {
                 },
             },
             devMiddleware: {
-                publicPath: paths.publicUrlOrPath.slice(0, -1),
+                publicPath: '/henkilo-ui/'.slice(0, -1),
             },
             host,
             historyApiFallback: {
                 disableDotRule: true,
-                index: paths.publicUrlOrPath,
+                index: '/henkilo-ui/',
             },
             port: 3000,
             proxy: [
@@ -104,36 +89,31 @@ module.exports = function (webpackEnv) {
                 },
             ],
         },
-        entry: paths.appIndexJs,
+        entry: path.resolve(__dirname, 'src', 'index.tsx'),
         output: {
-            path: paths.appBuild,
+            path: path.resolve(__dirname, 'build'),
             pathinfo: isEnvDevelopment,
             filename: isEnvProduction
                 ? 'static/js/[name].[contenthash:8].js'
                 : isEnvDevelopment && 'static/js/bundle.js',
-            // There are also additional JS chunk files if you use code splitting.
             chunkFilename: isEnvProduction
                 ? 'static/js/[name].[contenthash:8].chunk.js'
                 : isEnvDevelopment && 'static/js/[name].chunk.js',
             assetModuleFilename: 'static/media/[name].[hash][ext]',
-            // webpack uses `publicPath` to determine where the app is being served from.
-            // It requires a trailing slash, or the file assets will get an incorrect path.
-            // We inferred the "public path" (such as / or /my-project) from homepage.
-            publicPath: paths.publicUrlOrPath,
-            // Point sourcemap entries to original disk location (format as URL on Windows)
+            publicPath: '/henkilo-ui/',
             devtoolModuleFilenameTemplate: isEnvProduction
-                ? (info) => path.relative(paths.appSrc, info.absoluteResourcePath).replace(/\\/g, '/')
+                ? (info) => path.relative(path.resolve(__dirname, 'src'), info.absoluteResourcePath).replace(/\\/g, '/')
                 : isEnvDevelopment && ((info) => path.resolve(info.absoluteResourcePath).replace(/\\/g, '/')),
         },
         cache: {
             type: 'filesystem',
-            version: createEnvironmentHash(env.raw),
-            cacheDirectory: paths.appWebpackCache,
+            version: createEnvironmentHash(env),
+            cacheDirectory: path.resolve(__dirname, 'node_modules', '.cache'),
             store: 'pack',
             buildDependencies: {
                 defaultWebpack: ['webpack/lib/'],
                 config: [__filename],
-                tsconfig: [paths.appTsConfig],
+                tsconfig: [path.resolve(__dirname, 'tsconfig.json')],
             },
         },
         infrastructureLogging: {
@@ -145,38 +125,22 @@ module.exports = function (webpackEnv) {
                 new TerserPlugin({
                     terserOptions: {
                         parse: {
-                            // We want terser to parse ecma 8 code. However, we don't want it
-                            // to apply any minification steps that turns valid ecma 5 code
-                            // into invalid ecma 5 code. This is why the 'compress' and 'output'
-                            // sections only apply transformations that are ecma 5 safe
-                            // https://github.com/facebook/create-react-app/pull/4234
                             ecma: 8,
                         },
                         compress: {
                             ecma: 5,
                             warnings: false,
-                            // Disabled because of an issue with Uglify breaking seemingly valid code:
-                            // https://github.com/facebook/create-react-app/issues/2376
-                            // Pending further investigation:
-                            // https://github.com/mishoo/UglifyJS2/issues/2011
                             comparisons: false,
-                            // Disabled because of an issue with Terser breaking valid code:
-                            // https://github.com/facebook/create-react-app/issues/5250
-                            // Pending further investigation:
-                            // https://github.com/terser-js/terser/issues/120
                             inline: 2,
                         },
                         mangle: {
                             safari10: true,
                         },
-                        // Added for profiling in devtools
                         keep_classnames: isEnvProductionProfile,
                         keep_fnames: isEnvProductionProfile,
                         output: {
                             ecma: 5,
                             comments: false,
-                            // Turned on because emoji and regex is not minified properly using default
-                            // https://github.com/facebook/create-react-app/issues/2488
                             ascii_only: true,
                         },
                     },
@@ -184,39 +148,36 @@ module.exports = function (webpackEnv) {
             ],
         },
         resolve: {
-            // These are the reasonable defaults supported by the Node ecosystem.
-            // We also include JSX as a common component filename extension to support
-            // some tools, although we do not recommend using it, see:
-            // https://github.com/facebook/create-react-app/issues/290
-            // `web` extension prefixes have been added for better support
-            // for React Native Web.
-            extensions: paths.moduleFileExtensions.map((ext) => `.${ext}`),
+            extensions: [
+                '.web.mjs',
+                '.mjs',
+                '.web.js',
+                '.js',
+                '.web.ts',
+                '.ts',
+                '.web.tsx',
+                '.tsx',
+                '.json',
+                '.web.jsx',
+                '.jsx',
+            ],
             alias: {
-                // Allows for better profiling with ReactDevTools
                 ...(isEnvProductionProfile && {
                     'react-dom$': 'react-dom/profiling',
                     'scheduler/tracing': 'scheduler/tracing-profiling',
                 }),
-                ...(modules.webpackAliases || {}),
             },
         },
         module: {
             strictExportPresence: true,
             rules: [
-                // Handle node_modules packages that contain sourcemaps
                 shouldUseSourceMap && {
                     enforce: 'pre',
                     test: /\.(js|mjs|jsx|ts|tsx|css)$/,
                     loader: require.resolve('source-map-loader'),
                 },
                 {
-                    // "oneOf" will traverse all following loaders until one will
-                    // match the requirements. When no loader matches it will fall
-                    // back to the "file" loader at the end of the loader list.
                     oneOf: [
-                        // "url" loader works like "file" loader except that it embeds assets
-                        // smaller than specified limit in bytes as data URLs to avoid requests.
-                        // A missing `test` is equivalent to a match.
                         {
                             test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
                             type: 'asset',
@@ -277,16 +238,7 @@ module.exports = function (webpackEnv) {
                             test: cssModuleRegex,
                             use: ['style-loader', 'css-loader'],
                         },
-                        // "file" loader makes sure those assets get served by WebpackDevServer.
-                        // When you `import` an asset, you get its (virtual) filename.
-                        // In production, they would get copied to the `build` folder.
-                        // This loader doesn't use a "test" so it will catch all modules
-                        // that fall through the other loaders.
                         {
-                            // Exclude `js` files to keep "css" loader working as it injects
-                            // its runtime that would otherwise be processed through "file" loader.
-                            // Also exclude `html` and `json` extensions so they get processed
-                            // by webpacks internal loaders.
                             exclude: [/^$/, /\.(js|mjs|jsx|ts|tsx)$/, /\.html$/, /\.json$/],
                             type: 'asset/resource',
                         },
@@ -295,13 +247,12 @@ module.exports = function (webpackEnv) {
             ].filter(Boolean),
         },
         plugins: [
-            // Generates an `index.html` file with the <script> injected.
             new HtmlWebpackPlugin(
                 Object.assign(
                     {},
                     {
                         inject: true,
-                        template: paths.appHtml,
+                        template: path.resolve(__dirname, 'public', 'index.html'),
                     },
                     isEnvProduction
                         ? {
@@ -321,29 +272,13 @@ module.exports = function (webpackEnv) {
                         : undefined
                 )
             ),
-            // Inlines the webpack runtime script. This script is too small to warrant
-            // a network request.
-            // https://github.com/facebook/create-react-app/issues/5358
-            isEnvProduction &&
-                shouldInlineRuntimeChunk &&
-                new InlineChunkHtmlPlugin(HtmlWebpackPlugin, [/runtime-.+[.]js/]),
-            // Makes some environment variables available in index.html.
-            // The public URL is available as %PUBLIC_URL% in index.html, e.g.:
-            // <link rel="icon" href="%PUBLIC_URL%/favicon.ico">
-            // It will be an empty string unless you specify "homepage"
-            // in `package.json`, in which case it will be the pathname of that URL.
-            new InterpolateHtmlPlugin(HtmlWebpackPlugin, env.raw),
-            // This gives some necessary context to module not found errors, such as
-            // the requesting resource.
-            new ModuleNotFoundPlugin(paths.appPath),
-            // Makes some environment variables available to the JS code, for example:
-            // if (process.env.NODE_ENV === 'production') { ... }. See `./env.js`.
-            // It is absolutely essential that NODE_ENV is set to production
-            // during a production build.
-            // Otherwise React will be compiled in the very slow development mode.
-            new webpack.DefinePlugin(env.stringified),
-            // Experimental hot reloading for React .
-            // https://github.com/facebook/react/tree/main/packages/react-refresh
+            new webpack.DefinePlugin({
+                'process.env': {
+                    NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+                    PUBLIC_URL: '/henkilo-ui',
+                    FAST_REFRESH: 'true',
+                },
+            }),
             isEnvDevelopment &&
                 new ReactRefreshWebpackPlugin({
                     overlay: false,
@@ -355,15 +290,13 @@ module.exports = function (webpackEnv) {
                     filename: 'static/css/[name].[contenthash:8].css',
                     chunkFilename: 'static/css/[name].[contenthash:8].chunk.css',
                 }),
-            // Generate an asset manifest file with the following content:
-            // - "files" key: Mapping of all asset filenames to their corresponding
-            //   output file so that tools can pick it up without having to parse
-            //   `index.html`
-            // - "entrypoints" key: Array of files which are included in `index.html`,
-            //   can be used to reconstruct the HTML if necessary
+            isEnvProduction &&
+                new CopyWebpackPlugin({
+                    patterns: [{ from: path.resolve(__dirname, 'public', 'favicon.ico') }],
+                }),
             new WebpackManifestPlugin({
                 fileName: 'asset-manifest.json',
-                publicPath: paths.publicUrlOrPath,
+                publicPath: '/henkilo-ui/',
                 generate: (seed, files, entrypoints) => {
                     const manifestFiles = files.reduce((manifest, file) => {
                         manifest[file.name] = file.path;
@@ -377,11 +310,6 @@ module.exports = function (webpackEnv) {
                     };
                 },
             }),
-            // Moment.js is an extremely popular library that bundles large locale files
-            // by default due to how webpack interprets its code. This is a practical
-            // solution that requires the user to opt into importing specific locales.
-            // https://github.com/jmblog/how-to-optimize-momentjs-with-webpack
-            // You can remove this if you don't use Moment.js:
             new webpack.IgnorePlugin({
                 resourceRegExp: /^\.\/locale$/,
                 contextRegExp: /moment$/,
@@ -390,7 +318,7 @@ module.exports = function (webpackEnv) {
                 async: isEnvDevelopment,
                 typescript: {
                     typescriptPath: resolve.sync('typescript', {
-                        basedir: paths.appNodeModules,
+                        basedir: path.resolve(__dirname, 'node_modules'),
                     }),
                     configOverwrite: {
                         compilerOptions: {
@@ -400,21 +328,17 @@ module.exports = function (webpackEnv) {
                             declarationMap: false,
                             noEmit: true,
                             incremental: true,
-                            tsBuildInfoFile: paths.appTsBuildInfoFile,
+                            tsBuildInfoFile: path.resolve(__dirname, 'node_modules', '.cache', 'tsconfig.tsbuildinfo'),
                         },
                     },
-                    context: paths.appPath,
+                    context: path.resolve(__dirname),
                     diagnosticOptions: {
                         syntactic: true,
                     },
                     mode: 'write-references',
                 },
                 issue: {
-                    // This one is specifically to match during CI tests,
-                    // as micromatch doesn't match
-                    // '../cra-template-typescript/template/src/App.tsx'
-                    // otherwise.
-                    include: [{ file: '../**/src/**/*.{ts,tsx}' }, { file: '**/src/**/*.{ts,tsx}' }],
+                    include: [{ file: '**/src/**/*.{ts,tsx}' }],
                     exclude: [
                         { file: '**/src/**/__tests__/**' },
                         { file: '**/src/**/?(*.){spec|test}.*' },
@@ -422,12 +346,10 @@ module.exports = function (webpackEnv) {
                     ],
                 },
                 logger: {
-                    infrastructure: 'silent',
+                    log: (message) => console.log(message),
+                    error: (message) => console.error(message),
                 },
             }),
         ].filter(Boolean),
-        // Turn off performance processing because we utilize
-        // our own hints via the FileSizeReporter
-        performance: false,
     };
 };
