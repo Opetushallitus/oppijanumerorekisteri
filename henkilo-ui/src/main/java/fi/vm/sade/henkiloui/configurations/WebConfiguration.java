3ae653778bba76cfa030072231c8b5e66c81f6a8
Tomi Karlstedt <tomi.karlstedt@reaktor.com>
2023-11-03 11:09:38 +0200

    simplify webconfiguration

--- henkilo-ui/src/main/java/fi/vm/sade/henkiloui/configurations/WebConfiguration.java
+++ henkilo-ui/src/main/java/fi/vm/sade/henkiloui/configurations/WebConfiguration.java
@@ -1,28 +1,24 @@
 package fi.vm.sade.henkiloui.configurations;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
-
-    @Override
-    public void configurePathMatch(PathMatchConfigurer configurer) {
-        // Disable extensions causing trouble when OID is provided at the end of rest API.
-        configurer.setUseRegisteredSuffixPatternMatch(true);
-    }
-
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/kayttaja/")
+                .setViewName("forward:/kayttaja/index.html");
         registry.addViewController("/kayttaja/{spring:\\w+}")
-                .setViewName("forward:/kayttaja/");
+                .setViewName("forward:/kayttaja/index.html");
+        registry.addViewController("/kayttaja/**/{spring:\\w+}")
+                .setViewName("forward:/kayttaja/index.html");
+        registry.addViewController("/")
+                .setViewName("forward:/index.html");
         registry.addViewController("/{spring:\\w+}")
-                .setViewName("forward:/");
+                .setViewName("forward:/index.html");
         registry.addViewController("/**/{spring:\\w+}")
-                .setViewName("forward:/");
-        registry.addViewController("/{spring:\\w+}/**{spring:?!(\\.js|\\.css)$}")
-                .setViewName("forward:/");
+                .setViewName("forward:/index.html");
     }
 }
