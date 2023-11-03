c70dfc25840793c33a721d3c065eb7ab5aa7d2e3
Tomi Karlstedt <tomi.karlstedt@reaktor.com>
2023-11-03 14:07:25 +0200

    don't use query param for request cache

--- henkilo-ui/src/main/java/fi/vm/sade/henkiloui/configurations/security/SecurityConfiguration.java
+++ henkilo-ui/src/main/java/fi/vm/sade/henkiloui/configurations/security/SecurityConfiguration.java
@@ -18,9 +18,9 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.security.web.context.SecurityContextRepository;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
 
 @Configuration
 @EnableWebSecurity
@@ -94,6 +94,8 @@ public class SecurityConfiguration {
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http, CasAuthenticationFilter casAuthenticationFilter,
             AuthenticationEntryPoint authenticationEntryPoint, SecurityContextRepository securityContextRepository) throws Exception {
+        HttpSessionRequestCache requestCache = new HttpSessionRequestCache();
+        requestCache.setMatchingRequestParameterName(null);
         http
             .headers(headers -> headers.disable())
             .csrf(csrf -> csrf.disable())
@@ -117,6 +119,7 @@ public class SecurityConfiguration {
             .securityContext(securityContext -> securityContext
                 .requireExplicitSave(true)
                 .securityContextRepository(securityContextRepository))
+            .requestCache(cache -> cache.requestCache(requestCache))
             .exceptionHandling(handling -> handling.authenticationEntryPoint(authenticationEntryPoint));
         return http.build();
     }
