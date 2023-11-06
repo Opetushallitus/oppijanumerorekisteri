79e1059eb2fe5188d42c70a774e833c8802ad1be
Tomi Karlstedt <tomi.karlstedt@reaktor.com>
2023-11-06 14:58:31 +0200

    add the deprecated suffix pattern match back since OIDs clearly don't work without it

--- henkilo-ui/src/main/java/fi/vm/sade/henkiloui/configurations/WebConfiguration.java
+++ henkilo-ui/src/main/java/fi/vm/sade/henkiloui/configurations/WebConfiguration.java
@@ -1,11 +1,18 @@
 package fi.vm.sade.henkiloui.configurations;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        // Disable extensions causing trouble when OID is provided at the end of rest API.
+        configurer.setUseRegisteredSuffixPatternMatch(true);
+    }
+
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/kayttaja/")
