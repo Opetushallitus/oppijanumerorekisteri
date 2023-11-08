154736d5150fb335e75acb1bd692ca70a1089eaa
Tomi Karlstedt <tomi.karlstedt@reaktor.com>
2023-11-08 11:51:35 +0200

    forward paths with OIDs correctly

--- henkilo-ui/src/main/java/fi/vm/sade/henkiloui/configurations/WebConfiguration.java
+++ henkilo-ui/src/main/java/fi/vm/sade/henkiloui/configurations/WebConfiguration.java
@@ -12,8 +12,20 @@ public class WebConfiguration implements WebMvcConfigurer {
                  .setViewName("forward:/kayttaja.html");
         registry.addViewController("/kayttaja/**")
                  .setViewName("forward:/kayttaja.html");
+
+        // this does not forward paths with e.g. dots so things like favicon.ico (and indeed main.html) work
         registry.addViewController("/{path:\\w+}")
                  .setViewName("forward:/main.html");
+
+        // explicit paths where a path may include an OID (the !static controller below does not catch these for some reason)
+        registry.addViewController("/virkailija/**")
+                 .setViewName("forward:/main.html");
+        registry.addViewController("/oppija/**")
+                 .setViewName("forward:/main.html");
+        registry.addViewController("/admin/**")
+                 .setViewName("forward:/main.html");
+
+        // rest of the paths to non-static resources
         registry.addViewController("/{path:!static}/**")
                  .setViewName("forward:/main.html");
     }
