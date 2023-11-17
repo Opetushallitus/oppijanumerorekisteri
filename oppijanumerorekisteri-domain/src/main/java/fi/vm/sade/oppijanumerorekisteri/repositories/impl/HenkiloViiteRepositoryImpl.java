edc8b5d64357c717feec8203b05d6333e27f5b0e
Henry Heikkinen <henry.heikkinen@reaktor.com>
2023-11-17 11:17:09 +0200

    Simplify henkiloviite search query by removing joins that are unnecessary because of the new foreign key constraints

--- oppijanumerorekisteri-domain/src/main/java/fi/vm/sade/oppijanumerorekisteri/repositories/impl/HenkiloViiteRepositoryImpl.java
+++ oppijanumerorekisteri-domain/src/main/java/fi/vm/sade/oppijanumerorekisteri/repositories/impl/HenkiloViiteRepositoryImpl.java
@@ -11,24 +11,16 @@ import java.util.Set;
 
 
 public class HenkiloViiteRepositoryImpl extends AbstractRepository implements HenkiloViiteRepositoryCustom {
-
     @Override
     public List<HenkiloViiteDto> findBy(Set<String> oids) {
         if (!CollectionUtils.isEmpty(oids)) {
-            Query q = em.createNativeQuery("select distinct hv2.master_oid as masterOid, hv2.slave_oid as henkiloOid " +
-                    "        from henkiloViite " +
-                    "                 join henkiloViite as hv2 on henkiloViite.master_oid = hv2.master_oid " +
-                    "                 join henkilo as h1 on hv2.master_oid = h1.oidhenkilo " +
-                    "                 join henkilo as h2 on hv2.slave_oid = h2.oidhenkilo " +
-                    "        where henkiloviite.master_oid in ?1 or henkiloviite.slave_oid in ?1", "map_from_native");
+            Query q = em.createNativeQuery("SELECT DISTINCT masterviite.master_oid AS masteroid, masterviite.slave_oid AS henkilooid FROM henkiloviite" +
+                    " JOIN henkiloviite masterviite ON henkiloviite.master_oid = masterviite.master_oid" +
+                    " WHERE henkiloviite.master_oid IN ?1 OR henkiloviite.slave_oid IN ?1", "map_from_native");
             q.setParameter(1, oids);
             return q.getResultList();
         } else {
-            Query q = em.createNativeQuery("select distinct henkiloViite.master_oid as masterOid, henkiloViite.slave_oid as henkiloOid " +
-                            "        from henkiloViite " +
-                            "                 join henkilo as h1 on henkiloViite.master_oid = h1.oidhenkilo " +
-                            "                 join henkilo as h2 on henkiloViite.slave_oid = h2.oidhenkilo "
-                    , "map_from_native");
+            Query q = em.createNativeQuery("SELECT DISTINCT henkiloviite.master_oid AS masteroid, henkiloviite.slave_oid AS henkilooid FROM henkiloviite ", "map_from_native");
             return q.getResultList();
         }
     }
