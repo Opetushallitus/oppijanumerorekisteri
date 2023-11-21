58daed9c42205ec54dfd5201062d6bc4cf71e0f2
Henry Heikkinen <henry.heikkinen@reaktor.com>
2023-11-21 12:56:02 +0200

    Revert "Operate on Spring sessions without transaction to avoid locks when updating timestamps"
    This reverts commit d7aa9a8250ef3afd1aae4bc1c356237b945193da.

--- oppijanumerorekisteri-service/src/test/java/fi/vm/sade/oppijanumerorekisteri/LogSanitationTest.java
+++ oppijanumerorekisteri-service/src/test/java/fi/vm/sade/oppijanumerorekisteri/LogSanitationTest.java
@@ -7,6 +7,7 @@ import org.junit.runners.Parameterized;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestContextManager;
 import org.springframework.test.context.web.WebAppConfiguration;
 
@@ -27,6 +28,7 @@ import static org.junit.Assert.assertEquals;
 @RunWith(Parameterized.class)
 @WebAppConfiguration
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@ContextConfiguration(classes = {TestApplication.class})
 public class LogSanitationTest {
 
     private static final Logger logger = LoggerFactory.getLogger(LogSanitationTest.class);
