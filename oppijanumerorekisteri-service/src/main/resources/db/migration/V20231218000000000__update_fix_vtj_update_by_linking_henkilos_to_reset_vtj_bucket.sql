ed7c6f34ce9238666178ec35bf8c30bc312af376
Henry Heikkinen <henry.heikkinen@reaktor.com>
2023-12-18 14:32:58 +0200

    KJHH-2438 Clear vtj_bucket of duplicate when merging VTJ-yksilöity henkilös

---
+++ oppijanumerorekisteri-service/src/main/resources/db/migration/V20231218000000000__update_fix_vtj_update_by_linking_henkilos_to_reset_vtj_bucket.sql
@@ -0,0 +1,36 @@
+CREATE OR REPLACE FUNCTION fix_vtj_update_by_linking_henkilos(
+    original_oid text,
+    duplicate_oid text
+) RETURNS void AS $$
+DECLARE
+    current_hetu text;
+BEGIN
+    -- Store the new hetu from duplicate
+    SELECT hetu INTO current_hetu FROM henkilo WHERE oidhenkilo = duplicate_oid;
+
+    -- Remove VTJ ykislöinti from duplicate henkilo and mark it as duplicate
+    UPDATE henkilo
+    SET hetu = NULL,
+        yksiloityvtj = false,
+        vtj_register = false,
+        duplicate = true,
+        vtj_bucket = NULL,
+        modified = current_timestamp
+    WHERE oidhenkilo = duplicate_oid;
+
+    -- Move hetu history from duplicate to original
+    UPDATE henkilo_hetu
+    SET henkilo_id = (SELECT id FROM henkilo WHERE oidhenkilo = original_oid)
+    WHERE henkilo_id = (SELECT id FROM henkilo WHERE oidhenkilo = duplicate_oid);
+
+    -- Update current hetu for original henkilo
+    UPDATE henkilo
+    SET hetu = current_hetu,
+        modified = current_timestamp
+    WHERE oidhenkilo = original_oid;
+
+    -- Link the henkilos
+    INSERT INTO henkiloviite (version, master_oid, slave_oid, id)
+    VALUES (0, original_oid, duplicate_oid, nextval('hibernate_sequence'));
+END;
+$$ LANGUAGE plpgsql;
