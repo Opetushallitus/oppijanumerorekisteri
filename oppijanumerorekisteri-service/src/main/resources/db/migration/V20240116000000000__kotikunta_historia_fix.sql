8ddb042b3d7621260ce8168755778ec8a25f299b
Tomi Karlstedt <tomi.karlstedt@reaktor.com>
2024-01-16 10:38:52 +0200

    KJHH-2387 fix the previous fix

--- oppijanumerorekisteri-service/src/main/resources/db/migration/V20240116000000000__kotikunta_historia_fix.sql
+++ oppijanumerorekisteri-service/src/main/resources/db/migration/V20240116000000000__kotikunta_historia_fix.sql
@@ -1,3 +1,3 @@
 ALTER TABLE kotikunta_historia ALTER COLUMN kunnasta_pois_muuttopv DROP NOT NULL;
 
-ALTER TABLE tietoturva_kotikunta_historia ALTER COLUMN kunnasta_pois_muuttopv DROP NOT NULL;
+ALTER TABLE turvakielto_kotikunta_historia ALTER COLUMN kunnasta_pois_muuttopv DROP NOT NULL;
