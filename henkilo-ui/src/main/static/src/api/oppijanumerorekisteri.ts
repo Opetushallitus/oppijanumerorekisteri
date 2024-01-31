a162f7b9c427085a9c2073252e3bcefd94e53f3f
Henry Heikkinen <henry.heikkinen@reaktor.com>
2024-01-31 13:31:14 +0200

    KJHH-2458 Fix setting UI language based on user's asiointikieli

--- henkilo-ui/src/main/static/src/api/oppijanumerorekisteri.ts
+++ henkilo-ui/src/main/static/src/api/oppijanumerorekisteri.ts
@@ -57,7 +57,7 @@ export const oppijanumerorekisteriApi = createApi({
             }),
             async onQueryStarted(_, { dispatch, queryFulfilled }) {
                 const { data } = await queryFulfilled;
-                dispatch({ type: FETCH_HENKILO_ASIOINTIKIELI_SUCCESS, data });
+                dispatch({ type: FETCH_HENKILO_ASIOINTIKIELI_SUCCESS, lang: data });
                 dispatch({ type: LOCATION_CHANGE });
             },
             providesTags: ['locale'],
