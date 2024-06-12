419ab876e4ee71833260f0f36e4814b9110ca959
Henry Heikkinen <henry.heikkinen@reaktor.com>
2024-06-12 12:07:53 +0300

    OPHYK-156 Disable Base64 encoding of session identifiers
    Sometimes on startup Tomcat generates session ids using
    org.apache.catalina.util.StandardSessionIdGenerator which produces
    random bytes as hex string. Spring Session expects Base64 encoded UUIDs
    as session ids by default as implemented by the combination of
    org.springframework.session.web.http.DefaultCookieSerializer and
    org.springframework.session.UuidSessionIdGenerator.
    
    Treat them as opaque string tokens instead of Base64 encoded UUID to
    avoid decoding them to strings containing zero bytes which causes
    issues.
    
    Fix described here but the root cause is not clear yet:
    https://github.com/spring-projects/spring-session/issues/1201#issuecomment-942644892

--- oppijanumerorekisteri-service/src/main/java/fi/vm/sade/oppijanumerorekisteri/configurations/CookieConfiguration.java
+++ oppijanumerorekisteri-service/src/main/java/fi/vm/sade/oppijanumerorekisteri/configurations/CookieConfiguration.java
@@ -1,7 +1,13 @@
 package fi.vm.sade.oppijanumerorekisteri.configurations;
 
+import org.springframework.boot.autoconfigure.session.DefaultCookieSerializerCustomizer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class CookieConfiguration {
+    @Bean
+    public DefaultCookieSerializerCustomizer cookieSerializerCustomizer() {
+        return cookieSerializer -> cookieSerializer.setUseBase64Encoding(false);
+    }
 }
