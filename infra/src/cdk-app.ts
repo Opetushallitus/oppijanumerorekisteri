5eb9be499d8d43f1164e9cefb7fa604de7fb67d3
Timo Suomela <timo.suomela@reaktor.com>
2024-11-21 12:09:05 +0200

    OPHYK-206 Add subdomain and point it to a loadbalancer

--- infra/src/cdk-app.ts
+++ infra/src/cdk-app.ts
@@ -4,6 +4,11 @@ import * as ec2 from "aws-cdk-lib/aws-ec2";
 import * as ecs from "aws-cdk-lib/aws-ecs";
 import * as rds from "aws-cdk-lib/aws-rds"
 import * as s3 from "aws-cdk-lib/aws-s3"
+import * as elasticloadbalancingv2 from "aws-cdk-lib/aws-elasticloadbalancingv2";
+import * as route53 from "aws-cdk-lib/aws-route53";
+import * as route53_targets from "aws-cdk-lib/aws-route53-targets";
+import * as ssm from "aws-cdk-lib/aws-ssm";
+import * as certificatemanager from "aws-cdk-lib/aws-certificatemanager";
 import * as sharedAccount from "./shared-account";
 
 class CdkApp extends cdk.App {
@@ -18,6 +23,7 @@ class CdkApp extends cdk.App {
 
     new ECSStack(this, sharedAccount.prefix("ECSStack"), stackProps);
     new DatabaseStack(this, sharedAccount.prefix("Database"), stackProps);
+    new OppijanumerorekisteriApplicationStack(this, sharedAccount.prefix("OppijanumerorekisteriApplication"), stackProps);
   }
 }
 
@@ -67,6 +73,53 @@ class DatabaseStack extends cdk.Stack {
       readers: [],
       s3ExportBuckets: [exportBucket],
     });
+  }
+}
+
+class OppijanumerorekisteriApplicationStack extends cdk.Stack {
+  constructor(
+      scope: constructs.Construct,
+      id: string,
+      props: cdk.StackProps
+  ) {
+    super(scope, id, props);
+    const vpc = ec2.Vpc.fromLookup(this, "Vpc", {vpcName: sharedAccount.VPC_NAME});
+
+    const alb = new elasticloadbalancingv2.ApplicationLoadBalancer(
+        this,
+        "LoadBalancer",
+        {
+          vpc,
+          internetFacing: true,
+        }
+    );
+
+    const sharedHostedZone = route53.HostedZone.fromLookup(
+        this,
+        "YleiskayttoisetHostedZone",
+        {
+          domainName: ssm.StringParameter.valueFromLookup(this, "zoneName"),
+        }
+    );
+    const albHostname = `oppijanumerorekisteri.${sharedHostedZone.zoneName}`;
+
+    new route53.ARecord(this, "ALBARecord", {
+      zone: sharedHostedZone,
+      recordName: albHostname,
+      target: route53.RecordTarget.fromAlias(
+          new route53_targets.LoadBalancerTarget(alb)
+      ),
+    });
+
+    new certificatemanager.Certificate(
+        this,
+        "AlbCertificate",
+        {
+          domainName: albHostname,
+          validation:
+              certificatemanager.CertificateValidation.fromDns(sharedHostedZone),
+        }
+    );
   }
 }
 
