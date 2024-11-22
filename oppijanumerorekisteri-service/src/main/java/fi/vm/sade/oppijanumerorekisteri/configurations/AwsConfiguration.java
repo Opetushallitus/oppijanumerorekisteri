02a1040fbfdabc17ce76968f0588699cc1a19680
Henry Heikkinen <henry.heikkinen@reaktor.com>
2024-11-22 17:00:29 +0200

    OPHYK-206 Assume role to publish SNS messages
    Also clarify naming of AWS credential providers and clients

--- oppijanumerorekisteri-service/src/main/java/fi/vm/sade/oppijanumerorekisteri/configurations/AwsConfiguration.java
+++ oppijanumerorekisteri-service/src/main/java/fi/vm/sade/oppijanumerorekisteri/configurations/AwsConfiguration.java
@@ -2,6 +2,8 @@ package fi.vm.sade.oppijanumerorekisteri.configurations;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
@@ -13,13 +15,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.services.sts.auth.StsAssumeRoleCredentialsProvider;
+import software.amazon.awssdk.services.sts.model.AssumeRoleRequest;
 
 @Getter
 @Setter
 @Configuration
 @ConfigurationProperties(prefix = "aws")
 public class AwsConfiguration {
+    public static final String ONR_CREDENTIALS_PROVIDER = "onrCredentialsProvider";
+    public static final String OPINTOPOLKU_CREDENTIALS_PROVIDER = "opintopolkuCredentialsProvider";
     private String region;
+    private String opintopolkuRoleArn;
     private HenkiloModifiedTopic henkiloModifiedTopic = new HenkiloModifiedTopic();
 
     @Getter
@@ -29,24 +36,40 @@ public class AwsConfiguration {
         private String topicArn;
     }
 
-    @Bean("opintopolkuCredentialsProvider")
-    AwsCredentialsProvider getAwsCredentialsProvider() {
+    @Bean(ONR_CREDENTIALS_PROVIDER)
+    AwsCredentialsProvider onrCredentialsProvider() {
         return DefaultCredentialsProvider.create();
     }
 
     @Bean
     @Autowired
-    StsClient getStsClient(AwsCredentialsProvider opintopolkuCredentialsProvider) {
+    StsClient getStsClient(@Qualifier(ONR_CREDENTIALS_PROVIDER) AwsCredentialsProvider onrCredentialsProvider) {
         return StsClient
                 .builder()
                 .region(Region.of(region))
-                .credentialsProvider(opintopolkuCredentialsProvider)
+                .credentialsProvider(onrCredentialsProvider)
+                .build();
+    }
+
+    @Bean(OPINTOPOLKU_CREDENTIALS_PROVIDER)
+    AwsCredentialsProvider opintopolkuCredentialsProvider(@Qualifier(ONR_CREDENTIALS_PROVIDER) AwsCredentialsProvider onrCredentialsProvider) {
+        var stsClient = StsClient.builder()
+                .credentialsProvider(onrCredentialsProvider)
+                .region(Region.of(region))
+                .build();
+
+        return StsAssumeRoleCredentialsProvider.builder()
+                .stsClient(stsClient)
+                .refreshRequest(() -> AssumeRoleRequest.builder()
+                        .roleArn(opintopolkuRoleArn)
+                        .roleSessionName("onr-henkilo-modified-sns-publish")
+                        .build())
                 .build();
     }
 
     @Bean
     @Autowired
-    SnsClient getSnsClient(AwsCredentialsProvider opintopolkuCredentialsProvider) {
+    SnsClient opintopolkuSnsClient(@Qualifier(OPINTOPOLKU_CREDENTIALS_PROVIDER) AwsCredentialsProvider opintopolkuCredentialsProvider) {
         return SnsClient
                 .builder()
                 .region(Region.of(region))
@@ -55,9 +78,9 @@ public class AwsConfiguration {
     }
 
     @Bean
-    public S3AsyncClient opintopolkuS3Client(AwsCredentialsProvider opintopolkuCredentialsProvider) {
+    public S3AsyncClient onrS3Client(@Qualifier(ONR_CREDENTIALS_PROVIDER) AwsCredentialsProvider onrCredentialsProvider) {
         return S3AsyncClient.builder()
-                .credentialsProvider(opintopolkuCredentialsProvider)
+                .credentialsProvider(onrCredentialsProvider)
                 .region(Region.of(region))
                 .build();
     }
