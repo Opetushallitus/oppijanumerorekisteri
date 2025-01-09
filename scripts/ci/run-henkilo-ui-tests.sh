2364424d5fd79272560a437101df7df3d89711b3
Tomi Karlstedt <tomi.karlstedt@reaktor.com>
2025-01-09 13:58:43 +0200

    OPHYK-208 install playwright deps on CI, try 2

--- scripts/ci/run-henkilo-ui-tests.sh
+++ scripts/ci/run-henkilo-ui-tests.sh
@@ -4,8 +4,6 @@ set -o errexit -o nounset -o pipefail
 repo="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd ../.. && pwd )"
 source "${repo}/scripts/lib/common-functions.sh"
 
-trap cleanup EXIT
-
 function main {
   init_nodejs
 
@@ -15,8 +13,8 @@ function main {
 
   npm run lint
   npm run prettier
-  npx playwright install-deps
   npm run e2e:install-deps
+  npm run e2e:chromium
   CI=true npm run e2e:ci
 }
 
