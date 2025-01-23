ea12db7f3a4f028f42030b9c5800b1439b9b503e
Timo Suomela <timo.suomela@reaktor.com>
2025-01-23 14:26:21 +0200

    OPHYK-401 Remove redundant imports

--- infra/src/cdk-app.ts
+++ infra/src/cdk-app.ts
@@ -1,7 +1,5 @@
 import * as cdk from "aws-cdk-lib";
 import * as constructs from "constructs";
-import * as cloudwatch from "aws-cdk-lib/aws-cloudwatch";
-import * as cloudwatch_actions from "aws-cdk-lib/aws-cloudwatch-actions";
 import * as ec2 from "aws-cdk-lib/aws-ec2";
 import * as ecs from "aws-cdk-lib/aws-ecs";
 import * as iam from "aws-cdk-lib/aws-iam";
