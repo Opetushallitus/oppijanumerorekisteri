30646ff4796a72d7cd6e34f55dea65fc1e0124d3
Tomi Karlstedt <tomi.karlstedt@reaktor.com>
2025-04-01 09:33:10 +0300

    OPHYK-332 use different OID node value for new henkilos in test environments

--- oppijanumerorekisteri-service/src/main/java/fi/vm/sade/oppijanumerorekisteri/services/impl/OidGeneratorImpl.java
+++ oppijanumerorekisteri-service/src/main/java/fi/vm/sade/oppijanumerorekisteri/services/impl/OidGeneratorImpl.java
@@ -2,11 +2,16 @@ package fi.vm.sade.oppijanumerorekisteri.services.impl;
 
 import fi.vm.sade.oidgenerator.OIDGenerator;
 import fi.vm.sade.oppijanumerorekisteri.services.OidGenerator;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class OidGeneratorImpl implements OidGenerator {
+    @Value("${oppijanumerorekisteri.henkilo.solmuluokka}")
+    private String solmuluokka;
+
     public String generateOID() {
-        return OIDGenerator.generateOID(OIDGenerator.HENKILO_OID_NODE); // <- 1.2.246.562.24 is the OID node for a persons.
+        return OIDGenerator.generateOID(Integer.parseInt(solmuluokka));
     }
 }
