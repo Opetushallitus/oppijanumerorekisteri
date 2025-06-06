c250ca1d613b1907319e4f5a802d57c2b7d60aff
Tomi Karlstedt <tomi.karlstedt@reaktor.com>
2025-06-06 15:51:15 +0300

    OPHYK-420 add a playwright test for filtering kayttooikeusryhma list by kayttooikeus

---
+++ henkilo-ui/src/main/static/e2e/app/kayttooikeusryhmat.spec.ts
@@ -0,0 +1,34 @@
+import { test, expect } from '@playwright/test';
+
+import kayttooikeusryhmat from '../../mock-api/src/api/kayttooikeus-service/kayttooikeusryhma/GET.json';
+
+test.describe('kayttooikeusryhmat list', () => {
+    test('filters by kayttooikeus', async ({ page }) => {
+        await page.goto('/henkilo-ui/kayttooikeusryhmat');
+        await expect(page.locator('.oph-ds-accordion-header')).toHaveCount(3);
+
+        await page.click('.oph-ds-accordion-header:first-child');
+        await expect(page.locator("span:has-text('kuvauds2')")).toBeVisible();
+
+        await page.route(
+            '/kayttooikeus-service/kayttooikeusryhma?passiiviset=false&palvelu=GAGA&kayttooikeus=GUGU',
+            async (route) => {
+                await route.fulfill({ json: [kayttooikeusryhmat[0]] });
+            }
+        );
+
+        await page.type('#react-select-2-input', 'palvelu');
+        await page.keyboard.press('Enter');
+
+        await expect(page.locator('#react-select-3-input')).toBeEditable();
+        await page.type('#react-select-3-input', 'gugu');
+        await page.keyboard.press('Enter');
+
+        await expect(page.locator('.oph-ds-accordion-header')).toHaveCount(1);
+        await page.click('.oph-ds-accordion-header:first-child');
+        await expect(page.locator("span:has-text('kuvaus1')")).toBeVisible();
+
+        await page.click('.oph-ds-select-clearIndicator:first-child');
+        await expect(page.locator('.oph-ds-accordion-header')).toHaveCount(3);
+    });
+});
