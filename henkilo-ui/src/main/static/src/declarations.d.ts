03ea509f8db01b84f81ffc7691c29f4ae7937b20
Tomi Karlstedt <tomi.karlstedt@reaktor.com>
2025-07-22 13:40:50 +0300

    downgrade react-router to 7.6.3 and add type declarations to fix problems with react 18 types

--- henkilo-ui/src/main/static/src/declarations.d.ts
+++ henkilo-ui/src/main/static/src/declarations.d.ts
@@ -1,7 +1,14 @@
 export {};
 
+import R from 'react';
+
 declare global {
     interface Window {
         opintopolku_caller_id?: string;
+    }
+
+    // fix for react-visibility-sensor types
+    namespace React {
+        type StatelessComponent<P> = R.FunctionComponent<P>;
     }
 }
