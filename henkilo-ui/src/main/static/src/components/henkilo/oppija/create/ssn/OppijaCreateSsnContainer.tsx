31fb6a21c9ef3432b7383e019c8c4500878331d5
Tomi Karlstedt <tomi.karlstedt@reaktor.com>
2025-07-23 15:39:28 +0300

    remove oppijanumero check and create state from redux store

--- henkilo-ui/src/main/static/src/components/henkilo/oppija/create/ssn/OppijaCreateSsnContainer.tsx
+++ henkilo-ui/src/main/static/src/components/henkilo/oppija/create/ssn/OppijaCreateSsnContainer.tsx
@@ -1,95 +1,204 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import type { RootState } from '../../../../../store';
-import type { ExistenceCheckRequest, ExistenceCheckState } from '../../../../../reducers/existence.reducer';
-import { doExistenceCheck, clearExistenceCheck } from '../../../../../actions/existence.actions';
-import type { CreatePersonState } from '../../../../../reducers/create.reducer';
-import { createPerson } from '../../../../../actions/create.actions';
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { joiResolver } from '@hookform/resolvers/joi';
+import Joi from 'joi';
+import classNames from 'classnames';
+import ReactMarkdown from 'react-markdown';
+
 import CloseButton from '../../../../common/button/CloseButton';
-import Create from './Create';
-import DetailsForm from './DetailsForm';
+import { useLocalisations } from '../../../../../selectors';
+import Button from '../../../../common/button/Button';
+import { SpinnerInButton } from '../../../../common/icons/SpinnerInButton';
+import {
+    CreateHenkiloRequest,
+    useCreateHenkiloMutation,
+    useHenkiloExistsMutation,
+} from '../../../../../api/oppijanumerorekisteri';
+import CopyToClipboard from './CopyToClipboard';
+
+import './DetailsForm.css';
 
 type OwnProps = {
     goBack: () => void;
 };
 
-type StateProps = {
-    existence: ExistenceCheckState;
-    person: CreatePersonState;
-    translate: (key: string) => string;
+export const schema = Joi.object({
+    hetu: Joi.string()
+        .trim(true)
+        .regex(/^\d{6}[ABCDEFYXWVU+-]\d{3}[0123456789ABCDEFHJKLMNPRSTUVWXY]$/)
+        .required(),
+    etunimet: Joi.string().trim(true).required(),
+    kutsumanimi: Joi.string()
+        .trim(true)
+        .custom((kutsumanimi, { state }) => {
+            const firstnames = state.ancestors[0]['etunimet']
+                .split(/\s/)
+                .flatMap((s: string) => [s, ...s.split('-')])
+                .map((s: string) => s.toLowerCase())
+                .filter((value: string, index: number, arr: string[]) => arr.indexOf(value) === index);
+            if (firstnames.includes(kutsumanimi.toLowerCase())) return kutsumanimi;
+            throw new Error();
+        })
+        .required(),
+    sukunimi: Joi.string().trim(true).required(),
+});
+
+type FormField = {
+    name: keyof CreateHenkiloRequest;
+    localizationKey: string;
 };
 
-type DispatchProps = {
-    clearExistenceCheck: () => void;
-    doExistenceCheck: (payload: ExistenceCheckRequest) => void;
-    createPerson: (payload: ExistenceCheckRequest) => void;
+const formFields: FormField[] = [
+    {
+        name: 'hetu',
+        localizationKey: 'HENKILO_HETU',
+    },
+    {
+        name: 'etunimet',
+        localizationKey: 'HENKILO_ETUNIMET',
+    },
+    {
+        name: 'kutsumanimi',
+        localizationKey: 'HENKILO_KUTSUMANIMI',
+    },
+    {
+        name: 'sukunimi',
+        localizationKey: 'HENKILO_SUKUNIMI',
+    },
+];
+
+const formFieldErrors: Record<string, string> = {
+    'string.empty': 'LOMAKE_PAKOLLINEN_TIETO',
+    'any.custom': 'HENKILO_KUTSUMANIMI_VALIDOINTI',
 };
 
-type Props = OwnProps & StateProps & DispatchProps;
+export const statusToMessage = {
+    200: 'EXISTENCE_CHECK_ONR',
+    204: 'EXISTENCE_CHECK_VTJ',
+    400: 'EXISTENCE_CHECK_BAD_REQUEST',
+    404: 'EXISTENCE_CHECK_NOT_FOUND',
+    409: 'EXISTENCE_CHECK_CONFLICT',
+};
 
-export const OppijaCreateSsnContainer = ({
-    goBack,
-    translate,
-    person,
-    existence,
-    doExistenceCheck,
-    clearExistenceCheck,
-    createPerson,
-}: Props) => {
-    const [create, setCreate] = React.useState<boolean>(false);
-    const [data, setData] = React.useState<ExistenceCheckRequest>();
+const resolveErrorKey = (key: string): string => formFieldErrors[key] || 'LOMAKE_KENTTA_SISALTAA_VIRHEITA';
+
+export const OppijaCreateSsnContainer = ({ goBack }: OwnProps) => {
+    const { L } = useLocalisations();
+    const [henkiloExists, { isLoading }] = useHenkiloExistsMutation({});
+    const [createHenkilo] = useCreateHenkiloMutation();
+
+    const [existingOid, setExistingOid] = useState<string>();
+    const [existingStatus, setExistingStatus] = useState<number>();
+    const [existingMessage, setExistingMessage] = useState<string>();
+    const [createOid, setCreateOid] = useState<string>();
+    const [createError, setCreateError] = useState(false);
+
+    const {
+        register,
+        handleSubmit,
+        reset,
+        formState: { errors, isValid },
+        getValues,
+    } = useForm<CreateHenkiloRequest>({ resolver: joiResolver(schema), mode: 'onChange' });
+
+    const handleExists = async (data: CreateHenkiloRequest) => {
+        await henkiloExists(data)
+            .unwrap()
+            .then(({ oid, status }) => {
+                setExistingOid(oid);
+                setExistingStatus(status);
+                setExistingMessage(statusToMessage[status]);
+            })
+            .catch(({ status }) => {
+                setExistingStatus(status);
+                setExistingMessage(statusToMessage[status]);
+            });
+    };
+
+    const handleCreate = async () => {
+        await createHenkilo(getValues())
+            .unwrap()
+            .then(setCreateOid)
+            .catch(() => setCreateError(true));
+    };
+
+    const resetState = () => {
+        setExistingOid(undefined);
+        setExistingMessage(undefined);
+        setExistingStatus(undefined);
+        setCreateOid(undefined);
+        setCreateError(false);
+        reset();
+    };
 
     return (
         <div className="mainContent wrapper">
-            <span className="oph-h2 oph-bold">{translate('OPPIJAN_LUONTI_OTSIKKO')}</span>
+            <span className="oph-h2 oph-bold">{L['OPPIJAN_LUONTI_OTSIKKO']}</span>
             <span className="float-right">
                 <CloseButton closeAction={goBack} />
             </span>
             <div className="oph-field">
-                {create ? (
-                    <Create
-                        {...{
-                            ...person,
-                            payload: data,
-                            translate,
-                            createPerson,
-                            reset: () => {
-                                clearExistenceCheck();
-                                setData(undefined);
-                                setCreate(false);
-                            },
-                        }}
-                    />
-                ) : (
-                    <DetailsForm
-                        {...{
-                            ...existence,
-                            translate,
-                            doExistenceCheck: doExistenceCheck,
-                            clearExistenceCheck: clearExistenceCheck,
-                            cache: setData,
-                            create: () => setCreate(true),
-                        }}
-                    />
+                {createOid && (
+                    <>
+                        <div className="create-result oph-alert-success">
+                            <ReactMarkdown>{L['CREATE_PERSON_SUCCESS']}</ReactMarkdown>
+                            <CopyToClipboard text={createOid} L={L} />
+                        </div>
+                        <Button action={resetState}>{L['HENKILO_LUOYHTEYSTIETO']}</Button>
+                    </>
+                )}
+                {createOid && (
+                    <div className="create-result oph-alert-error">
+                        <ReactMarkdown>{L['CREATE_PERSON_FAILURE']}</ReactMarkdown>
+                    </div>
+                )}
+                {existingMessage && (
+                    <div
+                        className={classNames('check-result', {
+                            'oph-alert-success': existingStatus === 200,
+                            'oph-alert-info': existingStatus === 204,
+                            'oph-alert-error': existingStatus >= 400,
+                        })}
+                    >
+                        <ReactMarkdown>{L[existingMessage]}</ReactMarkdown>
+                        {existingStatus === 200 && <CopyToClipboard text={existingOid} L={L} />}
+                        {existingStatus === 204 && <Button action={handleCreate}>{L['HENKILO_LUOYHTEYSTIETO']}</Button>}
+                    </div>
+                )}
+                {!createOid && !createError && (
+                    <form>
+                        {formFields.map((field) => (
+                            <div className="oph-field oph-field-is-required" key={field.name}>
+                                <label className="oph-label">{L[field.localizationKey]}</label>
+                                <input
+                                    className={classNames('oph-input', {
+                                        'oph-input-has-error': !!errors[field.name],
+                                    })}
+                                    placeholder={L[field.localizationKey]}
+                                    type="text"
+                                    {...register(field.name)}
+                                />
+                                {!!errors[field.name] && (
+                                    <div className="oph-field-text oph-error">
+                                        {L[resolveErrorKey(errors[field.name].type)]}
+                                    </div>
+                                )}
+                            </div>
+                        ))}
+                        <div className="oph-field">
+                            <Button action={handleSubmit(handleExists)} disabled={!isValid || isLoading}>
+                                <SpinnerInButton show={isLoading} />
+                                {L['KUTSUTUT_VIRKAILIJAT_HAKU_HENKILO']}
+                            </Button>
+                            <Button className="margin-left" action={() => resetState()} disabled={isLoading}>
+                                {L['TYHJENNA']}
+                            </Button>
+                        </div>
+                    </form>
                 )}
             </div>
         </div>
     );
 };
 
-const mapStateToProps = (state: RootState): StateProps => ({
-    existence: state.existenceCheck,
-    person: state.createPerson,
-    translate: (key: string) => state.l10n.localisations[state.locale][key] || key,
-});
-
-const mapDispatchToProps = {
-    clearExistenceCheck: clearExistenceCheck,
-    doExistenceCheck: doExistenceCheck,
-    createPerson: createPerson,
-};
-
-export default connect<StateProps, DispatchProps, OwnProps, RootState>(
-    mapStateToProps,
-    mapDispatchToProps
-)(OppijaCreateSsnContainer);
+export default OppijaCreateSsnContainer;
