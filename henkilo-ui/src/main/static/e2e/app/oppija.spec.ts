4d3cf7df4cc22d9cc08aa0636cf43cdda8ff6c21
Henry Heikkinen <henry.heikkinen@reaktor.com>
2025-09-26 14:07:57 +0300

    OPHYK-810 Add basic test case for korkeakoulu pääkäyttäjä on oppija view
    Roles in /omattiedot are based on the following käyttöoikeusryhmäs from
    QA environment:
    
    - PROD Yleiskäyttöisten palvelujen kehittäjä
    - PROD Pääkäyttäjä (kk)

--- henkilo-ui/src/main/static/e2e/app/oppija.spec.ts
+++ henkilo-ui/src/main/static/e2e/app/oppija.spec.ts
@@ -1,10 +1,45 @@
-import { expect, test } from '@playwright/test';
+import { expect, Page, test } from '@playwright/test';
+import { readFileSync } from 'node:fs';
+
+const TESTIAINEISTO_GRETA_OID = '1.2.246.562.98.24707445854';
 
 test.describe('oppija view', () => {
-    test('shows basic information', async ({ page }) => {
-        const testiaineistoGretaOid = '1.2.246.562.98.24707445854';
-        await page.goto(`/henkilo-ui/oppija/${testiaineistoGretaOid}?permissionCheckService=ATARU`);
-        await expect(page.locator('div#HENKILO_ETUNIMET span.field')).toHaveText('Greta');
-        await expect(page.locator('div#HENKILO_SUKUNIMI span.field')).toHaveText('Denimman');
+    test('shows basic information for kehittaja', async ({ page }) => {
+        await overrideOmattiedot(page, JSON.parse(readFileSync('./e2e/app/omattiedot-kehittaja.json', 'utf8')));
+        const oppijaPage = await gotoOppijaView(page, TESTIAINEISTO_GRETA_OID);
+        await expect(oppijaPage.etunimet).toHaveText('Greta');
+        await expect(oppijaPage.sukunimi).toHaveText('Denimman');
+        await expect(oppijaPage.henkilotunnisteet).toBeVisible();
+    });
+
+    test('shows basic information for korkeakoulun pääkäyttäjä', async ({ page }) => {
+        // HUOM: Tämä testi ei täysin vastaa todellisuutta koska:
+        // - mockatut vastaukset ei tee mitään käyttöoikeustarkistuksia
+        // - joten 401 vastausten käsittely ei pääse tässä testissä oikeasti tapahtumaan
+        // - käytännössä testaa että käli näyttää tai on näyttämättä elementtejä käyttöoikeuksien perusteella
+        await overrideOmattiedot(page, JSON.parse(readFileSync('./e2e/app/omattiedot-kk-paakayttaja.json', 'utf8')));
+        const oppijaPage = await gotoOppijaView(page, TESTIAINEISTO_GRETA_OID);
+        await expect(oppijaPage.etunimet).toHaveText('Greta');
+        await expect(oppijaPage.sukunimi).toHaveText('Denimman');
+        await expect(oppijaPage.henkilotunnisteet).toHaveCount(0);
     });
 });
+
+async function gotoOppijaView(page: Page, oid: string) {
+    await page.goto(`/henkilo-ui/oppija/${oid}?permissionCheckService=ATARU`);
+    return {
+        etunimet: page.locator('div#HENKILO_ETUNIMET span.field'),
+        sukunimi: page.locator('div#HENKILO_SUKUNIMI span.field'),
+        henkilotunnisteet: page.getByText('Henkilötunnisteet'),
+    };
+}
+
+async function overrideOmattiedot(page: Page, json: object) {
+    await mockRoute(page, '/kayttooikeus-service/henkilo/current/omattiedot', { json });
+}
+
+async function mockRoute(page: Page, endpoint: string, options: { body?: string; json?: object }) {
+    await page.route(endpoint, async (route) => {
+        await route.fulfill(options);
+    });
+}
