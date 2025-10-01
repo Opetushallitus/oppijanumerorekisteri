e82524cb2ffa9aa7e5b5e6058527c31f8b7707b5
Tomi Karlstedt <tomi.karlstedt@reaktor.com>
2025-10-01 11:14:26 +0300

    remove named span component

--- henkilo-ui/src/main/static/src/components/kayttooikeusryhmat/listaus/KayttooikeusryhmaTiedot.tsx
+++ henkilo-ui/src/main/static/src/components/kayttooikeusryhmat/listaus/KayttooikeusryhmaTiedot.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { Link } from 'react-router';
 
 import { Kayttooikeusryhma } from '../../../types/domain/kayttooikeus/kayttooikeusryhma.types';
-import LocalizedTextGroup from '../../common/LocalizedTextGroup';
 import { Localisations } from '../../../types/localisation.type';
 import { Locale } from '../../../types/locale.type';
 import { useGetKayttooikeusryhmaRoolisQuery } from '../../../api/kayttooikeus';
+import { localizeTextGroup } from '../../../utilities/localisation.util';
 
 import './KayttooikeusryhmaTiedot.css';
 
@@ -25,17 +25,17 @@ const KayttooikeusryhmaTiedot = ({ item, L, locale, muokkausoikeus, show }: Prop
 
     return show ? (
         <div className="kayttooikeusryhma-tiedot">
-            <LocalizedTextGroup texts={kuvaus} locale={locale}></LocalizedTextGroup>
+            <span>{localizeTextGroup(kuvaus, locale)}</span>
             <div className="kayttooikeusryhma-tiedot-roolit">
                 <div className="kayttooikeusryhma-tiedot-otsikko">{L['KAYTTOOIKEUSRYHMAT_LISAA_PALVELU']}</div>
                 <div className="kayttooikeusryhma-tiedot-otsikko">{L['KAYTTOOIKEUSRYHMAT_LISAA_KAYTTOOIKEUS']}</div>
                 {(palveluRoolit ?? []).map((item, index) => (
                     <React.Fragment key={index + item.palveluTexts[0].text}>
                         <div className="kayttooikeusryhma-tiedot-palvelu">
-                            <LocalizedTextGroup locale={locale} texts={item.palveluTexts} />
+                            <span>{localizeTextGroup(item.palveluTexts, locale)}</span>
                         </div>
                         <div className="kayttooikeusryhma-tiedot-rooli">
-                            <LocalizedTextGroup locale={locale} texts={item.rooliTexts} />
+                            <span>{localizeTextGroup(item.rooliTexts, locale)}</span>
                         </div>
                     </React.Fragment>
                 ))}
