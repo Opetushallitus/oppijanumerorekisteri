3e2e24beee2bfa9b4867e54687f4e6f8ed5a3f41
Tomi Karlstedt <tomi.karlstedt@reaktor.com>
2025-11-06 10:56:15 +0200

    fix navigation tabs to show correct active tabs

--- henkilo-ui/src/main/static/src/components/navigation/TopNavigation.tsx
+++ henkilo-ui/src/main/static/src/components/navigation/TopNavigation.tsx
@@ -65,6 +65,7 @@ export const TopNavigation = () => {
                                     })
                                 }
                                 to={tab.path}
+                                end
                             >
                                 {({ isActive }) => (
                                     <span>
