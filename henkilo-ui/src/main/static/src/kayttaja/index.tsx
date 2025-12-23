6473d7f23d99d17b950460f99b0017d078486f6b
Tomi Karlstedt <tomi.karlstedt@reaktor.com>
2025-12-23 16:06:41 +0200

    OPHYK-965 add OPH design system styles to kayttaja bundle

--- henkilo-ui/src/main/static/src/kayttaja/index.tsx
+++ henkilo-ui/src/main/static/src/kayttaja/index.tsx
@@ -11,6 +11,7 @@ import 'oph-virkailija-style-guide/oph-styles.css';
 import '../index.css';
 import 'react-datepicker/dist/react-datepicker.css';
 import '../flex.css';
+import '../oph-design-system.css';
 
 const root = document.getElementById('root');
 if (root) {
