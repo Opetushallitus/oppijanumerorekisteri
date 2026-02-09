9fc3cca343f6e381bce0f21ecb3dd9b7d7c44dd2
Timo Suomela <timo.suomela@reaktor.com>
2026-02-09 09:58:35 +0200

    OPHYK-984 Add missing properties

--- infra/src/config.ts
+++ infra/src/config.ts
@@ -87,6 +87,7 @@ export const dev: Config = {
     "oppijanumerorekisteri.tasks.datantuonti.export.enabled": true,
     "oppijanumerorekisteri.tasks.datantuonti.import.enabled": true,
     "oppijanumerorekisteri.tasks.testidatantuonti.import.enabled": true,
+    "tiedotuspalvelu.fetch-oppija.enabled": false,
   },
   lampiExport: {
     enabled: true,
@@ -106,6 +107,7 @@ export const qa: Config = {
     "oppijanumerorekisteri.tasks.datantuonti.export.enabled": true,
     "oppijanumerorekisteri.tasks.datantuonti.import.enabled": true,
     "oppijanumerorekisteri.tasks.testidatantuonti.import.enabled": true,
+    "tiedotuspalvelu.fetch-oppija.enabled": false,
   },
   lampiExport: {
     enabled: true,
