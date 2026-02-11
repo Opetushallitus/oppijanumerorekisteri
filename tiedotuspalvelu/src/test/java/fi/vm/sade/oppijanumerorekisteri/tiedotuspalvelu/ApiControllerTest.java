62651bd2d98d5052ea4e24e9b585afa76da9fda9
Timo Suomela <timo.suomela@reaktor.com>
2026-02-11 11:43:27 +0200

    OPHYK-971 Extract shared code

--- tiedotuspalvelu/src/test/java/fi/vm/sade/oppijanumerorekisteri/tiedotuspalvelu/ApiControllerTest.java
+++ tiedotuspalvelu/src/test/java/fi/vm/sade/oppijanumerorekisteri/tiedotuspalvelu/ApiControllerTest.java
@@ -138,14 +138,8 @@ public class ApiControllerTest {
     tiedoteRepository.deleteAll();
     String idempotencyKey = UUID.randomUUID().toString();
     var tiedote = createTiedote(idempotencyKey);
-
-    String firstResponse = postTiedote(tiedote);
-
-    UUID firstId = UUID.fromString(objectMapper.readTree(firstResponse).get("id").asText());
-
-    String secondResponse = postTiedote(tiedote);
-
-    UUID secondId = UUID.fromString(objectMapper.readTree(secondResponse).get("id").asText());
+    var firstId = postTiedoteAndReturnTiedoteId(tiedote);
+    var secondId = postTiedoteAndReturnTiedoteId(tiedote);
 
     assertEquals(firstId, secondId);
 
@@ -154,41 +148,41 @@ public class ApiControllerTest {
     assertEquals(idempotencyKey, tiedotteet.get(0).getIdempotencyKey());
   }
 
-  private @NonNull String postTiedote(Map<String, String> tiedote) throws Exception {
-    return mockMvc
-        .perform(
-            post("/api/v1/tiedote/kielitutkintotodistus")
-                .with(
-                    jwt()
-                        .authorities(
-                            new SimpleGrantedAuthority(
-                                ROLE_APP_TIEDOTUSPALVELU_KIELITUTKINTOTODISTUS_TIEDOTE_CRUD)))
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(tiedote)))
-        .andExpect(status().isOk())
-        .andExpect(jsonPath("$.id").exists())
-        .andReturn()
-        .getResponse()
-        .getContentAsString();
-  }
-
   @Test
   public void createTiedoteWithDifferentIdempotencyKeysCreatesDifferentRecords() throws Exception {
     tiedoteRepository.deleteAll();
     var tiedote1 = createTiedote();
     var tiedote2 = createTiedote();
 
-    String firstResponse = postTiedote(tiedote1);
+    var firstId = postTiedoteAndReturnTiedoteId(tiedote1);
+    var secondId = postTiedoteAndReturnTiedoteId(tiedote2);
 
-    UUID firstId = UUID.fromString(objectMapper.readTree(firstResponse).get("id").asText());
-
-    String secondResponse = postTiedote(tiedote2);
-
-    UUID secondId = UUID.fromString(objectMapper.readTree(secondResponse).get("id").asText());
     assertEquals(false, firstId.equals(secondId));
 
     List<Tiedote> tiedotteet = tiedoteRepository.findAll();
     assertEquals(2, tiedotteet.size());
+  }
+
+  private @NonNull UUID postTiedoteAndReturnTiedoteId(Map<String, String> tiedote)
+      throws Exception {
+    var response =
+        mockMvc
+            .perform(
+                post("/api/v1/tiedote/kielitutkintotodistus")
+                    .with(
+                        jwt()
+                            .authorities(
+                                new SimpleGrantedAuthority(
+                                    ROLE_APP_TIEDOTUSPALVELU_KIELITUTKINTOTODISTUS_TIEDOTE_CRUD)))
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(objectMapper.writeValueAsString(tiedote)))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.id").exists())
+            .andReturn()
+            .getResponse()
+            .getContentAsString();
+
+    return UUID.fromString(objectMapper.readTree(response).get("id").asText());
   }
 
   private Map<String, String> createTiedote() {
