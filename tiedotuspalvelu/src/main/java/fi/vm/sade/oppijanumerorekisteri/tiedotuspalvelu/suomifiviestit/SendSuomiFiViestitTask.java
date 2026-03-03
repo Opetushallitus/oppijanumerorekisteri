a9c4e654ad52389a9ef4075599279a103900f6df
Timo Suomela <timo.suomela@reaktor.com>
2026-03-03 17:04:35 +0200

    OPHYK-1070 Remove redundant indirection

--- tiedotuspalvelu/src/main/java/fi/vm/sade/oppijanumerorekisteri/tiedotuspalvelu/suomifiviestit/SendSuomiFiViestitTask.java
+++ tiedotuspalvelu/src/main/java/fi/vm/sade/oppijanumerorekisteri/tiedotuspalvelu/suomifiviestit/SendSuomiFiViestitTask.java
@@ -1,6 +1,9 @@
 package fi.vm.sade.oppijanumerorekisteri.tiedotuspalvelu.suomifiviestit;
 
+import fi.vm.sade.oppijanumerorekisteri.tiedotuspalvelu.TiedotuspalveluProperties;
+import fi.vm.sade.oppijanumerorekisteri.tiedotuspalvelu.locale.LocalisationRepository;
 import java.time.OffsetDateTime;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -13,7 +16,9 @@ public class SendSuomiFiViestitTask {
 
   private static final int _24_HOURS_IN_MINUTES = 60 * 24;
   private final SuomiFiViestiRepository suomiFiViestiRepository;
-  private final SuomiFiViestitService suomiFiViestitService;
+  private final SuomiFiViestitClient suomiFiViestitClient;
+  private final TiedotuspalveluProperties tiedotuspalveluProperties;
+  private final LocalisationRepository localisationRepository;
   private final TransactionTemplate transactionTemplate;
 
   public void execute() {
@@ -23,7 +28,7 @@ public class SendSuomiFiViestitTask {
       try {
         transactionTemplate.executeWithoutResult(
             status -> {
-              var messageId = suomiFiViestitService.sendSuomiFiViesti(viesti);
+              var messageId = sendSuomiFiViesti(viesti);
               viesti.setMessageId(messageId);
               viesti.setProcessedAt(OffsetDateTime.now());
               viesti.setNextRetry(null);
@@ -44,5 +49,29 @@ public class SendSuomiFiViestitTask {
       }
     }
     log.info("Finished running SendSuomiFiViestitTask");
+  }
+
+  private String sendSuomiFiViesti(SuomiFiViesti suomiFiViesti) {
+    var title = localisationRepository.translate("OMAT_VIESTIT_SUOMIFI_OTSIKKO", "fi");
+    var body = localisationRepository.translate("OMAT_VIESTIT_SUOMIFI_VIESTI", "fi");
+    var request =
+        new ElectronicMessageRequest(
+            new ElectronicPart(
+                List.of(),
+                body,
+                "Text",
+                "Normal",
+                new MessageNotifications(
+                    new UnreadMessageNotification("Default reminder"),
+                    "Organisation and service name"),
+                "No one",
+                title,
+                "Normal"),
+            suomiFiViesti.getTiedoteId().toString(),
+            new Recipient(suomiFiViesti.getHenkilotunnus()),
+            new Sender(tiedotuspalveluProperties.suomifiViestit().senderServiceId()));
+    var messageId = suomiFiViestitClient.sendElectronicMessage(request);
+    log.info("Sent Suomi.fi viesti for tiedote {}", suomiFiViesti.getTiedoteId());
+    return messageId;
   }
 }
