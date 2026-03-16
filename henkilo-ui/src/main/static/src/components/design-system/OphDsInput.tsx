7ac7590b1cebab29f59ee5c1f1ca5bc8bd5c8497
Tomi Karlstedt <tomi.karlstedt@reaktor.com>
2026-03-16 10:28:17 +0200

    OPHYK-1096 add placeholder, optional background icon, and optional clear button to input field

--- henkilo-ui/src/main/static/src/components/design-system/OphDsInput.tsx
+++ henkilo-ui/src/main/static/src/components/design-system/OphDsInput.tsx
@@ -1,5 +1,7 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+
 import { useDebounce } from '../../useDebounce';
+import { useLocalisations } from '../../selectors';
 
 type PageProps = {
     defaultValue?: string;
@@ -10,6 +12,9 @@ type PageProps = {
     label: string;
     onChange: (s: string) => void;
     debounceTimeout?: number;
+    placeholder?: string;
+    icon?: 'search';
+    isClearable?: boolean;
 };
 
 export const OphDsInput = ({
@@ -21,9 +26,16 @@ export const OphDsInput = ({
     onChange,
     type,
     debounceTimeout,
+    placeholder,
+    icon,
+    isClearable,
 }: PageProps) => {
+    const { L } = useLocalisations();
+    const inputRef = useRef<HTMLInputElement | null>(null);
     const [value, setValue] = useState(defaultValue ?? '');
     const debounced = debounceTimeout ? useDebounce(value, debounceTimeout) : undefined;
+    const iconClass = icon ? `oph-ds-input-icon oph-ds-input-icon-${icon}` : '';
+    const clearButtonClass = isClearable ? 'oph-ds-input-clearable' : '';
 
     useEffect(() => {
         if (debounced !== undefined) {
@@ -31,21 +43,55 @@ export const OphDsInput = ({
         }
     }, [debounced, debounceTimeout]);
 
+    const handleClear = () => {
+        setValue('');
+        inputRef.current?.focus();
+    };
+
     return (
         <div>
             <label htmlFor={id} className="oph-ds-label">
                 {label}
             </label>
-            <input
-                id={id}
-                name={id}
-                type={type ?? 'text'}
-                defaultValue={value}
-                onChange={(e) => (debounceTimeout ? setValue(e.target.value) : onChange(e.target.value))}
-                className={`oph-ds-input ${error !== undefined ? 'oph-ds-input-error' : ''}`}
-                disabled={disabled}
-                autoComplete="false"
-            />
+            <div className="oph-ds-input-container">
+                <input
+                    ref={inputRef}
+                    id={id}
+                    name={id}
+                    type={type ?? 'text'}
+                    value={value}
+                    onChange={(e) => (debounceTimeout ? setValue(e.target.value) : onChange(e.target.value))}
+                    className={`oph-ds-input ${iconClass} ${clearButtonClass} ${error !== undefined ? 'oph-ds-input-error' : ''}`}
+                    disabled={disabled}
+                    autoComplete="false"
+                    placeholder={placeholder}
+                />
+                {isClearable && (
+                    <button
+                        className={`oph-ds-input-clear-button ${icon ? 'oph-ds-input-clear-button-with-icon' : ''}`}
+                        onClick={handleClear}
+                        disabled={!value}
+                        aria-disabled={!value}
+                        style={value ? { cursor: 'pointer' } : { display: 'none' }}
+                    >
+                        <svg
+                            width="24"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                            aria-hidden="true"
+                            focusable="false"
+                        >
+                            <path
+                                d="M19 6.41L17.59 5L12 10.59L6.41 5L5 6.41L10.59 12L5 17.59L6.41 19L12 13.41L17.59 19L19 17.59L13.41 12L19 6.41Z"
+                                fill="#1D1D1D"
+                            />
+                        </svg>
+                        <span className="oph-ds-hidden-aria-label">{L('TYHJENNA')}</span>
+                    </button>
+                )}
+            </div>
             {error && <span className="oph-ds-error">{error}</span>}
         </div>
     );
