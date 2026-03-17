1dba653f9232ce4c5d969877976490090507045b
Tomi Karlstedt <tomi.karlstedt@reaktor.com>
2026-03-17 16:17:56 +0200

    OPHYK-1192 add happy path test for kutsuminen

---
+++ henkilo-ui/src/main/static/e2e/app/kutsuminen.spec.ts
@@ -0,0 +1,75 @@
+import { test, expect } from '@playwright/test';
+
+import { gotoKutsuminen } from './locators/kutsuminen-page';
+
+import henkilo from '../../mock-api/src/api/oppijanumerorekisteri-service/henkilo/1.2.246.562.24.00000000007/GET.json';
+import { toastWithText } from '../locators';
+
+test.describe('kutsuminen', () => {
+    test.beforeEach(async ({ page }) => {
+        await page.route('/oppijanumerorekisteri-service/henkilo/1.2.246.562.24.00000000007', async (route) => {
+            await route.fulfill({
+                json: {
+                    ...henkilo,
+                    yksiloityVTJ: true,
+                    hetu: '121212-1234',
+                },
+            });
+        });
+    });
+
+    test('happy path', async ({ page }) => {
+        const { confirmation, form, organisations, submit, validationBanner } = await gotoKutsuminen(page);
+
+        await expect(validationBanner).toContainText('Täytä kaikki kentät');
+        await expect(validationBanner).toContainText('Valitse organisaatio sekä vähintään yksi käyttöoikeusryhmä');
+
+        await form.etunimi.fill('etunimi');
+        await form.sukunimi.fill('sukunimi');
+
+        await form.email.fill('email');
+        await expect(validationBanner).toContainText('Virheellinen sähköposti');
+        await form.email.fill('email@email.fi');
+        await expect(validationBanner).not.toContainText('Virheellinen sähköposti');
+
+        await form.kieliSelect.select('ruotsi');
+        await expect(validationBanner).not.toContainText('Täytä kaikki kentät');
+
+        await form.viesti.fill('saate');
+
+        await organisations.add.click();
+        await organisations.get(0).organisaatio.select('aliorg');
+        await organisations.get(0).kayttooikeus.click();
+        await organisations.get(0).modal.valittava('rajapintakäyttäjä').click();
+        await organisations.get(0).modal.lisaa.click();
+        await organisations.get(0).modal.valittava('tallentaja').click();
+        await organisations.get(0).modal.lisaa.click();
+        await organisations.get(0).modal.close.click();
+
+        await organisations.add.click();
+        await organisations.get(1).ryhma.select('ryhma');
+        await organisations.get(1).kayttooikeus.click();
+        await organisations.get(1).modal.valittava('tallentaja').click();
+        await organisations.get(1).modal.lisaa.click();
+        await organisations.get(1).modal.close.click();
+
+        await expect(validationBanner).not.toBeAttached();
+        await submit.click();
+
+        await expect(confirmation.content).toContainText('aliorgrajapintakäyttäjä tallentaja');
+        await expect(confirmation.content).toContainText('ryhma1tallentaja');
+
+        await page.route('/kayttooikeus-service/kutsu', async (route, request) => {
+            const j = request.postDataJSON();
+            if (j.asiointikieli !== 'sv' || j.etunimi !== 'etunimi' || j.sukunimi !== 'sukunimi') {
+                throw new Error('Invalid request!');
+            }
+            await route.fulfill({
+                json: '2356425',
+            });
+        });
+
+        await confirmation.submit.click();
+        await expect(toastWithText(page, 'Kutsu lähetetty')).toBeVisible();
+    });
+});
---
+++ henkilo-ui/src/main/static/e2e/app/locators/kutsuminen-page.ts
@@ -0,0 +1,41 @@
+import { Page } from '@playwright/test';
+import { selectLocator } from '../../locators';
+
+export async function gotoKutsuminen(page: Page) {
+    await page.goto('/henkilo-ui/kutsulomake');
+
+    return {
+        form: {
+            etunimi: page.getByLabel('Etunimi'),
+            sukunimi: page.getByLabel('Sukunimi'),
+            email: page.getByLabel('Sähköposti'),
+            kieliSelect: selectLocator(page, '#kieli-select'),
+            viesti: page.getByLabel('Viesti'),
+        },
+        organisations: {
+            add: page.getByRole('button', { name: 'Lisää organisaatio...' }),
+            get: (i: number) => {
+                const org = page.getByTestId(`addedorg-${i}`);
+                return {
+                    organisaatio: selectLocator(page, `#organisaatio-${i}`),
+                    ryhma: selectLocator(page, `#ryhma-${i}`),
+                    kayttooikeus: org.getByRole('button', { name: 'Valitse käyttöoikeus' }),
+                    voimassa: org.locator(`voimassa-${i}`),
+                    modal: {
+                        valittava: (t: string) => page.locator('.valittavat').getByText(t),
+                        close: page.locator('.oph-button-close'),
+                        lisaa: page.getByRole('button', { name: 'Lisää haettaviin käyttöoikeuksiin' }),
+                    },
+                };
+            },
+        },
+        submit: page.getByRole('button', { name: 'Tallenna ja lähetä itserekisteröitymissähköposti' }),
+        validationBanner: page.locator('.oph-ds-banner-warning'),
+        confirmation: {
+            content: page.locator('.oph-modal-content'),
+            submit: page
+                .locator('.oph-modal-content')
+                .getByRole('button', { name: 'Tallenna ja lähetä itserekisteröitymissähköposti' }),
+        },
+    };
+}
