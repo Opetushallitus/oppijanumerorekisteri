05f4652c765c1b8229fd42e8a6c8a299cbcfcb2e
Tomi Karlstedt <tomi.karlstedt@reaktor.com>
2026-04-13 14:22:51 +0300

    OPHYK-1115 test the actual access log masking pattern instead of testing test logging configuration

--- oppijanumerorekisteri-service/src/test/java/fi/vm/sade/oppijanumerorekisteri/AccessLogTest.java
+++ oppijanumerorekisteri-service/src/test/java/fi/vm/sade/oppijanumerorekisteri/AccessLogTest.java
@@ -1,105 +1,112 @@
 package fi.vm.sade.oppijanumerorekisteri;
 
-import org.junit.After;
-import org.junit.Before;
+import ch.qos.logback.access.common.PatternLayout;
+import ch.qos.logback.access.common.spi.IAccessEvent;
+import ch.qos.logback.core.ContextBase;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.PrintStream;
+import java.lang.reflect.Proxy;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static org.junit.Assert.assertEquals;
 
-/**
- * This test boots up tomcat server and tests masking of sensitive
- * information in access log produced. Behavior is same as in
- * final product, but access log format is more terse for brevity.
- * <p>
- * check <a href="file:../resources/logback-access.xml">logback-access.xml</a>
- * in test resources to see the tested regexp.
- */
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@ContextConfiguration(classes = {OppijanumerorekisteriServiceApplication.class})
 public class AccessLogTest {
 
-    private final PrintStream restore = System.out;
-    @Autowired
-    private TestRestTemplate restTemplate;
-    private ByteArrayOutputStream output;
-
-    @Before
-    public void setUp() {
-        output = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(output));
-    }
-
-    @After
-    public void tearDown() throws IOException {
-        System.setOut(restore);
-        output.close();
-    }
+    private static final Path MAIN_ACCESS_LOGBACK_XML = Path.of("src/main/resources/logback-access.xml");
+    private static final Pattern REQUEST_PATTERN =
+        Pattern.compile("&quot;request&quot;:\\s*&quot;([^&]|&(?!quot;))*&quot;");
+    private static final String ACCESS_LOG_PATTERN = readAccessLogPattern();
 
     @Test
     public void requestIsLogged() {
-        restTemplate.getForEntity("/", String.class);
-        assertEquals("GET / HTTP/1.1", resolveLog(output));
+        assertEquals("GET / HTTP/1.1", render("GET", "/", "HTTP/1.1"));
     }
 
     @Test
     public void sensitiveInformationInPathIsMasked() {
-        restTemplate.getForEntity("/123456+7890", String.class);
-        assertEquals("GET /123456+**** HTTP/1.1", resolveLog(output));
+        assertEquals("GET /123456+**** HTTP/1.1", render("GET", "/123456+7890", "HTTP/1.1"));
     }
 
     @Test
     public void sensitiveInformationInPathIsMaskedNewSpec() {
-        restTemplate.getForEntity("/123456X7890", String.class);
-        assertEquals("GET /123456X**** HTTP/1.1", resolveLog(output));
+        assertEquals("GET /123456X**** HTTP/1.1", render("GET", "/123456X7890", "HTTP/1.1"));
     }
 
     @Test
     public void sensitiveInformationInRequestParameterIsMasked() {
-        restTemplate.getForEntity("/?test=123456-7890", String.class);
-        assertEquals("GET /?test=123456-**** HTTP/1.1", resolveLog(output));
+        assertEquals("GET /?test=123456-**** HTTP/1.1", render("GET", "/?test=123456-7890", "HTTP/1.1"));
     }
 
     @Test
     public void allSensitiveInformationIsMasked() {
-        restTemplate.getForEntity("/123456+789A/123456-789B?test=123456A7890&test=123456-789R", String.class);
-        assertEquals("GET /123456+****/123456-****?test=123456A****&test=123456-**** HTTP/1.1", resolveLog(output));
+        assertEquals(
+            "GET /123456+****/123456-****?test=123456A****&test=123456-**** HTTP/1.1",
+            render("GET", "/123456+789A/123456-789B?test=123456A7890&test=123456-789R", "HTTP/1.1")
+        );
     }
 
     @Test
     public void onlyExactMatchesAreMaskedIncorrectPrefix() {
-        restTemplate.getForEntity("/1234567-890A", String.class);
-        assertEquals("GET /1234567-890A HTTP/1.1", resolveLog(output));
+        assertEquals("GET /1234567-890A HTTP/1.1", render("GET", "/1234567-890A", "HTTP/1.1"));
     }
 
     @Test
     public void onlyExactMatchesAreMaskedIncorrectSuffix() {
-        restTemplate.getForEntity("/123456-890AA", String.class);
-        assertEquals("GET /123456-890AA HTTP/1.1", resolveLog(output));
+        assertEquals("GET /123456-890AA HTTP/1.1", render("GET", "/123456-890AA", "HTTP/1.1"));
     }
 
     @Test
     public void handlesOids() {
-        restTemplate.getForEntity("/1.2.246.562.24.43116640405", String.class);
-        assertEquals("GET /1.2.246.562.24.43116640405 HTTP/1.1", resolveLog(output));
+        assertEquals("GET /1.2.246.562.24.43116640405 HTTP/1.1", render("GET", "/1.2.246.562.24.43116640405", "HTTP/1.1"));
     }
 
-    private String resolveLog(ByteArrayOutputStream output) {
-        for (String s : output.toString().split(System.getProperty("line.separator"), 10)) {
-            if (s.startsWith("GET")) {
-                return s;
+    private String render(String method, String requestUri, String protocol) {
+        PatternLayout layout = new PatternLayout();
+        layout.setContext(new ContextBase());
+        layout.setPattern(ACCESS_LOG_PATTERN);
+        layout.start();
+        return layout.doLayout(accessEvent(method, requestUri, protocol)).stripTrailing();
+    }
+
+    private static String readAccessLogPattern() {
+        try {
+            String xml = Files.readString(MAIN_ACCESS_LOGBACK_XML);
+            Matcher matcher = REQUEST_PATTERN.matcher(xml);
+            if (!matcher.find()) {
+                throw new IllegalStateException("Could not find request pattern in " + MAIN_ACCESS_LOGBACK_XML);
             }
+            String entry = matcher.group();
+            int start = entry.indexOf("&quot;request&quot;: &quot;") + "&quot;request&quot;: &quot;".length();
+            int end = entry.lastIndexOf("&quot;");
+            return entry.substring(start, end);
+        } catch (IOException e) {
+            throw new ExceptionInInitializerError(e);
         }
-        return "FAIL";
+    }
+
+    private IAccessEvent accessEvent(String method, String requestUri, String protocol) {
+        String requestLine = method + " " + requestUri + " " + protocol;
+        return (IAccessEvent) Proxy.newProxyInstance(
+            IAccessEvent.class.getClassLoader(),
+            new Class<?>[]{IAccessEvent.class},
+            (proxy, invokedMethod, args) -> switch (invokedMethod.getName()) {
+                case "getMethod" -> method;
+                case "getRequestURI" -> requestUri;
+                case "getRequestURL" -> requestLine;
+                case "getProtocol" -> protocol;
+                case "prepareForDeferredProcessing", "setThreadName" -> null;
+                case "getElapsedTime", "getElapsedSeconds", "getContentLength", "getSequenceNumber", "getTimeStamp" -> 0L;
+                case "getStatusCode", "getLocalPort" -> 0;
+                case "getRequestHeaderNames" -> java.util.Collections.emptyEnumeration();
+                case "getRequestHeaderMap", "getRequestParameterMap", "getResponseHeaderMap" -> java.util.Collections.emptyMap();
+                case "getResponseHeaderNameList", "getCookies" -> java.util.Collections.emptyList();
+                case "getRequestParameter" -> new String[0];
+                default -> null;
+            }
+        );
     }
 }
