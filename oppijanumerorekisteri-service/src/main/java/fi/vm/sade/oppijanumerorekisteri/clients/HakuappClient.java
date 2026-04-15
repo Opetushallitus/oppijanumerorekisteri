8d638db352d44cae7f27ca7df691eeab7edfed06
Tomi Karlstedt <tomi.karlstedt@reaktor.com>
2026-04-15 14:02:43 +0300

    OPHYK-1115 refactor hakuapp client to use local CAS client implementation

--- oppijanumerorekisteri-service/src/main/java/fi/vm/sade/oppijanumerorekisteri/clients/HakuappClient.java
+++ oppijanumerorekisteri-service/src/main/java/fi/vm/sade/oppijanumerorekisteri/clients/HakuappClient.java
@@ -1,10 +1,69 @@
 package fi.vm.sade.oppijanumerorekisteri.clients;
 
-import fi.vm.sade.oppijanumerorekisteri.dto.HakemusDto;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
+import fi.vm.sade.oppijanumerorekisteri.clients.cas.ApiResponse;
+import fi.vm.sade.oppijanumerorekisteri.clients.cas.CasAuthenticatedServiceClient;
+import fi.vm.sade.oppijanumerorekisteri.clients.cas.CasClient;
+import fi.vm.sade.oppijanumerorekisteri.dto.HakemusDto;
+import fi.vm.sade.oppijanumerorekisteri.exceptions.HttpConnectionException;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
-public interface HakuappClient {
-    Map<String, List<HakemusDto>> fetchApplicationsByOid(List<String> oids);
+@Slf4j
+public class HakuappClient extends CasAuthenticatedServiceClient {
+    private final ObjectMapper objectMapper;
+    private final String baseUrl;
+
+    public HakuappClient(HttpClient httpClient, CasClient casClient, String baseUrl, ObjectMapper objectMapper) {
+        super(httpClient, casClient, baseUrl);
+        this.objectMapper = objectMapper;
+        this.baseUrl = baseUrl;
+    }
+
+    public Map<String, List<HakemusDto>> fetchApplicationsByOid(List<String> oids) {
+        try {
+            var response = post("/applications/byPersonOid", objectMapper.writeValueAsString(oids));
+            Map<String, List<Map<String, Object>>> applicationsByPersonOid = switch (response.getStatus()) {
+                case 200 -> objectMapper.readValue(response.getBody(), new TypeReference<Map<String, List<Map<String, Object>>>>() {});
+                default -> throw new HttpConnectionException();
+            };
+            Map<String, List<HakemusDto>> hakemuksetByHenkiloOid = new HashMap<>();
+            applicationsByPersonOid.keySet().forEach(henkiloOid -> {
+                List<Map<String, Object>> hakemukset = applicationsByPersonOid.get(henkiloOid);
+                List<HakemusDto> hakemusList = hakemukset.stream().map(hakemusData -> {
+                    hakemusData.put("service", "haku-app");
+                    return new HakemusDto(hakemusData);
+                }).collect(Collectors.toList());
+
+                hakemuksetByHenkiloOid.put(henkiloOid, hakemusList);
+            });
+            return hakemuksetByHenkiloOid;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new RuntimeException(e);
+        }
+    }
+
+    private ApiResponse post(String path, String requestBody) throws IOException, InterruptedException {
+        log.info("Doing request to {} with body {}", path, requestBody);
+        var request = HttpRequest.newBuilder()
+                .uri(URI.create(baseUrl + path))
+                .method("POST", HttpRequest.BodyPublishers.ofString(requestBody))
+                .header("Content-Type", "application/json")
+                .header("Accept", "application/json");
+        var response = sendRequest(request);
+        return new ApiResponse(response.statusCode(), response.body());
+    }
 }
--- oppijanumerorekisteri-service/src/main/java/fi/vm/sade/oppijanumerorekisteri/configurations/ClientConfiguration.java
+++ oppijanumerorekisteri-service/src/main/java/fi/vm/sade/oppijanumerorekisteri/configurations/ClientConfiguration.java
@@ -1,9 +1,9 @@
 package fi.vm.sade.oppijanumerorekisteri.configurations;
 
 import fi.vm.sade.oppijanumerorekisteri.clients.AtaruClient;
+import fi.vm.sade.oppijanumerorekisteri.clients.HakuappClient;
 import fi.vm.sade.oppijanumerorekisteri.clients.cas.CasClient;
 import fi.vm.sade.oppijanumerorekisteri.configurations.properties.AuthenticationProperties;
-import fi.vm.sade.oppijanumerorekisteri.configurations.properties.UrlConfiguration;
 import fi.vm.sade.properties.OphProperties;
 import lombok.RequiredArgsConstructor;
 
@@ -24,10 +24,8 @@ public class ClientConfiguration {
 
     @Value("${ataru.baseurl}")
     private String ataruBaseUrl;
-    @Value("${authentication.viestintapalvelu.username}")
-    private String username;
-    @Value("${authentication.viestintapalvelu.password}")
-    private String password;
+    @Value("${haku-app.baseurl}")
+    private String hakuAppBaseurl;
 
     @Bean
     public AtaruClient ataruClient(OphProperties properties, ObjectMapper objectMapper) {
@@ -41,9 +39,28 @@ public class ClientConfiguration {
                 httpClient,
                 casBase,
                 authenticationProperties.getAtaru().getUsername(),
-                authenticationProperties.getAtaru().getPassword()
+                authenticationProperties.getAtaru().getPassword(),
+                ""
         );
 
         return new AtaruClient(httpClient, casClient, ataruBaseUrl, objectMapper);
+    }
+
+    @Bean
+    public HakuappClient hakuappClient(OphProperties properties, ObjectMapper objectMapper) {
+        var casBase = properties.require("cas.url");
+
+        var httpClient = HttpClient.newBuilder()
+                .cookieHandler(new CookieManager())
+                .connectTimeout(Duration.ofSeconds(60))
+                .build();
+        var casClient = new CasClient(
+                httpClient,
+                casBase,
+                authenticationProperties.getHakuapp().getUsername(),
+                authenticationProperties.getHakuapp().getPassword()
+        );
+
+        return new HakuappClient(httpClient, casClient, hakuAppBaseurl, objectMapper);
     }
 }
