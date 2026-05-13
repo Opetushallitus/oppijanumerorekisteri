aac1aa96c312835634acddbdaf9d3cff733d5dc5
Tomi Karlstedt <tomi.karlstedt@reaktor.com>
2026-05-13 12:59:00 +0300

    use design system components in virkailija testitunnus

--- henkilo-ui/src/main/static/src/components/henkilo/VirkailijaCreateContainer.tsx
+++ henkilo-ui/src/main/static/src/components/henkilo/VirkailijaCreateContainer.tsx
@@ -2,13 +2,15 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router';
 
 import { VirkailijaCreate } from '../../types/domain/kayttooikeus/virkailija.types';
-import VirkailijaCreateForm from './VirkailijaCreateForm';
 import { isValidKutsumanimi } from '../../validation/KutsumanimiValidator';
 import { isValidPassword } from '../../validation/PasswordValidator';
 import { isValidKayttajatunnus } from '../../validation/KayttajatunnusValidator';
 import { useLocalisations } from '../../selectors';
 import { usePostCreateVirkailijaMutation } from '../../api/kayttooikeus';
-import { OphDsBanner } from '../design-system/OphDsBanner';
+import { OphDsPage } from '../design-system/OphDsPage';
+import { OphDsInput } from '../design-system/OphDsInput';
+import { add } from '../../slices/toastSlice';
+import { useAppDispatch } from '../../store';
 
 const initialVirkailija: VirkailijaCreate = {
     etunimet: '',
@@ -20,17 +22,15 @@ const initialVirkailija: VirkailijaCreate = {
     vahvastiTunnistettu: true,
 };
 
-/**
- * Virkailijan luonti -näkymä.
- */
 export const VirkailijaCreateContainer = () => {
     const navigate = useNavigate();
-    const [virheet, setVirheet] = useState<string[]>([]);
+    const dispatch = useAppDispatch();
+    const [virheet, setVirheet] = useState<VirkailijaCreate>(initialVirkailija);
     const [virkailija, setVirkailija] = useState(initialVirkailija);
     const [postCreateVirkailija] = usePostCreateVirkailijaMutation();
     const { L } = useLocalisations();
     const disabled =
-        virheet.length > 0 || // pakolliset kentät:
+        Object.values(virheet).filter((v) => !!v && v !== true).length > 0 || // pakolliset kentät:
         !virkailija.etunimet ||
         !virkailija.kutsumanimi ||
         !virkailija.sukunimi ||
@@ -39,63 +39,115 @@ export const VirkailijaCreateContainer = () => {
         !virkailija.salasanaUudestaan;
 
     const onChange = (newVirkailija: VirkailijaCreate): void => {
+        setVirheet({
+            ...virheet,
+            kutsumanimi:
+                newVirkailija.kutsumanimi && !isValidKutsumanimi(newVirkailija.etunimet, newVirkailija.kutsumanimi)
+                    ? L('REKISTEROIDY_ERROR_KUTSUMANIMI')
+                    : '',
+            kayttajatunnus:
+                newVirkailija.kayttajatunnus && !isValidKayttajatunnus(newVirkailija.kayttajatunnus)
+                    ? L('NOTIFICATION_HENKILOTIEDOT_KAYTTAJATUNNUS_VIRHE')
+                    : '',
+            salasana:
+                newVirkailija.salasana && !isValidPassword(newVirkailija.salasana)
+                    ? L('REKISTEROIDY_ERROR_PASSWORD_INVALID')
+                    : '',
+            salasanaUudestaan:
+                newVirkailija.salasana &&
+                newVirkailija.salasanaUudestaan &&
+                newVirkailija.salasana !== newVirkailija.salasanaUudestaan
+                    ? L('REKISTEROIDY_ERROR_PASSWORD_MATCH')
+                    : '',
+        });
         setVirkailija(newVirkailija);
-        setVirheet(validate(newVirkailija));
-    };
-
-    const validate = (virkailija: VirkailijaCreate): string[] => {
-        const virheet: string[] = [];
-        if (virkailija.kutsumanimi) {
-            if (!isValidKutsumanimi(virkailija.etunimet, virkailija.kutsumanimi)) {
-                virheet.push(L('REKISTEROIDY_ERROR_KUTSUMANIMI'));
-            }
-        }
-        if (virkailija.kayttajatunnus) {
-            if (!isValidKayttajatunnus(virkailija.kayttajatunnus)) {
-                virheet.push(L('NOTIFICATION_HENKILOTIEDOT_KAYTTAJATUNNUS_VIRHE'));
-            }
-        }
-        if (virkailija.salasana) {
-            if (!isValidPassword(virkailija.salasana)) {
-                virheet.push(L('REKISTEROIDY_ERROR_PASSWORD_INVALID'));
-            }
-            if (virkailija.salasana !== virkailija.salasanaUudestaan) {
-                virheet.push(L('REKISTEROIDY_ERROR_PASSWORD_MATCH'));
-            }
-        }
-        return virheet;
     };
 
     const onSubmit = async (virkailijaCreate: VirkailijaCreate): Promise<void> => {
         await postCreateVirkailija(virkailijaCreate)
             .unwrap()
             .then((oid) => navigate(`/virkailija/${oid}`))
-            .catch(handleError);
-    };
-
-    const handleError = (error: { data?: { errorType?: string } }): void => {
-        if (error.data?.errorType === 'AccessDeniedException') {
-            setVirheet([...virheet, L('VIRKAILIJAN_LUONTI_EI_OIKEUKSIA')]);
-        } else if (error.data?.errorType === 'UsernameAlreadyExistsException') {
-            setVirheet([...virheet, L('REKISTEROIDY_USERNAMEEXISTS_OTSIKKO')]);
-        } else {
-            setVirheet([...virheet, L('HENKILON_LUONTI_EPAONNISTUI')]);
-        }
+            .catch((error: { data?: { errorType?: string } }) => {
+                if (error.data?.errorType === 'AccessDeniedException') {
+                    dispatch(
+                        add({
+                            id: `testitunnus-${Math.random()}`,
+                            header: L('VIRKAILIJAN_LUONTI_EI_OIKEUKSIA'),
+                            type: 'error',
+                        })
+                    );
+                } else if (error.data?.errorType === 'UsernameAlreadyExistsException') {
+                    dispatch(
+                        add({
+                            id: `testitunnus-${Math.random()}`,
+                            header: L('REKISTEROIDY_USERNAMEEXISTS_OTSIKKO'),
+                            type: 'error',
+                        })
+                    );
+                } else {
+                    dispatch(
+                        add({
+                            id: `testitunnus-${Math.random()}`,
+                            header: L('HENKILON_LUONTI_EPAONNISTUI'),
+                            type: 'error',
+                        })
+                    );
+                }
+            });
     };
 
     return (
-        <div className="mainContent wrapper">
-            <span className="oph-h2 oph-bold">{L('VIRKAILIJAN_LUONTI_OTSIKKO')}</span>
-            <VirkailijaCreateForm virkailija={virkailija} disabled={disabled} onChange={onChange} onSubmit={onSubmit} />
-            {virheet.length > 0 && (
-                <OphDsBanner type="error">
-                    <ul>
-                        {virheet.map((virhe, index) => (
-                            <li key={index}>{virhe}</li>
-                        ))}
-                    </ul>
-                </OphDsBanner>
-            )}
-        </div>
+        <OphDsPage header={L('VIRKAILIJAN_LUONTI_OTSIKKO')}>
+            <div style={{ display: 'flex', flexDirection: 'column', gap: '1rem' }}>
+                <OphDsInput
+                    id="etunimet"
+                    label={L('HENKILO_ETUNIMET')}
+                    defaultValue={virkailija.etunimet}
+                    onChange={(s) => onChange({ ...virkailija, etunimet: s })}
+                    error={virheet.etunimet || undefined}
+                />
+                <OphDsInput
+                    id="kutsumanimi"
+                    label={L('HENKILO_KUTSUMANIMI')}
+                    defaultValue={virkailija.kutsumanimi}
+                    onChange={(s) => onChange({ ...virkailija, kutsumanimi: s })}
+                    error={virheet.kutsumanimi || undefined}
+                />
+                <OphDsInput
+                    id="sukunimi"
+                    label={L('HENKILO_SUKUNIMI')}
+                    defaultValue={virkailija.sukunimi}
+                    onChange={(s) => onChange({ ...virkailija, sukunimi: s })}
+                    error={virheet.sukunimi || undefined}
+                />
+                <OphDsInput
+                    id="kayttajatunnus"
+                    label={L('HENKILO_KAYTTAJANIMI')}
+                    defaultValue={virkailija.kayttajatunnus}
+                    onChange={(s) => onChange({ ...virkailija, kayttajatunnus: s })}
+                    error={virheet.kayttajatunnus || undefined}
+                />
+                <OphDsInput
+                    id="salasana"
+                    label={L('HENKILO_PASSWORD')}
+                    defaultValue={virkailija.salasana}
+                    onChange={(s) => onChange({ ...virkailija, salasana: s })}
+                    error={virheet.salasana || undefined}
+                />
+                <p>{L('SALASANA_OHJE')}</p>
+                <OphDsInput
+                    id="salasanaUudestaan"
+                    label={L('HENKILO_PASSWORDAGAIN')}
+                    defaultValue={virkailija.salasanaUudestaan}
+                    onChange={(s) => onChange({ ...virkailija, salasanaUudestaan: s })}
+                    error={virheet.salasanaUudestaan || undefined}
+                />
+                <div>
+                    <button className="oph-ds-button" onClick={() => onSubmit(virkailija)} disabled={disabled}>
+                        {L('TALLENNA_LINKKI')}
+                    </button>
+                </div>
+            </div>
+        </OphDsPage>
     );
 };
